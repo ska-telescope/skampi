21f85461541b1d209d6cfffb78feb30b394c1f0f
vla22 <vla22@cam.ac.uk>
2019-10-23 12:16:27 +0100

    Fixed bugs in shell script.

--- resources/smoketest.sh
+++ resources/smoketest.sh
@@ -5,11 +5,11 @@ echo "hello"
 set -e -o pipefail
 echo "pipefail set"
 
-x = "$(kubectl get pods -n integration --field-selector=status.phase=Running | wc -l)"
+x=$(kubectl get pods -n integration --field-selector=status.phase=Running | wc -l)
 echo "x is set"
 echo "$x"
-
-if [ "$x" != "2" ]; then
+if [ $x != "21" ]; then
+	echo "not all pods are running"
 	exit 1
 
 fi

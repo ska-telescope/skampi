e127d28fee52fa71cf31830aef07eeb218299b3a
Sett <swai@ska.ac.za>
2019-11-08 17:45:04 +0200

    Update smoke test to accomodate more pods

--- scripts/smoketest.sh
+++ scripts/smoketest.sh
@@ -5,7 +5,7 @@ echo "pipefail set"
 sleep 4m
 x=$(kubectl get pods -n integration --field-selector=status.phase=Running | wc -l)
 echo "$x"
-if [ $x != "21" ]; then
+if [ $x != "27" ]; then
 	echo "not all pods are running"
 	exit 1
 

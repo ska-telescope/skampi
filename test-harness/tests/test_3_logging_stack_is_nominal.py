a291f0a56510f81958b8b7cd9f0f5cd5f419035c
SKAJohanVenter <aventer@ska.ac.za>
2019-11-11 12:20:09 +0200

    Added Kibana port to test as Kibana does not serve off port 80

--- test-harness/tests/test_3_logging_stack_is_nominal.py
+++ test-harness/tests/test_3_logging_stack_is_nominal.py
@@ -6,7 +6,7 @@ import json
 INGRESS_HOSTNAME="kibana-logging-sarao"
 
 def test_kibana_should_be_accessible_via_ingress():
-    url = "http://{}/app/kibana".format(INGRESS_HOSTNAME)
+    url = "http://{}:5601/app/kibana".format(INGRESS_HOSTNAME)
     res = requests.get(url)
 
     assert res.status_code == 200

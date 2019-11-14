88d40167c33d4f6dc8b7428c7df828cd89cc88e5
Apurva Patkar <apurva.ska@gmail.com>
2019-11-14 17:23:26 +0530

    Write test case for archiver.

---
+++ test-harness/tests/test_archiver.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Test archiver
+"""
+from tango import DeviceProxy
+
+def test_init():    
+  print("Init test archiver")
+
+def test_archiver():
+  config_mngr_devproxy = DeviceProxy("archiving/hdbpp/confmanager01")
+  #evt_sub_devproxy = DeviceProxy("archiving/hdbpp/eventsubscriber01")
+  result = config_mngr_devproxy.command_inout("AttributeStatus","tango://databaseds-tango-base-test:10000/mid_d0001/elt/master/WindSpeed")
+  print(result)
+  assert 1
+

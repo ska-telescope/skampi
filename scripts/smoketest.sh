39284abe1f041c2568dc8e6eb38faa5624bbd518
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2019-11-20 15:24:52 +0100

    smoke test refactoring

--- scripts/smoketest.sh
+++ scripts/smoketest.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 set -e -o pipefail
-echo "pipefail set"
-sleep 4m
-x=$(kubectl get pods -n integration --field-selector=status.phase=Running | wc -l)
-echo "$x"
-if [ $x != "31" ]; then
-	echo "not all pods are running"
-	exit 1
-
-fi
+echo "Smoke test START (expected pods=31)"
+expected_pod=31
+n=10
+while [ $n -gt 0 ]; do 
+	n=$(expr $n - 1)
+	x=$(kubectl get pods -n integration --field-selector=status.phase=Running | wc -l)
+	echo "Running pods=$x"
+	if [ $x != $expected_pod ]; then 
+    	echo "Waiting for pods to become running..."
+		sleep 30s
+	fi
+	if [ $x == $expected_pod ]; then 
+		exit 0
+	fi 
+done 
+exit 1

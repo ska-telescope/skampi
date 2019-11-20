dd250939c9ce7f850d9de98a9f58809334fb671b
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2019-11-20 16:52:58 +0100

    smoke test refactor (checking waiting containers)

--- scripts/smoketest.sh
+++ scripts/smoketest.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 set -e -o pipefail
-echo "Smoke test START (expected pods=31)"
-expected_pod=31
+echo "Smoke test START"
 n=10
 while [ $n -gt 0 ]; do 
-	n=$(expr $n - 1)
-	x=$(kubectl get pods -n integration --field-selector=status.phase=Running | wc -l)
-	echo "Running pods=$x"
-	if [ $x != $expected_pod ]; then 
-    	echo "Waiting for pods to become running..."
+	waiting=$(kubectl get pods -n integration -o=jsonpath='{.items[*].status.containerStatuses[*].state.waiting.reason}' | wc -w)
+	echo "Waiting containers=$waiting"
+	if [ $waiting != 0 ]; then 
+    	echo "Waiting 30s for pods to become running...#$n"
 		sleep 30s
 	fi
-	if [ $x == $expected_pod ]; then 
+	if [ $waiting == 0 ]; then 
+		echo "Smoke test SUCCESS"
 		exit 0
 	fi 
-done 
-exit 1
+	if [ $n == 1 ]; then 
+		waiting=$(kubectl get pods -n integration -o=jsonpath='{.items[*].status.containerStatuses[*].state.waiting.reason}' | wc -w)
+		echo "Smoke test FAILS"
+		echo "Found $waiting waiting containers: "
+		kubectl get pods -n integration -o=jsonpath='{range .items[*].status.containerStatuses[?(.state.waiting)]}{.state.waiting.message}{"\n"}{end}'
+		exit 1
+	fi 
+	n=$(expr $n - 1)
+done

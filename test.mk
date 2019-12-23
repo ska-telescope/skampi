e7b2b0f1b9b0a7b9d81a8360035805caad62a902
Sett <swai@ska.ac.za>
2019-12-23 12:49:20 +0200

    Wrap pytest invocation in make targets
    Flag --use-tiller-plugin is automatically enabled outside of CI

--- test.mk
+++ test.mk
@@ -8,3 +8,9 @@ template_tests:
 		|| rc=2 && continue; \
 	done; \
 	exit $$rc
+
+template_pytests:
+	pytest -m no_deploy $(if $(CI),,--use-tiller-plugin)
+
+chart_pytests:
+	pytest -m chart_deploy $(if $(CI),,--use-tiller-plugin)

09d7049d163dc05c277b15d89adfea9fb80ef958
gerhard <gerhardlr@gmail.com>
2020-01-28 11:48:08 +0000

    added to dos

--- test-harness/acceptance_tests/tests.py
+++ test-harness/acceptance_tests/tests.py
@@ -23,9 +23,13 @@ def pause():
 
 
 def test_allocation():
+
+    #given is from 29  to 44
+
     the_telescope = SKAMid()
     the_subarray = SubArray(1)
     the_resource_allocation = ResourceAllocation(dishes=[Dish(1), Dish(2)])
+
 
     #"under the hood resources for observing the internal state"
     tmc_subarray_node_01 = resource('ska_mid/tm_subarray_node/1')
@@ -34,17 +38,22 @@ def test_allocation():
     sdp_subarray_01 = resource('mid_sdp/elt/subarray_1')
 
     print("Starting up telescope ...")
+    #these are our assumptions for the initial state:
+    #1. Telescope is in the "OFF" state (this means all sub arrays are in the DISABLED state)
+    #we assume this is the state that the system will be after deployment
     the_telescope.start_up()
 
+    #maybe get rid of this
+    #print("Releasing any previously allocated resources... ")
+   # if (tmc_subarray_node_01.get("State") == 'ON') :
+   #     watch_State = watch(tmc_subarray_node_01).for_a_change_on("State")
+    #    result = the_subarray.deallocate()
+     #   State_val = watch_State.get_value_when_changed()
+     #   assert_that(State_val).is_equal_to("OFF")
 
-    print("Releasing any previously allocated resources... ")
-    if (tmc_subarray_node_01.get("State") == 'ON') :
-        watch_State = watch(tmc_subarray_node_01).for_a_change_on("State")
-        result = the_subarray.deallocate()
-        State_val = watch_State.get_value_when_changed()
-        assert_that(State_val).is_equal_to("OFF")
 
     print("Allocating new resources... ")
+    #when I alocate dishes 1 - 2 : lines 56 to 67
     #prepare
     watch_State = watch(tmc_subarray_node_01).for_a_change_on("State")
     watch_receptorIDList = watch(tmc_subarray_node_01).for_a_change_on("receptorIDList")
@@ -56,6 +65,7 @@ def test_allocation():
     State_val = watch_State.get_value_when_changed()
     receptorIDList_val = watch_receptorIDList.get_value_when_changed(20)
 
+    # then subarray cocrectly assigned resources
     # Confirm that TM_subbarray node has cocrectly assigned resources
     assert_that(result).is_equal_to(the_resource_allocation) 
     assert_that(State_val).is_equal_to("ON")
@@ -63,7 +73,7 @@ def test_allocation():
     assert_that(receptorIDList_val).is_equal_to((1, 2))
   
     # the following changes must have occurred before the previous
-    #CSP subarray must be in state ON and in obsState IDLE
+    #then CSP subarray must be in state ON and in obsState IDLE
     assert_that(csp_subarray_01.get('State')).is_equal_to('ON')
     assert_that(csp_subarray_01.get('obsState')).is_equal_to('IDLE')
     assert_that(csp_subarray_01.get('receptors')).is_equal_to((1,2))
@@ -71,10 +81,12 @@ def test_allocation():
     assert_that(csp_master.get('availableReceptorIDs')).is_equal_to((3,4))
     assert_that(csp_subarray_01.get('receptors')).is_equal_to((1,2))
 
-    #SDP subarray must be in state ON and in Obstate IDLE
+    #then SDP subarray must be in state ON and in Obstate IDLE
     assert_that(sdp_subarray_01.get('State')).is_equal_to('ON')
     assert_that(sdp_subarray_01.get('obsState')).is_equal_to('IDLE') 
 
+    #this should become part of the post session and the testing part should be another test
+    #############################################################################
     print("Now deallocating resources ... ")
     #prepare
     watch_State = watch(tmc_subarray_node_01).for_a_change_on("State")

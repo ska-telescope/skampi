11be01650416d557789bd6c71b0f5d671d36cf6b
Digvijay Mali <digvijay.ska@gmail.com>
2020-02-10 19:12:40 +0530

    story_AT1-447:Removed sleep statement in archiver test case and added logic to pass the test case.

--- test-harness/tests/test_4_archiver.py
+++ test-harness/tests/test_4_archiver.py
@@ -2,9 +2,7 @@
 """
 Test archiver
 """
-from tango import DeviceProxy, DevFailed
-from time import sleep
-import pytest
+from tango import DevFailed, DeviceProxy
 
 def test_init():
   print("Init test archiver")
@@ -14,45 +12,50 @@ def test_archiver():
   config_manager_device_fqdn = "archiving/hdbpp/confmanager01"
   conf_manager_proxy = DeviceProxy(config_manager_device_fqdn)
   evt_subscriber_device_proxy = DeviceProxy(evt_subscriber_device_fqdn)
-  attribute = "mid_d0001/elt/master/WindSpeed"
-  sleep(1)
-
+  attribute = "mid_d0001/elt/master/windspeed"
   # SetAttributeName
   conf_manager_proxy.write_attribute("SetAttributeName", attribute)
-  sleep(1)
   # SetArchiver
   conf_manager_proxy.write_attribute("SetArchiver", evt_subscriber_device_fqdn)
-  sleep(1)
   # SetStrategy
   conf_manager_proxy.write_attribute("SetStrategy", "ALWAYS")
-  sleep(1)
   # SetPollingPeriod
   conf_manager_proxy.write_attribute("SetPollingPeriod", 1000)
-  sleep(1)
   # SetEventPeriod
   conf_manager_proxy.write_attribute("SetPeriodEvent", 3000)
-  sleep(1)
+  # conf_manager_proxy.command_inout("AttributeAdd")
 
   try:
     # Add Attribute for archiving
     conf_manager_proxy.command_inout("AttributeAdd")
-    sleep(1)
+    flag=1
+    while flag:
+      attribute_list = evt_subscriber_device_proxy.read_attribute("AttributeList").value
+      for attribute_req in attribute_list:
+        if attribute in attribute_req:
+          flag=0
+          break
   except DevFailed as df:
     str_df = str(df)
     print("Exception: ", str_df)
 
   # Check status of Attribute Archiving in Configuration Manager
-  result_config_manager = conf_manager_proxy.command_inout("AttributeStatus",attribute)
+  result_config_manager = conf_manager_proxy.command_inout("AttributeStatus", attribute)
   # Check status of Attribute Archiving in Event Subscriber
   result_evt_subscriber = evt_subscriber_device_proxy.command_inout("AttributeStatus", attribute)
-
   assert "Archiving          : Started" in result_config_manager
   assert "Archiving          : Started" in result_evt_subscriber
 
   try:
     # Remove Attribute for archiving
     conf_manager_proxy.command_inout("AttributeRemove", attribute)
-    sleep(1)
+    flag=1
+    while flag:
+      attribute_list = evt_subscriber_device_proxy.read_attribute("AttributeList").value
+      for attribute_req in attribute_list:
+        if attribute not in attribute_req:
+          flag=0
+          break
   except DevFailed as df:
     str_df = str(df)
     print("Exception: ", str_df)

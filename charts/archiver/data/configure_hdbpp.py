1013f259bd1c625a3c085f38419bc508bff1bd6b
matteo1981 <matteo.dicarlo@inaf.it>
2020-02-20 16:59:56 +0000

    if already archived, do not add again it

--- charts/archiver/data/configure_hdbpp.py
+++ charts/archiver/data/configure_hdbpp.py
@@ -17,45 +17,38 @@ def cm_configure_attributes():
             
             attribute_fqdn = "tango://" + os.environ['TANGO_HOST'] + "/" + attribute
 
-            max_retries = 10
-            sleep_time = 30
-            for x in range(0, max_retries):
-                try:
-                    att = AttributeProxy(attribute_fqdn)
-                    att.read()
+            is_already_archived = False
+            attr_list = evt_subscriber_proxy.read_attribute("AttributeList").value
+            for already_archived in attr_list:
+                if attribute.lower() in str(already_archived).lower():
+                    is_already_archived = True
+                    already_configured_count += 1
                     break
-                except DevFailed as df:
-                    if(x == (max_retries -1)):
-                        raise df
-                    print("DevFailed exception: " + str(df.args[0].reason) + ". Sleeping for " + str(sleep_time) + "ss")
-                    sleep(sleep_time)
 
-            conf_manager_proxy.write_attribute("SetAttributeName", attribute_fqdn)
-            conf_manager_proxy.write_attribute("SetArchiver", evt_subscriber_device_fqdn)
-            conf_manager_proxy.write_attribute("SetStrategy", "ALWAYS")
-            conf_manager_proxy.write_attribute("SetPollingPeriod", 1000)
-            conf_manager_proxy.write_attribute("SetPeriodEvent", 3000)
-            
-            try:
-                conf_manager_proxy.command_inout("AttributeAdd")
+            if not is_already_archived:
+                max_retries = 10
+                sleep_time = 30
+                for x in range(0, max_retries):
+                    try:
+                        att = AttributeProxy(attribute_fqdn)
+                        att.read()
+                        break
+                    except DevFailed as df:
+                        if(x == (max_retries -1)):
+                            raise df
+                        print("DevFailed exception: " + str(df.args[0].reason) + ". Sleeping for " + str(sleep_time) + "ss")
+                        sleep(sleep_time)
+
+                conf_manager_proxy.write_attribute("SetAttributeName", attribute_fqdn)
+                conf_manager_proxy.write_attribute("SetArchiver", evt_subscriber_device_fqdn)
+                conf_manager_proxy.write_attribute("SetStrategy", "ALWAYS")
+                conf_manager_proxy.write_attribute("SetPollingPeriod", 1000)
+                conf_manager_proxy.write_attribute("SetPeriodEvent", 3000)
+                conf_manager_proxy.AttributeAdd()
                 configure_success_count += 1
                 print ("attribute_fqdn " + attribute_fqdn + " " + " added successfuly")
-            except DevFailed as df:
-                if str(df.args[0].reason) == 'Already archived':
-                    print("attribute_fqdn " + attribute_fqdn + " " + str(df.args[0].reason))
-                    already_configured_count += 1
-                    configure_success_count += 1
-                    continue
-                print("Exception: ", str(df))
-                configure_fail_count += 1
-                continue
+
     return configure_success_count, configure_fail_count, already_configured_count, total_attrib_count
-
-def start_archiving(str_attribute):
-    conf_manager_proxy.command_inout("AttributeStart", str_attribute)
-
-def remove_attribute(str_attribute):
-    conf_manager_proxy.command_inout("AttributeRemove", str_attribute)
 
 # Main entrypoint of the script.
 conf_manager_device_fqdn = ""
@@ -64,6 +57,7 @@ attr_list_file = ""
 ## parse arguments
 try:
     opts, args = getopt.getopt(sys.argv[1:], "c:e:a:", ["cm=", "es=", "attrfile="])
+
 except getopt.GetoptError:
     print("Please provide proper arguments.")
     print("Usage: $python configure_hdbpp.py --cm=<FQDN> --es=<FQDN> --attrfile=<filepath> OR")
@@ -83,12 +77,9 @@ for opt, arg in opts:
 conf_manager_proxy = DeviceProxy(conf_manager_device_fqdn)
 evt_subscriber_proxy = DeviceProxy(evt_subscriber_device_fqdn)
 
-conf_manager_proxy.set_timeout_millis(5000)
-evt_subscriber_proxy.set_timeout_millis(5000)
-
 configure_success_count, configure_fail_count, already_configured_count, total_attrib_count = cm_configure_attributes()
-
 print("Configured successfully: ", configure_success_count, "Failed: ", configure_fail_count, "Already configured: ", already_configured_count, "Total attributes: ", total_attrib_count)
+
 if configure_fail_count > 0:
     exit(-1)
 
--- test-harness/tests/test_4_archiver.py
+++ test-harness/tests/test_4_archiver.py
@@ -16,49 +16,46 @@ def test_archiver():
   conf_manager_proxy = DeviceProxy(config_manager_device_fqdn)
   evt_subscriber_device_proxy = DeviceProxy(evt_subscriber_device_fqdn)
 
-  conf_manager_proxy.set_timeout_millis(5000)
-  evt_subscriber_device_proxy.set_timeout_millis(5000)
+  # conf_manager_proxy.set_timeout_millis(5000)
+  # evt_subscriber_device_proxy.set_timeout_millis(5000)
 
-  attribute = "mid_d0001/elt/master/windspeed"
+  attribute = "sys/tg_test/1/double_scalar"
 
-  # wait for the attribute to be online. 
-  max_retries = 10
-  sleep_time = 30
-  for x in range(0, max_retries):
-      try:
-        att = AttributeProxy(attribute)
-        att.read()
-        break
-      except DevFailed as df:
-        if(x == (max_retries -1)):
-          raise df
-        logging.info("DevFailed exception: " + str(df.args[0].reason) + ". Sleeping for " + str(sleep_time) + "ss")
-        sleep(sleep_time)
+  is_already_archived = False
+  attr_list = evt_subscriber_device_proxy.read_attribute("AttributeList").value
+  for already_archived in attr_list:
+    if attribute in str(already_archived).lower():
+      is_already_archived = True
+      break
 
-  conf_manager_proxy.write_attribute("SetAttributeName", attribute)
-  conf_manager_proxy.write_attribute("SetArchiver", evt_subscriber_device_fqdn)
-  conf_manager_proxy.write_attribute("SetStrategy", "ALWAYS")
-  conf_manager_proxy.write_attribute("SetPollingPeriod", 1000)
-  conf_manager_proxy.write_attribute("SetPeriodEvent", 3000)
+  if not is_already_archived:
+    # wait for the attribute to be up and running for configuring it. 
+    max_retries = 10
+    sleep_time = 30
+    for x in range(0, max_retries):
+        try:
+          att = AttributeProxy(attribute)
+          att.read()
+          break
+        except DevFailed as df:
+          if(x == (max_retries -1)):
+            raise df
+          logging.info("DevFailed exception: " + str(df.args[0].reason) + ". Sleeping for " + str(sleep_time) + "ss")
+          sleep(sleep_time)
+  
+    conf_manager_proxy.write_attribute("SetAttributeName", attribute)
+    conf_manager_proxy.write_attribute("SetArchiver", evt_subscriber_device_fqdn)
+    conf_manager_proxy.write_attribute("SetStrategy", "ALWAYS")
+    conf_manager_proxy.write_attribute("SetPollingPeriod", 1000)
+    conf_manager_proxy.write_attribute("SetPeriodEvent", 3000)
+    conf_manager_proxy.AttributeAdd()
+    
+  evt_subscriber_device_proxy.Start()
 
-  try:
-    conf_manager_proxy.command_inout("AttributeAdd")
-  except DevFailed as df:
-    if not str(df.args[0].reason) == 'Already archived':    
-      logging.info("DevFailed exception: " + str(df.args[0].reason))
+  result_config_manager = conf_manager_proxy.AttributeStatus(attribute)
+  result_evt_subscriber = evt_subscriber_device_proxy.AttributeStatus(attribute)
+  
+  assert "Archiving          : Started" in result_config_manager
+  assert "Archiving          : Started" in result_evt_subscriber
 
-  max_retries = 10
-  sleep_time = 1
-  for x in range(0, max_retries):
-    try:
-      # Check status of Attribute Archiving in Configuration Manager
-      result_config_manager = conf_manager_proxy.command_inout("AttributeStatus", attribute)
-      # Check status of Attribute Archiving in Event Subscriber
-      result_evt_subscriber = evt_subscriber_device_proxy.command_inout("AttributeStatus", attribute)
-      assert "Archiving          : Started" in result_config_manager
-      assert "Archiving          : Started" in result_evt_subscriber
-    except DevFailed as df:
-      if(x == (max_retries -1)):
-        raise df
-      logging.info("DevFailed exception: " + str(df.args[0].reason) + ". Sleeping for " + str(sleep_time) + "ss")
-      sleep(sleep_time)
+  conf_manager_proxy.AttributeRemove(attribute)

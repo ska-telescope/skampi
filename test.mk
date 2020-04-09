0e890c2075c0d4cedc9597947b69585e5cbc7655
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2020-04-09 15:04:59 +0000

    no need for pytest.stdout

--- test.mk
+++ test.mk
@@ -3,15 +3,16 @@
 #
 # IMAGE_TO_TEST defines the tag of the Docker image to test
 #
-IMAGE_TO_TEST ?= nexus.engageska-portugal.pt/ska-docker/tango-vscode:0.2.1
+IMAGE_TO_TEST ?= nexus.engageska-portugal.pt/ska-docker/tango-vscode:0.2.2
 # Test runner - run to completion job in K8s
-TEST_RUNNER = test-makefile-runner-only-once-$(KUBE_NAMESPACE)-$(HELM_RELEASE)
+TEST_RUNNER = test-makefile-runner-$(CI_JOB_ID)-$(KUBE_NAMESPACE)-$(HELM_RELEASE)
 #
 # defines a function to copy the ./test-harness directory into the K8s TEST_RUNNER
 # and then runs the requested make target in the container.
 # capture the output of the test in a build folder inside the container 
 # 
 TANGO_HOST = databaseds-tango-base-$(HELM_RELEASE):10000
+MARK ?= fast
 #
 # defines a function to copy the ./test-harness directory into the K8s TEST_RUNNER
 # and then runs the requested make target in the container.

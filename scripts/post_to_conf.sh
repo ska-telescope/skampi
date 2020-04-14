ba3a6e74f13b3adef8f0ec1e43e616f66a046022
Dzianis Bartashevich <bartashevich@ua.pt>
2020-04-14 18:35:05 +0000

    add post to images page

--- scripts/post_to_conf.sh
+++ scripts/post_to_conf.sh
@@ -4,7 +4,6 @@ URL="https://confluence.skatelescope.org/rest/api/content/"
 page_name="Latest+Version+-+PI6"
 humane_page_name="Latest Version - PI6"
 Space="SWSI"
-KUBE_NAMESPACE=integration
 
 HELM_RELEASE=test
 KUBE_NAMESPACE=integration
@@ -45,5 +44,49 @@ data="$data\"version\":"
 data="$data{   \"number\":\"$page_version\"}}"
 #################################################
 
+
+curl -u $CONFLUENCE_USER:$CONFLUENCE_PWD -X PUT -H 'Content-Type: application/json' -d "$data" $URL$page_id | python -mjson.tool
+
+
+##########################################section on imges
+page_name="Images"
+humane_page_name="Images"
+
+path="{range .items[*]}"
+path="$path{range .spec.containers[*]}"
+path="$path{.image}{'\t\t'}{.name}{'\n'}"
+path="$path{end}{'\n'}"
+head="<p>The following table lists images used by pods as recorded on $date</p><p><table><colgroup><col /><col /></colgroup><tbody><tr><th>Image</th><th>Container</th></tr>"
+results=$(kubectl get pods -n $KUBE_NAMESPACE -o jsonpath="$path" | uniq | sort | awk 'NR > 1 {print "<tr><td>" $1 "</td><td>" $2 "</td></tr>"}')
+content="$head$results</tbody></table></p>"
+content=${content//$'\n'/}
+echo "$content" > "test.html"
+##get the page id
+page_id=$(curl -u $CONFLUENCE_USER:$CONFLUENCE_PWD -X GET "$URL?title=$page_name&spaceKey=$Space" |\
+ python -mjson.tool |\
+ python2 -c "import sys, json; print json.load(sys.stdin)['results'][0]['id']")
+#hget the page versionF
+page_version=$(curl -u $CONFLUENCE_USER:$CONFLUENCE_PWD -X GET \
+ "$URL?title=$page_name&spaceKey=$Space&expand=version" |\
+ python -mjson.tool |\
+ python2 -c "import sys, json; print json.load(sys.stdin)['results'][0]['version']['number']")
+echo "setting up for $page_id version $(($page_version + 1))"
+page_version=$(($page_version + 1))
+
+
+
+##data specification
+data="{\"id\":\"$page_id\","
+data="$data\"type\":\"page\","
+data="$data\"title\":\"$humane_page_name\","
+data="$data\"space\":{\"key\":\"$Space\"},"
+data="$data\"body\":"
+data="$data{   \"storage\":"
+data="$data{       \"value\":"
+data="$data                 \"$content\""
+data="$data,       \"representation\":\"storage\"}},"
+data="$data\"version\":"
+data="$data{   \"number\":\"$page_version\"}}"
+#################################################
 
 curl -u $CONFLUENCE_USER:$CONFLUENCE_PWD -X PUT -H 'Content-Type: application/json' -d "$data" $URL$page_id | python -mjson.tool

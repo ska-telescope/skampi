6e0d72e754524a9113c8ae06c5bab417f4034877
Apurva Patkar <apurva.ska@gmail.com>
2020-04-14 09:26:46 +0000

    update_images_tmc_csp: Revert the timeout values.

--- post-deployment/resources/test_support/helpers.py
+++ post-deployment/resources/test_support/helpers.py
@@ -63,7 +63,7 @@ class monitor(object):
             return comparison.all()
         else: return comparison
 
-    def _wait(self,timeout=200):
+    def _wait(self,timeout=80):
         timeout = timeout
         while ( self._is_not_changed()):
             timeout -=1
@@ -73,13 +73,13 @@ class monitor(object):
         return timeout
 
 
-    def get_value_when_changed(self,timeout=200):
+    def get_value_when_changed(self,timeout=50):
         response = self._wait(timeout)
         if (response == "timeout"):
             return "timeout"
         return self.current_value
     
-    def wait_until_value_changed(self,timeout=200):
+    def wait_until_value_changed(self,timeout=50):
         return self._wait(timeout)
 
 
@@ -100,7 +100,7 @@ def watch(resource):
 #this function may become depracated
 class state_checker:
 
-    def __init__(self,device,timeout=200,debug=False):
+    def __init__(self,device,timeout=80,debug=False):
         self.device = device
         self.timeout =timeout
         self.debug = debug
@@ -129,7 +129,7 @@ class state_checker:
                 timeout -= 1
         return "timed out"
 
-def wait_for(device,timeout=200):
+def wait_for(device,timeout=80):
     return state_checker(device,timeout)
 
 def take_subarray(id):
@@ -191,7 +191,7 @@ class waiter():
     def set_wait_for_ending_SB(self):
         self.waits.append(watch(resource('ska_mid/tm_subarray_node/1')).for_a_change_on("obsState"))
 
-    def wait(self,timeout = 200):
+    def wait(self,timeout = 80):
         self.logs = ""
         while self.waits:
             wait =self.waits.pop()

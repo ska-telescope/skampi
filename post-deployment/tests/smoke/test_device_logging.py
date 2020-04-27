3938452664d7cd609b0bef3785b8f573e8d11bd7
gerhard <gerhardlr@gmail.com>
2020-04-27 08:20:13 +0000

    abstracted out class for getting logging

--- post-deployment/tests/smoke/test_device_logging.py
+++ post-deployment/tests/smoke/test_device_logging.py
@@ -2,25 +2,44 @@ from resources.test_support.helpers import DeviceLogging
 import logging
 import json
 import pytest
+from assertpy import assert_that
 from time import sleep
 
 @pytest.mark.tracer
 def test_device_logging():
     d = DeviceLogging()
     d.update_traces(['sys/tg_test/1'])
-    #logging.debug("the device target = {}",str(d.tracer.dev))
-    logging.info('starting traces for sys/tg_test/1')
+    logging.debug('starting traces for sys/tg_test/1')
     d.start_tracing()
     d.wait_until_message_received("DataGenerator::generating data", 20)
-    logging.info(d.get_printable_messages())
+    dict_messages_before = d.get_messages_as_list_dict()
     d.stop_tracing()
-    d = DeviceLogging()
-    d.update_traces(['mid_csp/elt/subarray_01'])
-    logging.info('starting traces for mid_csp/elt/subarray_01')
+    d = DeviceLogging("TraceHelper")
+    d.update_traces(['sys/tg_test/1'])
+    logging.debug('starting traces again for for sys/tg_test/1')
     d.start_tracing()
-    sleep(5)
-    logging.info(d.get_printable_messages())
+    sleep(3)
+    dict_messages_after = d.get_messages_as_list_dict()
     d.stop_tracing()
+    assert_that(dict_messages_before).is_not_equal_to(dict_messages_after)
+
+
+
+def test_logging_on_test_device_as_string():
+    d = DeviceLogging()
+    d.update_traces(['sys/tg_test/1'])
+    logging.debug('starting traces for sys/tg_test/1')
+    d.start_tracing()
+    d.wait_until_message_received("DataGenerator::generating data", 20)
+    printeable_messages = d.get_printable_messages()
+    assert_that(printeable_messages).is_instance_of(str)
+    assert_that(printeable_messages).contains("DataGenerator::generating data")
+
+
+def test_throw_error_():
+    with pytest.raises(Exception):
+        DeviceLogging("wrong implementation")
+    DeviceLogging("TraceHelper")
 
 
     

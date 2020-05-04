f43df82ab70a646c3a9b5784cd06d1583475d6d5
samueltwum1 <samueltwum1@gmail.com>
2020-05-04 21:46:04 +0000

    Add more scenarios to the feature file and refactor the test module
    Co-authored-by: Paul Swart <paul@ska.ac.za>

--- post-deployment/features/XTG-131.feature
+++ post-deployment/features/XTG-131.feature
@@ -1,8 +1,26 @@
 Feature: TMC integration with Dish Software
-
+	
 	@XTG-131
-	Scenario: Dish to full power standby mode
-        	Given Dish Master reports OPERATE Dish mode
-		When I command Dish Master to STANDBY_FP Dish mode
-		Then Dish Master reports STANDBY_FP Dish mode
+	Scenario: Dish from STANDBY-LP to STANDBY-FP
+        Given Dish Master reports STANDBY-LP Dish mode
+		When I command Dish Master to STANDBY-FP Dish mode
+		Then Dish Master reports STANDBY-FP Dish mode
+		And Dish Master (device) is in STANDBY state
+
+	Scenario: Dish from STANDBY-FP to OPERATE
+        Given Dish Master reports STANDBY-FP Dish mode
+		When I command Dish Master to OPERATE Dish mode
+		Then Dish Master reports OPERATE Dish mode
+		And Dish Master (device) is in ON state	
+
+	Scenario: Dish from OPERATE to STANDBY-FP
+        Given Dish Master reports OPERATE Dish mode
+		When I command Dish Master to STANDBY-FP Dish mode
+		Then Dish Master reports STANDBY-FP Dish mode
+		And Dish Master (device) is in STANDBY state
+
+	Scenario: Dish from STANDBY-FP to STANDBY-LP
+        Given Dish Master reports STANDBY-FP Dish mode
+		When I command Dish Master to STANDBY-LP Dish mode
+		Then Dish Master reports STANDBY-LP Dish mode
 		And Dish Master (device) is in STANDBY state
--- post-deployment/tests/acceptance/mvp/test_XTG-131.py
+++ post-deployment/tests/acceptance/mvp/test_XTG-131.py
@@ -9,38 +9,71 @@ Acceptance tests for MVP.
 
 import logging
 import pytest
+from functools import partial
 from assertpy import assert_that
-from pytest_bdd import scenario, given, when, then
-from resources.test_support.helpers import resource, watch, waiter
+from pytest_bdd import scenario, given, when, then, parsers
+from resources.test_support.helpers import resource, watch
+
 
 LOGGER = logging.getLogger(__name__)
+scenario = partial(scenario, "../../../features/XTG-131.feature")
+mode_cmd_map = {
+    "STANDBY-LP": "SetStandbyLPMode",
+    "STANDBY-FP": "SetStandbyFPMode",
+    "OPERATE": "SetOperateMode"
+}
+
+
+def _change_dish_mode(cmd, dev_proxy):
+    getattr(dev_proxy, cmd)()
+    watch_dish_mode = (watch(resource('mid_d0001/elt/master')).for_a_change_on('dishMode'))
+    watch_dish_mode.wait_until_value_changed()
+
+@pytest.fixture
+def set_initial_dish_mode(create_dish_master_proxy):
+    # standbyfp mode is used as initial condition since it can be reached from other dish modes
+    _change_dish_mode('SetStandbyFPMode', create_dish_master_proxy)
+    _change_dish_mode('SetStandbyLPMode', create_dish_master_proxy)
+
+# define steps for scenario
+@given(parsers.parse("Dish Master reports {expected:D} Dish mode"))
+def pre_condition(expected):
+    logging.info('Dish 0001 initial dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
+    assert_that(resource('mid_d0001/elt/master').get('dishMode')).is_equal_to(expected)
+
+@when(parsers.parse("I command Dish Master to {requested:D} Dish mode"))
+def set_dish_mode(requested, create_dish_master_proxy):
+    _change_dish_mode(mode_cmd_map[requested], create_dish_master_proxy)
+    logging.info('Dish 0001 requested dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
+
+@then(parsers.parse("Dish Master reports {desired:D} Dish mode"))
+def check_dish_mode(desired):
+    logging.info('Dish 0001 desired dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
+    assert_that(resource('mid_d0001/elt/master').get('dishMode')).is_equal_to(desired)
+
+@then(parsers.parse("Dish Master (device) is in {desired:D} state"))
+def check_master_device_state(desired):
+    logging.info('Dish 0001 desired state: ' + resource('mid_d0001/elt/master').get('State'))
+    assert_that(resource('mid_d0001/elt/master').get('State')).is_equal_to(desired)
+
+
+# Note: scenarios have to be executed in sequential order in order for the tests to pass
+@pytest.mark.fast
+@scenario("Dish from STANDBY-LP to STANDBY-FP")
+def test_dish_standbylp_to_standbyfp_mode(set_initial_dish_mode):
+    pass
 
 @pytest.mark.fast
-@scenario("../../../features/XTG-131.feature", "Dish to full power standby mode")
-def test_dish_full_power_mode():
-    """Set Dish to full power standby mode"""
+@scenario("Dish from STANDBY-FP to OPERATE")
+def test_dish_standbyfp_to_operate_mode():
+    pass
 
-@given("Dish Master reports OPERATE Dish mode")
-def pre_condition():
-    logging.info('Dish 0001 dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
-    assert_that(resource('mid_d0001/elt/master').get('dishMode')).is_equal_to('OPERATE')
+@pytest.mark.fast
+@scenario("Dish from OPERATE to STANDBY-FP")
+def test_dish_operate_to_standbyfp():
+    pass
 
-@when("I command Dish Master to STANDBY_FP Dish mode")
-def set_dish_standby_fp(create_dish_master_proxy):
-    create_dish_master_proxy.SetStandbyFPMode()
-    # Using waiter object to wait for the mode change to happen
-    the_waiter = waiter()
-    the_waiter.waits.append(watch(resource('mid_d0001/elt/master')).for_a_change_on('dishMode'))
-    the_waiter.wait()
-    logging.info('Dish 0001 dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
-
-@then("Dish Master reports STANDBY_FP Dish mode")
-def check_dish_standby_fp():
-    logging.info('Dish 0001 dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
-    assert_that(resource('mid_d0001/elt/master').get('dishMode')).is_equal_to('STANDBY-FP')
-
-@then("Dish Master (device) is in STANDBY state")
-def check_master_device_state():
-    logging.info('Dish 0001 state: ' + resource('mid_d0001/elt/master').get('State'))
-    assert_that(resource('mid_d0001/elt/master').get('State')).is_equal_to('STANDBY')
-
+@pytest.mark.fast
+@scenario("Dish from STANDBY-FP to STANDBY-LP")
+def test_dish_standbyfp_to_standbylp():
+    pass

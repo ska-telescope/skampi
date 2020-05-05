1b5485045280efbe46bc695c0d2aa62320627bf1
shraddhabajare <shraddhab.ska@gmail.com>
2020-05-05 11:57:51 +0000

    SS-37-subarray-multi-scan:Updated smoke tests with new Json strings.

--- post-deployment/tests/bugs/test_skb3.py
+++ post-deployment/tests/bugs/test_skb3.py
@@ -12,7 +12,7 @@ def test_startup(create_centralnode_proxy, create_subarray1_proxy):
 
 @pytest.mark.fast
 def test_assignresources_sub1(create_centralnode_proxy, create_subarray1_proxy):
-    test_input = '{"subarrayID":1,"dish":{"receptorIDList":["0001"]}}'
+    test_input = '{"subarrayID":1,"dish":{"receptorIDList":["0001"]},"sdp":{"id":"sbi-mvp01-20200325-00001","max_length":100.0,"scan_types":[{"id":"science_A","coordinate_system":"ICRS","ra":"02:42:40.771","dec":"-00:00:47.84","subbands":[{"freq_min":0.35e9,"freq_max":1.05e9,"nchan":372,"input_link_map":[[1,0],[101,1]]}]},{"id":"calibration_B","coordinate_system":"ICRS","ra":"12:29:06.699","dec":"02:03:08.598","subbands":[{"freq_min":0.35e9,"freq_max":1.05e9,"nchan":372,"input_link_map":[[1,0],[101,1]]}]}],"processing_blocks":[{"id":"pb-mvp01-20200325-00001","workflow":{"type":"realtime","id":"vis_receive","version":"0.1.0"},"parameters":{}},{"id":"pb-mvp01-20200325-00002","workflow":{"type":"realtime","id":"test_realtime","version":"0.1.0"},"parameters":{}},{"id":"pb-mvp01-20200325-00003","workflow":{"type":"batch","id":"ical","version":"0.1.0"},"parameters":{},"dependencies":[{"pb_id":"pb-mvp01-20200325-00001","type":["visibilities"]}]},{"id":"pb-mvp01-20200325-00004","workflow":{"type":"batch","id":"dpreb","version":"0.1.0"},"parameters":{},"dependencies":[{"pb_id":"pb-mvp01-20200325-00003","type":["calibration"]}]}]}}'
     create_centralnode_proxy.AssignResources(test_input)
     sleep(3)
     result = create_subarray1_proxy.receptorIDList

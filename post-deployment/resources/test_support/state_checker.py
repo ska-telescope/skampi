eafa6c86372ecfd0ec11fa2c0780b58e2c0ce628
gerhard <gerhardlr@gmail.com>
2020-05-05 07:46:40 +0000

    filter to only unique recordings

--- post-deployment/resources/test_support/state_checker.py
+++ post-deployment/resources/test_support/state_checker.py
@@ -34,11 +34,14 @@ class StateChecker():
         with self.lock:
             self.th_records = list
 
-    def get_records(self):
+    def get_records(self,filtered=False):
         with self.lock:
             is_allowed = not self.th_running
         if is_allowed:
-            return self.th_records
+            if filtered:
+                return [record for record in self.th_records if record['unique']]
+            else:
+                return self.th_records
         
 
     def run(self,threaded=True,resolution=0.1):
@@ -57,6 +60,22 @@ class StateChecker():
         self._clear_running()
         self.thread.join()
 
+    def _get_filter_states_from_record(self,record):
+        keys = record.keys()
+        return [record[key] for key in keys if " state" in key]
+
+
+    def _is_unique(self,list,record):
+        if list == []:
+            return True
+        previous_filtered_states = self._get_filter_states_from_record(list[-1])
+        current_filteres_states = self._get_filter_states_from_record(record)
+        if previous_filtered_states == current_filteres_states:
+            return False
+        else:
+            return True
+
+
     def _loop(self,resources,state_name,resolution,max_nr_of_records):
         n = 0
         list = []
@@ -69,6 +88,10 @@ class StateChecker():
             for resource_name in resources:
                 record['{} state'.format(resource_name)] = resource(resource_name).get(state_name)
                 record['{} delta'.format(resource_name)] = '{:f}'.format(time() - start_time)
+            if self._is_unique(list,record):
+                record['unique'] = True
+            else:
+                record['unique'] = False
             list.append(record)
             if max_nr_of_records != None:
                 if n >= max_nr_of_records:
--- post-deployment/tests/smoke/test_state_checker.py
+++ post-deployment/tests/smoke/test_state_checker.py
@@ -5,12 +5,17 @@ import mock
 import logging
 from assertpy import assert_that
 import re
-from time import sleep
+from time import sleep,time
 from datetime import datetime
 
-def mock_get(par):
+def mock_get_unique(par):
     sleep(0.005)
-    return "dummy state for {}".format(par)
+    unique = time()
+    return "dummy state {} {}".format(unique,par)
+
+def mock_get_duplicate(par):
+    sleep(0.005)
+    return "{}".format(par)
 
 def validate_results(res):
 
@@ -24,7 +29,8 @@ def validate_results(res):
         'dummy resource-2 state',
         'dummy resource-2 delta',
         'dummy resource-3 state',
-        'dummy resource-3 delta',])
+        'dummy resource-3 delta',
+        'unique'])
     assert_that(
         [ record[keys[0]] for record in res] )\
         .is_equal_to(
@@ -37,7 +43,7 @@ def validate_results(res):
     )
     for n in range(2,6,2):
         assert_that(
-            [   re.match('dummy state for ObsState',record[keys[n]]) != None for record in res]).\
+            [   re.match('.+obsState',record[keys[n]]) != None for record in res]).\
             is_equal_to(
             [ True for n in range(10) ]
         )
@@ -47,15 +53,19 @@ def validate_results(res):
             is_equal_to(
             [ True for n in range(10) ]
         )
+    assert_that(
+        [ record[keys[8]] for record in res] )\
+        .is_equal_to(
+        [ True for n in range(10)]
+    )
     start_time_window = datetime.strptime(res[0]['time_window'],'%H:%M:%S')
     end_time_window =  datetime.strptime(res[9]['time_window'],'%H:%M:%S')
-    assert_that(end_time_window.second-start_time_window.second).is_equal_to(1)
-
+    assert_that((end_time_window-start_time_window).seconds).is_close_to(1,1)
 
 @mock.patch('resources.test_support.state_checker.resource')
 def test_non_threaded_loop(resource_mock):
     #given
-    resource_mock.return_value.get = mock_get
+    resource_mock.return_value.get = mock_get_unique
     s = StateChecker([
         'dummy resource-1',
         'dummy resource-2',
@@ -66,6 +76,8 @@ def test_non_threaded_loop(resource_mock):
     #then
     res = s.get_records()
     validate_results(res)
+    filtered_result = s.get_records(filtered=True)
+    assert_that(filtered_result).is_length(10)
     
 def test_threaded_loop():
     #give
@@ -80,3 +92,40 @@ def test_threaded_loop():
     res = s.get_records()
     assert_that(res).is_not_empty()
     
+@mock.patch('resources.test_support.state_checker.resource')
+def test_annotate_uniqueness(resource_mock):
+    #given
+    resource_mock.return_value.get = mock_get_duplicate
+    s = StateChecker([
+        'dummy resource-1',
+        'dummy resource-2',
+        'dummy resource-3'],
+        max_nr_of_records=10)
+    #when
+    s.run(threaded=False,resolution=0.1)
+    #then
+    res = s.get_records()
+    expected = [True]
+    expected.extend([ False for n in range(9)])
+    assert_that(
+        [ record['unique'] for record in res] )\
+        .is_equal_to(expected)
+    filtered_result = s.get_records(filtered=True)
+    assert_that(filtered_result).is_length(1)
+
+
+@mock.patch('resources.test_support.state_checker.resource')
+def test_time_window(resource_mock):
+    #given
+    resource_mock.return_value.get = mock_get_unique
+    s = StateChecker([
+        'dummy resource-1',
+        'dummy resource-2',
+        'dummy resource-3'],
+        max_nr_of_records=10)
+    #when
+    s.run(threaded=False,resolution=0.1)
+    #then
+    records = s.get_records()
+    for record in records:
+        logging.info(record['time_window'])

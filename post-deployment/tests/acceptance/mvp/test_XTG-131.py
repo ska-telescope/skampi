680da1fa34cda95c2bf92856ce197e195f1b32d0
samueltwum1 <samueltwum1@gmail.com>
2020-05-05 10:40:19 +0000

    Make implementation more generic
    Co-authored-by: Paul Swart <paul@ska.ac.za>

--- post-deployment/tests/acceptance/mvp/test_XTG-131.py
+++ post-deployment/tests/acceptance/mvp/test_XTG-131.py
@@ -12,6 +12,7 @@ import pytest
 from functools import partial
 from assertpy import assert_that
 from pytest_bdd import scenario, given, when, then, parsers
+from tango import DeviceProxy
 from resources.test_support.helpers import resource, watch
 
 
@@ -24,56 +25,55 @@ mode_cmd_map = {
 }
 
 
-def _change_dish_mode(cmd, dev_proxy):
+def _change_dish_mode(dev_proxy, cmd, device_name):
     getattr(dev_proxy, cmd)()
-    watch_dish_mode = (watch(resource('mid_d0001/elt/master')).for_a_change_on('dishMode'))
+    watch_dish_mode = watch(resource(device_name)).for_a_change_on('dishMode')
     watch_dish_mode.wait_until_value_changed()
 
-@pytest.fixture
-def set_initial_dish_mode(create_dish_master_proxy):
-    # standbyfp mode is used as initial condition since it can be reached from other dish modes
-    _change_dish_mode('SetStandbyFPMode', create_dish_master_proxy)
-    _change_dish_mode('SetStandbyLPMode', create_dish_master_proxy)
-
 # define steps for scenario
-@given(parsers.parse("Dish Master reports {expected:D} Dish mode"))
-def pre_condition(expected):
-    logging.info('Dish 0001 initial dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
-    assert_that(resource('mid_d0001/elt/master').get('dishMode')).is_equal_to(expected)
+@given(parsers.parse("{device_name} reports {expected} Dish mode"))
+def device_proxy(device_name, expected):
+    dev_proxy = DeviceProxy(device_name)
+    # standbyfp mode is used as initial condition since it can be reached from other dish modes
+    _change_dish_mode(dev_proxy, 'SetStandbyFPMode', device_name)
+    _change_dish_mode(dev_proxy, mode_cmd_map[expected], device_name)
+    logging.info(device_name + ' initial dishMode: ' + resource(device_name).get('dishMode'))
+    assert_that(resource(device_name).get('dishMode')).is_equal_to(expected)
+    return dev_proxy
 
-@when(parsers.parse("I command Dish Master to {requested:D} Dish mode"))
-def set_dish_mode(requested, create_dish_master_proxy):
-    _change_dish_mode(mode_cmd_map[requested], create_dish_master_proxy)
-    logging.info('Dish 0001 requested dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
+@when(parsers.parse("I command {device_name} to {requested} Dish mode"))
+def set_dish_mode(device_name, requested, device_proxy):
+    _change_dish_mode(device_proxy, mode_cmd_map[requested], device_name)
+    logging.info(device_name + ' requested dishMode: ' + resource(device_name).get('dishMode'))
 
-@then(parsers.parse("Dish Master reports {desired:D} Dish mode"))
-def check_dish_mode(desired):
-    logging.info('Dish 0001 desired dishMode: ' + resource('mid_d0001/elt/master').get('dishMode'))
-    assert_that(resource('mid_d0001/elt/master').get('dishMode')).is_equal_to(desired)
+@then(parsers.parse("{device_name} reports {desired} Dish mode"))
+def check_dish_mode(device_name, desired):
+    logging.info(device_name + ' desired dishMode: ' + resource(device_name).get('dishMode'))
+    assert_that(resource(device_name).get('dishMode')).is_equal_to(desired)
 
-@then(parsers.parse("Dish Master (device) is in {desired:D} state"))
-def check_master_device_state(desired):
-    logging.info('Dish 0001 desired state: ' + resource('mid_d0001/elt/master').get('State'))
-    assert_that(resource('mid_d0001/elt/master').get('State')).is_equal_to(desired)
+@then(parsers.parse("{device_name} is in {desired} state"))
+def check_master_device_state(device_name, desired):
+    logging.info(device_name + ' desired state: ' + resource(device_name).get('State'))
+    assert_that(resource(device_name).get('State')).is_equal_to(desired)
 
 
-# Note: scenarios have to be executed in sequential order in order for the tests to pass
+# mid_d0001/elt/master
 @pytest.mark.fast
-@scenario("Dish from STANDBY-LP to STANDBY-FP")
-def test_dish_standbylp_to_standbyfp_mode(set_initial_dish_mode):
+@scenario("mid_d0001/elt/master from STANDBY-LP to STANDBY-FP")
+def test_mid_d0001_from_standbylp_to_standbyfp_mode():
     pass
 
 @pytest.mark.fast
-@scenario("Dish from STANDBY-FP to OPERATE")
-def test_dish_standbyfp_to_operate_mode():
+@scenario("mid_d0001/elt/master from STANDBY-FP to OPERATE")
+def test_mid_d0001_from_standbyfp_to_operate_mode():
     pass
 
 @pytest.mark.fast
-@scenario("Dish from OPERATE to STANDBY-FP")
-def test_dish_operate_to_standbyfp():
+@scenario("mid_d0001/elt/master from OPERATE to STANDBY-FP")
+def test_mid_d0001_from_operate_to_standbyfp():
     pass
 
 @pytest.mark.fast
-@scenario("Dish from STANDBY-FP to STANDBY-LP")
-def test_dish_standbyfp_to_standbylp():
+@scenario("mid_d0001/elt/master from STANDBY-FP to STANDBY-LP")
+def test_mid_d0001_from_standbyfp_to_standbylp():
     pass

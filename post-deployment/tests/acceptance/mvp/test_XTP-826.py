52e32afaf49483e9d0a4eec1313564b581ea1ae5
mark-nicol <mark.nicol@stfc.ac.uk>
2020-05-25 09:10:55 +0000

    Added toggle to allow XTP-826t to be disabled - tidied up linitng issues

--- post-deployment/tests/acceptance/mvp/test_XTP-826.py
+++ post-deployment/tests/acceptance/mvp/test_XTP-826.py
@@ -6,23 +6,21 @@ test_XTP-826
 ----------------------------------
 Acceptance tests for MVP.
 """
+import os
 import logging
 import pytest
-from functools import partial
 from assertpy import assert_that
-from pytest_bdd import scenario, given, when, then, parsers
-from tango import DeviceProxy
-#SUT import
+from pytest_bdd import scenario, given, when, then
+# SUT import
 from oet.domain import SubArray
 
-## local imports
-from resources.test_support.helpers import resource, watch
-from resources.test_support.persistance_helping import update_resource_config_file
+# local imports
+from resources.test_support.helpers import resource
 from resources.test_support.logging_decorators import log_it
-from resources.test_support.sync_decorators import sync_scan_oet
-from resources.test_support.controls import set_telescope_to_standby, set_telescope_to_running, telescope_is_in_standby, take_subarray
+from resources.test_support.controls import set_telescope_to_standby, set_telescope_to_running
+from resources.test_support.controls import telescope_is_in_standby, take_subarray
 
-devices_to_log = [
+DEVICES_TO_LOG = [
     'ska_mid/tm_subarray_node/1',
     'mid_csp/elt/subarray_01',
     'mid_csp_cbf/sub_elt/subarray_01',
@@ -31,81 +29,126 @@ devices_to_log = [
     'mid_d0002/elt/master',
     'mid_d0003/elt/master',
     'mid_d0004/elt/master']
-non_default_states_to_check = {
-    'mid_d0001/elt/master' : 'pointingState',
-    'mid_d0002/elt/master' : 'pointingState',
-    'mid_d0003/elt/master' : 'pointingState',
-    'mid_d0004/elt/master' : 'pointingState'}
+NON_DEFAULT_DEVICES_TO_CHECK = {
+    'mid_d0001/elt/master': 'pointingState',
+    'mid_d0002/elt/master': 'pointingState',
+    'mid_d0003/elt/master': 'pointingState',
+    'mid_d0004/elt/master': 'pointingState'}
 
 
-@pytest.fixture()
-def result():
-    fixture = {'SUT executed':False,"Test Passed":True,"subarray":None}
+DEV_TEST_TOGGLE = os.environ.get('DISABLE_DEV_TESTS')
+if DEV_TEST_TOGGLE == "False":
+    DISABLE_TESTS_UNDER_DEVELOPMENT = False
+else:
+    DISABLE_TESTS_UNDER_DEVELOPMENT = True
+# by default these tests are disabled
+
+
+def test_can_read_env():
+    """
+    Check that we can read the toggle for development tests - if not the
+    default behaviour will be to assume the tests are disabled
+    """
+    logging.info(os.environ.get('DISABLE_DEV_TESTS'))
+
+
+@pytest.fixture(name="result")
+def fixture_result():
+    """structure used to hold details of the intermediate result at each stage of the test"""
+    fixture = {'SUT executed': False, "Test Passed": True, "subarray": None}
     yield fixture
-    ##teard down
+    # teardown
     end(fixture)
 
+
 def get_subarray_state():
+    """return the current state of the subarray"""
     return resource('ska_mid/tm_subarray_node/1').assert_attribute('State')
 
+
 def subarray_is_in_state(expected_state):
+    """confirtm the subarray is in a given state"""
     get_subarray_state().equals(expected_state)
 
+
+def check_resource_ready(resource_name):
+    "check that for the passed in resource name the reported obsState is READY"
+    obstate = resource(resource_name).get('obsState')
+    assert_that(obstate.value == 'READY')
+    msg = resource_name + " obsState :" + obstate
+    logging.info(msg)
 
 
 LOGGER = logging.getLogger(__name__)
 @scenario("../../../features/XTP-826.feature", "Run more than one scan on a sub array")
+@pytest.mark.skipif(DISABLE_TESTS_UNDER_DEVELOPMENT, reason="disbaled by local env")
 def test_multi_scan():
     """Multiscan Test."""
-    pass
+
 
 @given('a running telescope upon which a previously scan has successfully run')
 def setup_telescope_and_scan(result):
-
-    assert  telescope_is_in_standby(), f"Test failed as telescope state is {get_subarray_state().value}"
+    """
+    confirm the telescope is ready, and then to be sure we are testing a multiscan scenario
+    perform one scan
+    """
+    assert telescope_is_in_standby(
+    ), f"Test failed as telescope state is {get_subarray_state().value}"
     set_telescope_to_running()
 
     LOGGER.info("Ensuring resources are assigned")
     result['subarray'] = take_subarray(1).to_be_composed_out_of(2)
 
     LOGGER.info("configuring for first scan")
-    result['subarray'].and_configure_scan_by_file(file = 'resources/test_data/TMC_integration/configure1.json')
+    result['subarray'].and_configure_scan_by_file(
+        file='resources/test_data/TMC_integration/configure1.json')
 
     LOGGER.info("executing first scan")
     result['subarray'].and_run_a_scan()
     return result
 
+
 @given('I have configured it again')
 def configure_again(result):
+    """
+    reconfigure the telescope for a new scan
+    assuming this scenario includes a reconfiguration of the source from
+    what was done for the previous scan
+    """
     LOGGER.info("Configuring  second scan")
-    result['subarray'].and_configure_scan_by_file(file = 'resources/test_data/TMC_integration/configure2.json')
+    result['subarray'].and_configure_scan_by_file(
+        file='resources/test_data/TMC_integration/configure2.json')
+
 
 @when('I run the scan again')
-def configure_second_scan(result):
-
+def execute_second_scan(result):
+    """
+    execute the configured scan - this is the key part of this test
+    """
     LOGGER.info("Executing second scan")
     #####################SUT is execucted#################
-    @log_it('XTP-826',devices_to_log,non_default_states_to_check)
+    @log_it('XTP-826', DEVICES_TO_LOG, NON_DEFAULT_DEVICES_TO_CHECK)
     def scan():
         SubArray(1).scan()
     scan()
     #############################################
     result['SUT executed'] = True
 
-# we may not even have to put in this checks since implicitly if we got to this point it means we didnt had any exceptions
+
 @then('the scan should complete without any errors or exceptions')
 def check_completion_state(result):
+    """
+    interpreted as the TMC subarry, csp and sdp report subarray as being in the obsState = READY
+    if we got to this point it means we didnt have any exceptions
+    """
     LOGGER.info("checking completion status")
-    # check that the TMC report subarray as being in the obsState = READY
-    assert_that(resource('ska_mid/tm_subarray_node/1').get('obsState')).is_equal_to('READY')
-    logging.info("TMC-subarray obsState: " + resource('ska_mid/tm_subarray_node/1').get("obsState"))
-    # check that the CSP report subarray as being in the obsState = READY
-    assert_that(resource('mid_csp/elt/subarray_01').get('obsState')).is_equal_to('READY')
-    logging.info("CSP-subarray obsState: " + resource('mid_csp/elt/subarray_01').get("obsState"))
-    # check that the SDP report subarray as being in the obsState = READY
-    assert_that(resource('mid_sdp/elt/subarray_1').get('obsState')).is_equal_to('READY')
-    logging.info("SDP-subarray obsState: " + resource('mid_sdp/elt/subarray_1').get("obsState"))
+
+    check_resource_ready('ska_mid/tm_subarray_node/1')
+    check_resource_ready('mid_csp/elt/subarray_01')
+    check_resource_ready('mid_sdp/elt/subarray_01')
+
     result['test passed'] = True
+
 
 def end(result):
     """ teardown any state that was previously setup with a setup_function
@@ -115,7 +158,7 @@ def end(result):
     if result['subarray'] is not None:
         LOGGER.info("Resetting subarray")
         result['subarray'].reset()
-    if (resource('ska_mid/tm_subarray_node/1').get("State") == "ON"):
+    if resource('ska_mid/tm_subarray_node/1').get("State") == "ON":
         LOGGER.info("Release all resources assigned to subarray")
         take_subarray(1).and_release_all_resources()
     if telescope_is_in_standby():

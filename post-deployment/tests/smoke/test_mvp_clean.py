ee5a569268dec67a03c37a3dbba443bed41e1d48
Adriaan de Beer <adriaan@spaceadvisory.com>
2020-05-26 13:46:18 +0000

    Pre- and Post smell tests, with more visible log output

--- post-deployment/tests/smoke/test_mvp_clean.py
+++ post-deployment/tests/smoke/test_mvp_clean.py
@@ -7,6 +7,7 @@ from resources.test_support.helpers import resource, subarray_devices
 
 LOGGER = logging.getLogger(__name__)
 
+
 ####typical device sets
 # subarray_devices = [
 #         'ska_mid/tm_subarray_node/1',
@@ -15,13 +16,16 @@ LOGGER = logging.getLogger(__name__)
 #         'mid_sdp/elt/subarray_1']
 
 @pytest.mark.first
-@pytest.mark.last
-def test_mvp_clean():
-    LOGGER.info(f"CURRENT STATES ('Device Name': State,\tobsState)")
+def test_smell_mvp(pre_or_post="#PRE"):
+    LOGGER.info(    f"###{pre_or_post}-TEST STATES### 'Device Name': State, obsState)")
     for device in subarray_devices:
-        LOGGER.info(f"'{device}':\t\t{resource(device).get('State')}',\t'{resource(device).get('obsState')}'")
+        LOGGER.info(f"###################### '{device:>35}': '{resource(device).get('State'):<10}', '{resource(device).get('obsState')}'")
     LOGGER.info("Testing only for equality, omitting SDP states for now")
+
     assert_that(resource('mid_csp/elt/subarray_01').get('State')).is_equal_to(resource('ska_mid/tm_subarray_node/1').get('State'))
     assert_that(resource('ska_mid/tm_subarray_node/1').get('State')).is_equal_to(resource('mid_csp_cbf/sub_elt/subarray_01').get('State'))
     assert_that(resource('mid_csp/elt/subarray_01').get('State')).is_equal_to(resource('mid_csp_cbf/sub_elt/subarray_01').get('State'))
     
+@pytest.mark.last
+def test_smell_mvp_after():
+    test_smell_mvp("POST")

5be516319b2648ea67d4924c06e5496cfbe95cc5
Adriaan de Beer <adriaan@spaceadvisory.com>
2020-05-26 12:34:09 +0000

    Removed excessive tabs

--- post-deployment/tests/smoke/test_mvp_clean.py
+++ post-deployment/tests/smoke/test_mvp_clean.py
@@ -19,7 +19,7 @@ LOGGER = logging.getLogger(__name__)
 def test_mvp_clean():
     LOGGER.info(f"CURRENT STATES ('Device Name': State,\tobsState)")
     for device in subarray_devices:
-        LOGGER.info(f"\n'{device}' states:\n\t\t\t\t\t\t\t\t\t\t\t\t\t{resource(device).get('State')}',\t'{resource(device).get('obsState')}'")
+        LOGGER.info(f"'{device}':\t\t{resource(device).get('State')}',\t'{resource(device).get('obsState')}'")
     LOGGER.info("Testing only for equality, omitting SDP states for now")
     assert_that(resource('mid_csp/elt/subarray_01').get('State')).is_equal_to(resource('ska_mid/tm_subarray_node/1').get('State'))
     assert_that(resource('ska_mid/tm_subarray_node/1').get('State')).is_equal_to(resource('mid_csp_cbf/sub_elt/subarray_01').get('State'))

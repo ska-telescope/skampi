438db5577357b0bb1f4821df671723600a9a981d
Gerhard le Roux <gerhardlr@gmail.com>
2020-06-19 08:32:57 +0000

    updates to investigate script

--- post-deployment/exploration/explore_event_subscription.py
+++ post-deployment/exploration/explore_event_subscription.py
@@ -5,15 +5,15 @@ def run_polling():
     p = DeviceProxy('test/device/1') 
     strategy1 = StrategyListenbyPolling(p) 
     l = Listener(p, strategy1,serverside_polling=True)
-    #p.pushscalarchangeevents('{"attribute_name": "double_scalar", "number_of_events": 10, "rate_of_events": 1}') 
+    p.pushscalarchangeevents('{"attribute_name": "double_scalar", "number_of_events": 10, "rate_of_events": 1}') 
     for event in l.get_events_on('long_scalar'):
         print(f'Event recieved on {event.get_date().strftime("%H:%M:%S")}:\nName: {event.attr_value.name}\nValue: {event.attr_value.value}')
 
 def subscribe_basic(p, attr):
     return p.subscribe_event(attr,EventType.CHANGE_EVENT,10)
 
-def get_events(device_proxy, subscription_id):
-    return device_proxy.get_events(subscription_id)
+def get_events(p, sub_id):
+    return p.get_events(sub_id)
 
 def main():
     run_polling()

6d738c66e595f7c56ce22d53f4f683caf6f9fe49
samueltwum1 <samueltwum1@gmail.com>
2020-06-26 13:00:33 +0000

    flesh out functions for checking label conformity
    Co-authored-by: Katleho Madisa <katleho.madisa47@gmail.com>

--- post-deployment/tests/smoke/test_verify_devices_obsState_attribute.py
+++ post-deployment/tests/smoke/test_verify_devices_obsState_attribute.py
@@ -3,13 +3,43 @@ The LMC base classes now implement a state machine with changes to the enum labe
 of the obsState attribute. This test ensures that all devices have the same labels
 and values
 """
-from tango import Database, DeviceProxy
 import pytest
+from tango import Database, DeviceProxy
 
-def _is_enum_valid(device_name):
+# obsState enums as defined in lmc base class control model
+obs_state_enums = ["IDLE", "CONFIGURING", "READY", "SCANNING", "PAUSED", "ABORTED", "FAULT"]
+
+def _is_label_conformant(enum_labels):
+    label_order_correctness = []
+    for idx, label in enumerate(enum_labels):
+        try:
+            true_index = obs_state_enums.index(label)
+        except ValueError:
+            return False
+        else:
+            label_order_correctness.append(true_index==idx)
+    return all(label_order_correctness)
+
+
+def is_enum_valid(device_name):
     dp = DeviceProxy(device_name)
-    # do some checks
-    return
+    attr_list = dp.get_attribute_list()
+
+    # skip all devices w/o obsState attribute
+    if "obsState" not in attr_list:
+        return True
+
+    # skip checking for enums with less or more values
+    enum_labels = dp.get_attribute_config("obsState").enum_labels
+    if len(enum_labels) != len(obs_state_enums):
+        return False
+
+    # remove all special charaters from labels
+    for idx, label in enumerate(enum_labels):
+        new_label = ''.join(char for char in label if char.isalnum())
+        enum_labels[idx] = new_label.upper()
+
+    return _is_label_conformant(enum_labels)
 
 @pytest.mark.fast
 def test_check_obs_state_enum():
@@ -22,7 +52,7 @@ def test_check_obs_state_enum():
         for val in devices_and_classes:
             # ignore classes and grab only devices which are not in the dserver domain
             if not(val.lower().startswith('dserver') or len(val.split("/"))==1):
-                if not _is_enum_valid(val):
+                if not is_enum_valid(val):
                     defaulting_devices.append(val)
 
     msg = f"Devices ({defaulting_devices} don't have conforming obsState enum labels/values"

200a08c1c12323fda8aede2a0be342a1ebd792f6
kmadisa <kmadisa@ska.ac.za>
2020-07-02 13:28:01 +0000

    Removing the verbose logging.
    Signed-off-by: kmadisa <kmadisa@ska.ac.za>

--- post-deployment/tests/smoke/test_verify_devices_obsState_attribute.py
+++ post-deployment/tests/smoke/test_verify_devices_obsState_attribute.py
@@ -75,7 +75,6 @@ def test_obs_state_attribute_enum_labels_are_the_same(device_enum_labels_map):
     for device, enum_labels in device_enum_labels_map.items():
         formatted_enum_labels = _remove_special_characters_from_enum_labels(enum_labels)
         enum_variations.add(tuple(formatted_enum_labels))
-        logging.info(f"Device: {device}, enum labels: {formatted_enum_labels}.")
 
     msg = f"ObsState enum labels varies for some devices. The enum variations: {enum_variations}"
     assert len(enum_variations) == 1, msg

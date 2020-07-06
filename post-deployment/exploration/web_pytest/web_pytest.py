aad56df665962f913a9d711502933f9dc8fc034a
Gerhard le Roux <gerhardlr@gmail.com>
2020-07-06 15:11:58 +0000

    change to handle cors

--- post-deployment/exploration/web_pytest/web_pytest.py
+++ post-deployment/exploration/web_pytest/web_pytest.py
@@ -6,6 +6,7 @@ from queue import Queue
 from aiojobs.aiohttp import setup, spawn, get_scheduler_from_app, get_scheduler_from_request
 from time import sleep
 from ska.logging import configure_logging
+import aiohttp_cors
 # app modules
 from exploration.web_pytest.test_executor import validate_it, validate_test_spec,run_test
 from exploration.web_pytest.async_threading import async_is_empty_on_queue, get_async_from_queue,AsyncThreadRunner
@@ -25,7 +26,7 @@ async def ping(request):
 @routes.post('/dev-testing/test')
 @validate_it(validate_test_spec)
 async def request_test(request):
-    LOGGER.info(f"test request received {request}")
+    LOGGER.info(f"test request received")
     test_spec = await request.json()
     await spawn(request, run_test(test_spec,request.app))
     return web.json_response(test_spec)
@@ -53,12 +54,25 @@ async def wait_until_tests_finished(app):
  # app creation
 
 
+
+# Configure CORS on all routes.
+
 def create_app():
     app = web.Application()
+    # Configure default CORS settings
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
+    app.add_routes(routes)
+    for route in list(app.router.routes()):
+        cors.add(route)
     runner = AsyncThreadRunner()
     runner.__init__()
     setup(app)
-    app.add_routes(routes)
     app['test_results'] = Queue()
     return app
 

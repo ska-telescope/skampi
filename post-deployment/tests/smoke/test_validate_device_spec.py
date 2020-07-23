2f9a589eaede0fd2b791147b8412411b177c32ae
SKAJohanVenter <aventer@ska.ac.za>
2020-07-23 11:18:09 +0200

    Updated test to use fixtures

--- post-deployment/tests/smoke/test_validate_device_spec.py
+++ post-deployment/tests/smoke/test_validate_device_spec.py
@@ -13,19 +13,27 @@ SPEC_URLS = {
     "dish_master": (
         "https://gitlab.com/ska-telescope/telescope-model/-/raw/"
         "master/spec/tango/dsh/DishMaster.yaml"
-    )
+    ),
 }
 
-SPECS_TO_CHECK = {"ska_tango_guide_ska_wide": ("mid_d0001/elt/master",),
-                  "dish_master": ("mid_d0001/elt/master",)}
+SPECS_TO_CHECK = {
+    "ska_tango_guide_ska_wide": ("mid_d0001/elt/master",),
+    "dish_master": ("mid_d0001/elt/master",),
+}
+
+
+@pytest.fixture(params=SPECS_TO_CHECK.items())
+def spec_devices(request):
+    return request.param
 
 
 @pytest.mark.fast
-def test_device_conforms_to_spec():
+def test_device_conforms_to_spec(spec_devices):
     """Run through all the specifications and the devices against which to test them"""
-    for spec, devices in SPECS_TO_CHECK.items():
-        for test_device in devices:
-            print("\nChecking device {} against {}".format(test_device, spec))
-            result = validate_device_from_url(test_device, SPEC_URLS[spec], False)
-            # Printing differences for now, rather than asserting
-            print("Differences:\n{}".format(result))
+    spec = spec_devices[0]
+    devices = spec_devices[1]
+    for test_device in devices:
+        print("\nChecking device {} against {}".format(test_device, spec))
+        result = validate_device_from_url(test_device, SPEC_URLS[spec], False)
+        # Printing differences for now, rather than asserting
+        print("Differences:\n{}".format(result))

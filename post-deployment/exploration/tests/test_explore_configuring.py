89a8b77a70aaba35e1962022e365a45803732d29
gerhard <gerhardlr@gmail.com>
2020-08-10 10:12:22 +0000

    example of using idle subarray fixture

--- post-deployment/exploration/tests/test_explore_configuring.py
+++ post-deployment/exploration/tests/test_explore_configuring.py
@@ -1,5 +1,6 @@
-from resources.test_support.event_waiting import watchSpec
-from resources.test_support.fixtures import running_telescope,resource_config_file,idle_subarray
+#from resources.test_support.event_waiting import watchSpec
+#from resources.test_support.fixtures import running_telescope,resource_config_file,idle_subarray
+#import resources.test_support.fixtures
 
 
 import logging
@@ -8,11 +9,6 @@ import pytest
 
 LOGGER = logging.getLogger(__name__)
 from oet.domain import SKAMid, SubArray, ResourceAllocation, Dish, ResourceAllocation
-
-specs = [
-    watchSpec('ska_mid/tm_subarray_node/1','obsState'),        
-    watchSpec('mid_csp/elt/subarray_01','obsState'),
-    watchSpec('mid_csp_cbf/sub_elt/subarray_01','obsState')]
 
 
 def test_configure_subarray(idle_subarray):

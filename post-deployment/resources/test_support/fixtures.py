bb6630045031080c089c7f20cd69422ecba17036
gerhard <gerhardlr@gmail.com>
2020-08-10 09:59:52 +0000

    k8 fixture that clean config db at every test run

--- post-deployment/resources/test_support/fixtures.py
+++ post-deployment/resources/test_support/fixtures.py
@@ -58,6 +58,64 @@ def set_telescope_to_standby() -> None:
         SKAMid().standby()
 
 ## pytest fixtures
+ENV_VARS = [
+    'HELM_RELEASE',
+    'KUBE_NAMESPACE',
+    'TANGO_HOST']
+RunContext = namedtuple('RunContext', ENV_VARS)
+
+@pytest.fixture(scope="session")
+def run_context():
+    logging.info('in run_context')
+     # list of required environment vars
+    values = list()
+    
+    for var in ENV_VARS:
+        assert os.environ.get(var) # all ENV_VARS must have values set
+        values.append(os.environ.get(var))
+
+    return RunContext(*values)
+
+class K8_env():
+    '''
+    An object to help with managing the k8 context in order to 
+    ensure tests are not effected by dirty environments
+    '''
+    def __init__(self, run_context:RunContext ) -> None:
+        _, active_context = config.list_kube_config_contexts()
+        config.load_kube_config(context=active_context['name'])
+        self.v1 = client.CoreV1Api()
+        self.extensions_v1_beta1 = client.ExtensionsV1beta1Api()
+        self.env = run_context
+        self.clean_config_etcd()
+
+
+    def clean_config_etcd(self) -> None:
+        exec_command = [ 'sdpcfg', 'delete', '-R','/'] 
+        component_name = 'maintenance-interface'
+        namespace = self.env.KUBE_NAMESPACE
+        pods = self.v1.list_namespaced_pod(namespace).items
+        pod = [p.metadata.name for p in pods if p.metadata.labels.get('component') == component_name]   
+        assert len(pod) > 0, f'error in cleaning config db: pod labeled as {component_name} not fpound'
+        assert len(pod) < 2, f'error in cleaning config db: duplicate pods labeled as {component_name} foound'
+        pod = pod[0]
+        resp = stream(self.v1.connect_get_namespaced_pod_exec, 
+                pod, 
+                namespace, 
+                command=exec_command, 
+                stderr=True, stdin=False, 
+                stdout=True, tty=False)  
+        logging.info(f'cleaning configdb:{resp}')
+
+
+@pytest.fixture(scope='session',autouse=True)
+def k8(run_context) -> None:
+    '''
+    An fixture to help with managing the k8 context in order to 
+    ensure tests are not effected by dirty environments
+    '''
+    logging.info('k8 fixture called')
+    yield  K8_env(run_context)
 @pytest.fixture
 def running_telescope() -> None:
     try:

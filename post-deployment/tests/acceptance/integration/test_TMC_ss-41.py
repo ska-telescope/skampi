70f20414e7497d39c8947b98b4eb7468aa5a2dbe
kalyanithigale <kalyanit.ska@gmail.com>
2020-08-14 06:52:42 +0000

    Enable skipped TMC integration test cases

--- post-deployment/tests/acceptance/integration/test_TMC_ss-41.py
+++ post-deployment/tests/acceptance/integration/test_TMC_ss-41.py
@@ -37,8 +37,8 @@ non_default_states_to_check = {
 
 LOGGER = logging.getLogger(__name__)
 @pytest.mark.select
-@pytest.mark.skip(reason="Fails randomly")
-@pytest.mark.skipif(DISABLE_TESTS_UNDER_DEVELOPMENT, reason="disabaled by local env")
+# @pytest.mark.skip(reason="Fails randomly")
+# @pytest.mark.skipif(DISABLE_TESTS_UNDER_DEVELOPMENT, reason="disabaled by local env")
 def test_multi_scan():
 
     ####

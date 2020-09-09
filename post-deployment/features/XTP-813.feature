6f02210de8c00c9785d6f842809eac69ca4d30c6
samueltwum1 <samueltwum1@gmail.com>
2020-09-09 20:42:26 +0200

    update feature file and corresponding test for XTP-813

--- post-deployment/features/XTP-813.feature
+++ post-deployment/features/XTP-813.feature
@@ -1,29 +1,19 @@
-Feature: TMC integration with Dish Software
+@VTS-226
+Feature: Dish simulator confidence test via TM-Dish interface
+	# The Dish Simulator behaviour is tested by invoking the commands of the Dish master TANGO
+    # device and verifying that dishMode transitions are as per the documentation.
+	# Scenario: Dish from dish_mode_a to dish_mode_b
 
-@XTP-813 @XTP-811
-Scenario: mid_d0001/elt/master from STANDBY-LP to STANDBY-FP
-    Given mid_d0001/elt/master reports STANDBY-LP Dish mode
-    When I command mid_d0001/elt/master to STANDBY-FP Dish mode
-    Then mid_d0001/elt/master reports STANDBY-FP Dish mode
-    And mid_d0001/elt/master is in STANDBY state
-	
-@XTP-813 @XTP-811
-Scenario: mid_d0001/elt/master from STANDBY-FP to OPERATE
-    Given mid_d0001/elt/master reports STANDBY-FP Dish mode
-    When I command mid_d0001/elt/master to OPERATE Dish mode
-    Then mid_d0001/elt/master reports OPERATE Dish mode
-    And mid_d0001/elt/master is in ON state
-
-@XTP-813 @XTP-811
-Scenario: mid_d0001/elt/master from OPERATE to STANDBY-FP
-    Given mid_d0001/elt/master reports OPERATE Dish mode
-    When I command mid_d0001/elt/master to STANDBY-FP Dish mode
-    Then mid_d0001/elt/master reports STANDBY-FP Dish mode
-    And mid_d0001/elt/master is in STANDBY state
-
-@XTP-813 @XTP-811
-Scenario: mid_d0001/elt/master from STANDBY-FP to STANDBY-LP
-    Given mid_d0001/elt/master reports STANDBY-FP Dish mode
-    When I command mid_d0001/elt/master to STANDBY-LP Dish mode
-    Then mid_d0001/elt/master reports STANDBY-LP Dish mode
-    And mid_d0001/elt/master is in STANDBY state
+	@XTP-813 @XTP-811
+	Scenario Outline: Test dish-lmc-prototype dishMode change
+		Given <dish_master> reports <start_mode> Dish mode
+		When I command <dish_master> to <end_mode> Dish mode
+		Then <dish_master> reports <end_mode> Dish mode
+		And <dish_master> is in <end_state> state
+		
+		Examples:
+		| dish_master             | start_mode | end_mode   | end_state |
+		| mid_d0001/elt/master    | STANDBY-LP | STANDBY-FP | STANDBY   |
+		| mid_d0001/elt/master    | STANDBY-FP | OPERATE    | ON        |
+		| mid_d0001/elt/master    | OPERATE    | STANDBY-FP | STANDBY   |
+		| mid_d0001/elt/master    | STANDBY-FP | STANDBY-LP | STANDBY   |
--- post-deployment/tests/acceptance/integration/test_XTP-813.py
+++ post-deployment/tests/acceptance/integration/test_XTP-813.py
@@ -17,7 +17,6 @@ from resources.test_support.helpers import resource, watch
 
 
 LOGGER = logging.getLogger(__name__)
-scenario = partial(scenario, "XTP-813.feature")
 
 mode_cmd_map = {
     "STANDBY-LP": "SetStandbyLPMode",
@@ -27,20 +26,25 @@ mode_cmd_map = {
 # to be used in teardown
 device_proxies = {}
 
+
 def _change_dish_mode(dev_proxy, cmd, device_name):
     getattr(dev_proxy, cmd)()
     watch_dish_mode = watch(resource(device_name)).for_a_change_on('dishMode')
     watch_dish_mode.wait_until_value_changed()
 
+
 def pre_condition(dev_proxy, device_name, expected):
     """verify the device dish mode before executing mode transition requests"""
     actual = resource(device_name).get('dishMode')
     if actual != expected:
-        # standbyfp mode is used as initial condition since it can be reached from other dish modes
-        _change_dish_mode(dev_proxy, 'SetStandbyFPMode', device_name)
+        # standbyfp is used as initial condition because it can be reached from other
+        # dish modes but dont request standbyfp if this is the current dish mode
+        if actual != "STANDBY-FP":
+            _change_dish_mode(dev_proxy, 'SetStandbyFPMode', device_name)
         _change_dish_mode(dev_proxy, mode_cmd_map[expected], device_name)
     assert_that(resource(device_name).get('dishMode')).is_equal_to(expected)
-    LOGGER.info(device_name + ' initial dishMode: ' + resource(device_name).get('dishMode'))
+    LOGGER.info(f"{device_name} initial dishMode: {resource(device_name).get('dishMode')}")
+
 
 @pytest.fixture(autouse=True, scope="module")
 def restore_dish_state(request):
@@ -55,56 +59,37 @@ def restore_dish_state(request):
 
     request.addfinalizer(put_dish_in_standby_fp_mode)
 
-# mid_d0001/elt/master
-@pytest.mark.xfail
-@pytest.mark.xfail(reason="SetStandbyLPMode not allowed when the device is in STANDBY state")
-@scenario("mid_d0001/elt/master from STANDBY-LP to STANDBY-FP")
-def test_mid_d0001_from_standbylp_to_standbyfp_mode():
-    pass
-
 
 @pytest.mark.fast
-@scenario("mid_d0001/elt/master from STANDBY-FP to OPERATE")
-def test_mid_d0001_from_standbyfp_to_operate_mode():
+@scenario("XTP-813.feature", "Test dish-lmc-prototype dishMode change")
+def test_mode_transitions():
     pass
 
 
-@pytest.mark.fast
-@scenario("mid_d0001/elt/master from OPERATE to STANDBY-FP")
-def test_mid_d0001_from_operate_to_standbyfp():
-    pass
-
-@pytest.mark.xfail(reason="SetStandbyLPMode not allowed when the device is in STANDBY state")
-@scenario("mid_d0001/elt/master from STANDBY-FP to STANDBY-LP")
-def test_mid_d0001_from_standbyfp_to_standbylp():
-    pass
-
-
-# define steps for scenario
-@given(parsers.parse("{device_name} reports {expected} Dish mode"))
-def device_proxy(device_name, expected):
+@given(parsers.parse("<dish_master> reports <start_mode> Dish mode"))
+def device_proxy(dish_master, start_mode):
     # update the device_proxies collection for teardown
-    if device_name not in device_proxies:
-        dev_proxy = DeviceProxy(device_name)
-        device_proxies[device_name] = dev_proxy
+    if dish_master not in device_proxies:
+        dev_proxy = DeviceProxy(dish_master)
+        device_proxies[dish_master] = dev_proxy
 
-    pre_condition(device_proxies[device_name], device_name, expected)
-    return device_proxies[device_name]
-
-
-@when(parsers.parse("I command {device_name} to {requested} Dish mode"))
-def set_dish_mode(device_name, requested, device_proxy):
-    _change_dish_mode(device_proxy, mode_cmd_map[requested], device_name)
-    LOGGER.info(device_name + ' requested dishMode: ' + resource(device_name).get('dishMode'))
+    pre_condition(device_proxies[dish_master], dish_master, start_mode)
+    return device_proxies[dish_master]
 
 
-@then(parsers.parse("{device_name} reports {desired} Dish mode"))
-def check_dish_mode(device_name, desired):
-    assert_that(resource(device_name).get('dishMode')).is_equal_to(desired)
-    LOGGER.info(device_name + ' desired dishMode: ' + resource(device_name).get('dishMode'))
+@when(parsers.parse("I command <dish_master> to <end_mode> Dish mode"))
+def set_dish_mode(device_proxy, dish_master, end_mode):
+    _change_dish_mode(device_proxy, mode_cmd_map[end_mode], dish_master)
+    LOGGER.info(f"{dish_master} requested dishMode: {end_mode}")
 
 
-@then(parsers.parse("{device_name} is in {desired} state"))
-def check_master_device_state(device_name, desired):
-    assert_that(resource(device_name).get('State')).is_equal_to(desired)
-    LOGGER.info(device_name + ' desired state: ' + resource(device_name).get('State'))
+@then(parsers.parse("<dish_master> reports <end_mode> Dish mode"))
+def check_dish_mode(dish_master, end_mode):
+    assert_that(resource(dish_master).get('dishMode')).is_equal_to(end_mode)
+    LOGGER.info(f"{dish_master} desired dishMode: {resource(dish_master).get('dishMode')}")
+
+
+@then(parsers.parse("<dish_master> is in <end_state> state"))
+def check_master_device_state(dish_master, end_state):
+    assert_that(resource(dish_master).get('State')).is_equal_to(end_state)
+    LOGGER.info(f"{dish_master} desired state: {resource(dish_master).get('State')}")

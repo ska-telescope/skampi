4bbc39edca0f94ce995421318a9a31b6d8f1bbde
Adriaan de Beer <adriaan@spaceadvisory.com>
2020-09-13 17:47:58 +0200

    ST-473 Transferred Make target to shell script

---
+++ scripts/publish-charts.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# create clean repo cache dir
+if [ -d "chart-repo-cache" ]
+  then
+  rm -rf chart-repo-cache
+fi
+mkdir chart-repo-cache
+
+# add SKA Helm Repository
+helm repo add skatelescope $HELM_HOST/chart-repo-cache/helm-chart --chart-repo-cache-cache chart-repo-cache
+helm repo list
+helm repo update
+helm search repo skatelescope
+
+# Package charts
+for chart in charts/*/
+do
+  echo "$chart"
+  helm package "$chart" --destination chart-repo-cache
+done
+
+# rebuild index
+helm repo index chart-repo-cache --merge chart-repo-cache/cache/skatelescope-index.yaml
+for file in chart-repo-cache/*.tgz; do
+  echo "######### uploading ${file##*/}";
+  curl -v -u $HELM_USERNAME:$HELM_PASSWORD --upload-file ${file} $HELM_HOST/chart-repo-cache/helm-chart/${file##*/}; \
+done
+curl -v -u $HELM_USERNAME:$HELM_PASSWORD --upload-file chart-repo-cache/index.yaml $HELM_HOST/chart-repo-cache/helm-chart/${file##*/}; \
+
+helm search repo skatelescope >> chart-repo-cache/before
+helm repo update
+helm search repo skatelescope >> chart-repo-cache/after
+
+echo "This publishing step brought about the following changes:"
+diff chart-repo-cache/before chart-repo-cache/after --color
+
+rm -rf chart-repo-cache

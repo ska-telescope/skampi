93ba5d70a5eafc8719af4d502462963862cc00cc
Adriaan de Beer <adriaan@spaceadvisory.com>
2020-09-16 23:56:46 +0200

    ST-473 trying emoji

--- scripts/publish-charts.sh
+++ scripts/publish-charts.sh
@@ -40,7 +40,7 @@ helm repo index ./chart-repo-cache --merge ./chart-repo-cache/skatelescope-index
 # check for pre-existing files
 for file in $(cd chart-repo-cache; ls *.tgz); do
   echo "Checking if $file is already in index:"
-  cat ./chart-repo-cache/skatelescope-index.yaml | grep "$file" && echo "#########################################" || echo "Not found in index 👍";
+  cat ./chart-repo-cache/skatelescope-index.yaml | grep "$file" && echo "⚠️ #########################################" || echo "Not found in index 👍";
 done
 
 sleep 2

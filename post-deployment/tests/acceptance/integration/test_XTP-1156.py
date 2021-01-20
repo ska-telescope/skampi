882b1b84dad4c63094f4ae2c81a2d2023335e7f3
Liz <liz.bartlett@stfc.ac.uk>
2021-01-20 14:59:03 +0000

    marked test as 'common'

--- post-deployment/tests/acceptance/integration/test_XTP-1156.py
+++ post-deployment/tests/acceptance/integration/test_XTP-1156.py
@@ -19,7 +19,7 @@ OET_ENV["OET_REST_URI"] = f"http://oet-rest-{HELM_RELEASE}:5000/api/v1.0/procedu
 
 
 @pytest.mark.fast
-@pytest.mark.skamid
+@pytest.mark.common
 @scenario("XTP-1156.feature", "Observation Execution Tool")
 def test_create():
     pass

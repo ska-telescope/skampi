482c7e17913799d29d8fc6c93f3f1ed04362899f
payalgupta <payal.ska@gmail.com>
2021-02-16 13:30:42 +0000

    low-abort-obsreset-bdd: Update abort bdd test case and update assign, configure and scan json string.

--- post-deployment/resources/test_support/tmc_helpers_low.py
+++ post-deployment/resources/test_support/tmc_helpers_low.py
@@ -1,4 +1,4 @@
-from resources.test_support.sync_decorators_low import sync_start_up_telescope,sync_assign_resources,sync_configure, sync_scan, sync_end, sync_abort, sync_release_resources,sync_set_to_standby,time_it
+from resources.test_support.sync_decorators_low import sync_start_up_telescope,sync_assign_resources,sync_configure, sync_scan, sync_end, sync_abort, sync_obsreset, sync_release_resources,sync_set_to_standby,time_it
 from resources.test_support.logging_decorators import log_it
 from tango import DeviceProxy   
 from resources.test_support.helpers_low import waiter,watch,resource
@@ -80,8 +80,10 @@ def configure_sub():
 
 @sync_scan
 def scan_sub():
+    scan_file = 'resources/test_data/TMC_integration/mccs_scan.json'
+    scan_string = load_config_from_file(scan_file)
     SubarrayNodeLow = DeviceProxy('ska_low/tm_subarray_node/1')
-    SubarrayNodeLow.Scan('{"id":1}')
+    SubarrayNodeLow.Scan(scan_string)
     LOGGER.info('Scan Started')
 
 @sync_abort
@@ -92,6 +94,7 @@ def abort_sub():
     
 @sync_obsreset
 def obsreset_sub():
+    resource('ska_low/tm_subarray_node/1').assert_attribute('obsState').equals('ABORTED')
     SubarrayNodeLow = DeviceProxy('ska_low/tm_subarray_node/1')
     SubarrayNodeLow.ObsReset()
     LOGGER.info("Subarray obsState is: " + str(SubarrayNodeLow.obsState))

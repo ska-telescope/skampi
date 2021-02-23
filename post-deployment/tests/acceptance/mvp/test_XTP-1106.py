3104dfd7288f4cc466621374f1623a46f5042e03
Viivi Pursiainen <viivi.pursiainen@stfc.ac.uk>
2021-02-23 18:42:01 +0000

    AT2-599 OET domain object refactor

--- post-deployment/tests/acceptance/mvp/test_XTP-1106.py
+++ post-deployment/tests/acceptance/mvp/test_XTP-1106.py
@@ -15,7 +15,7 @@ from pytest_bdd import scenario, given, when, then
 
 
 #SUT
-from ska.scripting.domain import SKAMid, SubArray, ResourceAllocation, Dish
+from ska.scripting.domain import Telescope, SubArray
 #SUT infrastructure
 from tango import DeviceProxy, DevState
 ## local imports

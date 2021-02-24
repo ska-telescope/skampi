38f48c9d273bd75300bdbccf6122e9c9c2353653
Gerhard le Roux <gerhardlr@gmail.com>
2021-02-24 19:44:03 +0000

    add test checking landing page loads

---
+++ post-deployment/tests/smoke/test_landing_page_loads.py
@@ -0,0 +1,13 @@
+import requests
+import pytest
+import os
+from assertpy import assert_that
+
+@pytest.fixture(name='url')
+def fxt_url():
+    namespace = os.environ.get('KUBE_NAMESPACE','integration')
+    return f'http://kubernetes.engageska-portugal.pt/{namespace}/start/'
+
+
+def test_landing_page_loads(url):
+    assert_that(requests.get(url).status_code).is_equal_to(200)

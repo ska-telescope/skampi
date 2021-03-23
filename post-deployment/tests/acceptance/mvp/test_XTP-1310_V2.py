31451c33bfc2140c008f5d8809699d971847167a
Ursula Badenhorst <ubadenhorst@indulec.co.za>
2021-03-23 08:40:51 +0000

    Update post-deployment/tests/acceptance/mvp/test_XTP-1310_V2.py

--- post-deployment/tests/acceptance/mvp/test_XTP-1310_V2.py
+++ post-deployment/tests/acceptance/mvp/test_XTP-1310_V2.py
@@ -8,25 +8,53 @@ from pytest_bdd import (
     then,
     when,
 )
+# Create a shortcut for ON and OFF states
+ON = tango._tango.DevState.ON
+OFF = tango._tango.DevState.OFF
+
+# Create a list holding the target devices in the control chain
+ALL_DEVICES = [
+    tmc_central_node,
+    mccs_controller,
+    mccs_station_001,
+    mccs_tile_0001
+]
+
+# Define a function to print the state of all devices
+def print_device_states():
+    for device in ALL_DEVICES:
+        device_name = str(device).split('(')[0]
+        device_state = device.State()
+        print(f'{device_name}: {device_state}')
+
+
+
+
+
+
+
 @pytest.mark.singlerun
 @scenario('XTP-1310.feature', 'PSI0.1 test, Initialise the TPM using the OET (Jupyter Notebook)')
 def test_psi01_test_initialise_the_tpm_using_the_oet_jupyter_notebook():
     """PSI0.1 test, Initialise the TPM using the OET (Jupyter Notebook)."""
-@given('subsystems <subsystem-list> are ONLINE and in the Tango Device OFF state')
-def subsystems_are_online_and_in_the_tango_device_off_state(subsystem_list):
+@given('subsystems are ONLINE (with Tango Device in OFF state,except MccsTile in the DISABLE or OFF state)')
+def subsystems_are_online_and_in_the_tango_device_off_state():
     """subsystems <subsystem-list> are ONLINE and in the Tango Device OFF state."""
-    raise NotImplementedError
+   
+    for device in ALL_DEVICES:
+        assert device.State() is OFF, f'{device} is not in OFF state'
+    print_device_states()
+
+
+
 @given('the TPM_HW is powered ON and in the IDLE state')
 def the_tpm_hw_is_powered_on_and_in_the_idle_state():
     """the TPM_HW is powered ON and in the IDLE state."""
     raise NotImplementedError
-@given('the Tile Tango Device is ONLINE and in the DISABLE state')
-def the_tile_tango_device_is_online_and_in_the_disable_state():
-    """the Tile Tango Device is ONLINE and in the DISABLE state."""
-    raise NotImplementedError
-@when('I send the command <command> to the TMC')
-def i_send_the_command_to_the_tmc(command):
-    """I send the command <command> to the TMC."""
+
+@when('I send the command ON to the TMC')
+def i_send_the_command_to_the_tmc():
+    """I send the command to the TMC."""
     raise NotImplementedError
 @then('the TPM_HW is in the WORKING state')
 def the_tpm_hw_is_in_the_working_state():
@@ -36,7 +64,7 @@ def the_tpm_hw_is_in_the_working_state():
 def the_tpm_hw_will_be_programmed_and_initialized():
     """the TPM_HW will be programmed and initialized."""
     raise NotImplementedError
-@then('the state and the temperature of the TPM_HW can be monitored.')
+@then('the state and the temperature of the TPM_HW can be monitored')
 def the_state_and_the_temperature_of_the_tpm_hw_can_be_monitored():
     """the state and the temperature of the TPM_HW can be monitored.."""
     raise NotImplementedError

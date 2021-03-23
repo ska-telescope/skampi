72056a40c336088807353593d5545ed67c497c43
Gerhard le Roux <3607905-gerhardlr@users.noreply.gitlab.com>
2021-03-23 16:02:08 +0000

    Update post-deployment/tests/acceptance/mvp/test_XTP-1310_V2.py

--- post-deployment/tests/acceptance/mvp/test_XTP-1310_V2.py
+++ post-deployment/tests/acceptance/mvp/test_XTP-1310_V2.py
@@ -11,27 +11,37 @@ from pytest_bdd import (
 import tango
 import time  # used to sleep between measurements
 from tango import DeviceProxy
+
 class DeviceStates:
-    pass
+    
+    def __init__(self):
+        # create device proxy to TMC CentralNode
+        self.tmc_central_node = DeviceProxy('ska_low/tm_central/central_node')
+
+        # create device proxy to MCCS controller, station, and tile
+        self.mccs_controller = DeviceProxy('low-mccs/control/control')
+        self.mccs_station_001 = DeviceProxy('low-mccs/station/001')
+        self.mccs_tile_0001 = DeviceProxy('low-mccs/tile/0001')
+        # Create a list holding the target devices in the control chain
+        self.ALL_DEVICES = [
+            self.tmc_central_node,
+            self.mccs_controller,
+            self.mccs_station_001,
+            self.mccs_tile_0001
+        ]
+
+    def print_device_states():
+        for device self.ALL_DEVICES:
+            device_name = str(device).split('(')[0]
+            device_state = device.State()
+            print(f'{device_name}: {device_state}')
+
 
 @pytest.fixture
 def devices()-> DeviceStates:
-    device_states = DeviceStates()
-    # create device proxy to TMC CentralNode
-    device_states.tmc_central_node = DeviceProxy('ska_low/tm_central/central_node')
 
-    # create device proxy to MCCS controller, station, and tile
-    device_states.mccs_controller = DeviceProxy('low-mccs/control/control')
-    device_states.mccs_station_001 = DeviceProxy('low-mccs/station/001')
-    device_states.mccs_tile_0001 = DeviceProxy('low-mccs/tile/0001')
-    # Create a list holding the target devices in the control chain
-    device_states.ALL_DEVICES = [
-        device_states.tmc_central_node,
-        device_states.mccs_controller,
-        device_states.mccs_station_001,
-        device_states.mccs_tile_0001
-    ]
-    return device_states
+
+    return DeviceStates()
 
 # Create a shortcut for ON and OFF states
 ON = tango._tango.DevState.ON
@@ -39,11 +49,7 @@ OFF = tango._tango.DevState.OFF
 DISABLE = tango._tango.DevState.DISABLE
 
 # Define a function to print the state of all devices
-def print_device_states(devices):
-    for device in devices.ALL_DEVICES:
-        device_name = str(device).split('(')[0]
-        device_state = device.State()
-        print(f'{device_name}: {device_state}')
+
 
 @pytest.mark.singlerun
 @scenario('XTP-1310.feature', 'PSI0.1 test, Initialise the TPM using the OET (Jupyter Notebook)')
@@ -57,7 +63,7 @@ def subsystems_are_online_and_in_the_tango_device_off_state(devices):
     assert devices.mccs_controller.State() is OFF, f'mccs_controller is not in OFF state'
     assert devices.mccs_station_001.State() is OFF, f'mccs_station_001 is not in OFF state'
     assert devices.mccs_tile_0001.State() in [OFF,DISABLE], f'mccs_tile_0001 is not in OFF or DISABLE state'
-    print_device_states()
+    devices.print_device_states()
 
 @given('the TPM_HW is powered ON and in the IDLE state')
 def the_tpm_hw_is_powered_on_and_in_the_idle_state():
@@ -85,7 +91,7 @@ def the_state_and_the_temperature_of_the_tpm_hw_can_be_monitored(devices):
     """the state and the temperature of the TPM_HW can be monitored.."""
     if devices.mccs_tile_0001.simulationmode == 1:
         print ('MCCS tile 0001 is in simulation mode')
-    print_device_states()
+    devices.print_device_states()
 
     device = devices.mccs_tile_0001
 

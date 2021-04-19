d82979d35ca8c8c51b660f2fd83d065cac3ebcef
Gerhard le Roux <gerhardlr@gmail.com>
2021-04-19 18:07:03 +0200

    updates

--- post-deployment/tests/smoke/test_SKB_17.py
+++ post-deployment/tests/smoke/test_SKB_17.py
@@ -1,6 +1,6 @@
 from typing import Iterator
 import pytest
-from tango import DeviceProxy,EventType
+from tango import DeviceProxy,EventType # type: ignore
 from queue import Queue
 from assertpy import assert_that
 

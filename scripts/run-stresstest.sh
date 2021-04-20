5ff90e921780474b021987426f6770e0b4340fdf
Peter Wortmann <p.wortmann@skatelescope.org>
2021-04-20 16:16:41 +0000

    DPPT-23 Initial attempt to run stress test in CI

---
+++ scripts/run-stresstest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+END_TIME=$1
+i=0
+while (( `date +%s` < $END_TIME )); do
+    
+    export KUBE_NAMESPACE=$KUBE_NAMESPACE_PREFIX-$i
+    export KUBE_NAMESPACE_SDP=$KUBE_NAMESPACE_PREFIX-$i-sdp
+
+    echo === Iteration $i (namespaces $KUBE_NAMESPACE / $KUBE_NAMESPACE_SDP) ===
+    curl -s https://gitlab.com/ska-telescope/templates-repository/-/raw/master/scripts/namespace_auth.sh | bash -s $SERVICE_ACCOUNT $KUBE_NAMESPACE $KUBE_NAMESPACE_SDP    
+    kubectl delete namespace $KUBE_NAMESPACE $KUBE_NAMESPACE_SDP
+    kubectl create namespace $KUBE_NAMESPACE &&
+        kubectl create namespace $KUBE_NAMESPACE_SDP && \
+        make install && \
+        make smoketest && \
+        make k8s_test
+    sleep 1
+
+    # Move logs
+    mkdir -p logs
+    mv build logs/$i
+    
+    kubectl delete namespace $KUBE_NAMESPACE $KUBE_NAMESPACE_SDP &
+    i=$((i + 1))
+done

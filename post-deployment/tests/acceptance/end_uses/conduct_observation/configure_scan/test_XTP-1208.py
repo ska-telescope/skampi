6fd6611f7680ba20182e0de8a5d0f6e9d2eaf9d4
HandeSnehal <snehalhande.ska@gmail.com>
2021-04-30 07:17:11 +0000

    AT1-870 Update oet api for assign and configure in 1208 bdd test case

--- post-deployment/tests/acceptance/end_uses/conduct_observation/configure_scan/test_XTP-1208.py
+++ post-deployment/tests/acceptance/end_uses/conduct_observation/configure_scan/test_XTP-1208.py
@@ -20,7 +20,7 @@ import json
 from resources.test_support.helpers_low import resource, watch, waiter, wait_before_test
 from resources.test_support.persistance_helping import update_scan_config_file
 from resources.test_support.sync_decorators_low import sync_configure
-from resources.test_support.controls_low import set_telescope_to_standby,set_telescope_to_running,telescope_is_in_standby,restart_subarray
+from resources.test_support.controls_low import set_telescope_to_standby,set_telescope_to_running,telescope_is_in_standby,restart_subarray, to_be_composed_out_of, configure_by_file
 import pytest
 from resources.test_support.tmc_helpers_low import compose_sub, configure_sub, release_resources, end
 
@@ -61,27 +61,27 @@ def start_up():
     wait_before_test(timeout=10)
     LOGGER.info("Telescope is in ON state")
 
-# @given("Subarray is in IDLE state")
-# def assign(result):
-#     LOGGER.info("Allocating resources to Low Subarray 1")
-#     compose_sub()
-#     LOGGER.info("Subarray 1 is ready")
+@given("Subarray is in IDLE state")
+def assign(result):
+    LOGGER.info("Allocating resources to Low Subarray 1")
+    to_be_composed_out_of()
+    LOGGER.info("Subarray 1 is ready")
 
-# @when("I call the configure scan execution instruction")
-# def config(result):
-#     def test_SUT():
-#         configure_sub()
-#     test_SUT()
-#     LOGGER.info("Configure command on Subarray 1 is successful")
+@when("I call the configure scan execution instruction")
+def config(result):
+    def test_SUT():
+        configure_by_file()
+    test_SUT()
+    LOGGER.info("Configure command on Subarray 1 is successful")
 
-# @then("Subarray is in READY state for which subsequent scan commands can be directed to deliver a basic imaging outcome")
-# def check_state():
-#     LOGGER.info("Checking the results")
-#     # check that the TMC report subarray as being in the obsState = READY
-#     assert_that(resource('ska_low/tm_subarray_node/1').get('obsState')).is_equal_to('READY')
-#     # check that the MCCS report subarray as being in the obsState = READY
-#     assert_that(resource('low-mccs/subarray/01').get('obsState')).is_equal_to('READY')
-#     LOGGER.info("Results OK")
+@then("Subarray is in READY state for which subsequent scan commands can be directed to deliver a basic imaging outcome")
+def check_state():
+    LOGGER.info("Checking the results")
+    # check that the TMC report subarray as being in the obsState = READY
+    assert_that(resource('ska_low/tm_subarray_node/1').get('obsState')).is_equal_to('READY')
+    # check that the MCCS report subarray as being in the obsState = READY
+    assert_that(resource('low-mccs/subarray/01').get('obsState')).is_equal_to('READY')
+    LOGGER.info("Results OK")
 
 
 def teardown_function(function):

ae9a5cfb12bee6b24508708c394baff51d5c0a14
DigvijayMali92 <digvijay.ska@gmail.com>
2021-05-03 09:08:47 +0000

    AT1-860-Remove unwanted code

--- post-deployment/resources/test_support/persistance_helping.py
+++ post-deployment/resources/test_support/persistance_helping.py
@@ -12,26 +12,6 @@ from os.path import dirname, join
 from ska_ser_skuid.client import SkuidClient
 LOGGER = logging.getLogger(__name__)
 
-def inc_from_old_nr(oldnr,incremental=1,disable_logging=False):
-    #assumes trailing 5 digits is an integer counter unless succeeded with a dash and a non digit
-    #also assumes we wont get increments hgher than 50 000 in a day
-    # TODO : Commenting this logic to generate the number inc by 1
-    if not disable_logging:
-        LOGGER.info("Old nr:" + str(oldnr))
-    # with 1 increment in id value
-    # inc =  int(re.findall(r'\d{5}(?=$|-\D)',oldnr)[0])
-    # LOGGER.info("Last 5 digits of ID:" + str(inc))  
-    # old_inc = '{:05d}'.format(inc+incremental)
-    # LOGGER.info("With inc by 1 logic updated increamented ID: " + str(old_inc))
-    # random.seed(datetime.now())
-    # new_inc = f'{choice(range(0,99999)):05d}'
-    # (dt, micro) = datetime.utcnow().strftime('%S.%f').split('.')
-    # new_inc = "%s%03d" % (dt, int(micro) / 1000)
-    new_inc = datetime.utcnow().strftime('%M%3S')
-    if not disable_logging:
-        LOGGER.info("With random generation logic updated ID:" + str(re.sub(r'\d{5}(?=$|-\D)',new_inc,oldnr)))
-    return re.sub(r'\d{5}(?=$|-\D)',new_inc,oldnr)
-
 def update_file(file):
     import os 
     try:
@@ -68,8 +48,8 @@ def update_resource_config_file(file,disable_logging=False):
     data["sdp"]["id"] = sb_id
     if "processing_blocks" in data["sdp"]:
         for i in range(len(data["sdp"]["processing_blocks"])):
-            sb_id = client.fetch_skuid("pb")
-            data["sdp"]["processing_blocks"][i]["id"] = sb_id
+            pb_id = client.fetch_skuid("pb")
+            data["sdp"]["processing_blocks"][i]["id"] = pb_id
             if "dependencies" in data["sdp"]["processing_blocks"][i]:
                 if i == 0:
                     data["sdp"]["processing_blocks"][i]["dependencies"][0]["pb_id"] = \
@@ -78,23 +58,6 @@ def update_resource_config_file(file,disable_logging=False):
                     data["sdp"]["processing_blocks"][i]["dependencies"][0]["pb_id"] = \
                     data["sdp"]["processing_blocks"][i - 1]["id"]
     LOGGER.info(data)
-    # data['sdp']['id'] = inc_from_old_nr(data['sdp']['id'],disable_logging=disable_logging)
-    # #assumes index nrs are following inbrokenly from loweest nr to highest nr in the list
-    # #this means each indix needs to inc by their range = size of the list
-    # incremental = len(data['sdp']['processing_blocks'])
-    # for index,item in enumerate(data['sdp']['processing_blocks']):
-    #     if(index==0):
-    #         data['sdp']['processing_blocks'][index]['id'] = inc_from_old_nr(item['id'],incremental,disable_logging)
-    #         first_pb_id_num = data['sdp']['processing_blocks'][index]['id']
-    #         next_pb_id_num =  int(re.findall(r'\d{5}(?=$|-\D)',first_pb_id_num)[0])
-    #         if not disable_logging:
-    #             LOGGER.info("Last 5 digits of ID:" + str(next_pb_id_num))
-    #     else:
-    #         next_pb_id_num += 1
-    #         data['sdp']['processing_blocks'][index]['id'] =  re.sub(r'\d{5}(?=$|-\D)',str(next_pb_id_num).zfill(5),first_pb_id_num)
-    #     if 'dependencies' in item.keys():
-    #         for index2,item2 in enumerate(item['dependencies']):
-    #             data['sdp']['processing_blocks'][index]['dependencies'][index2]['pb_id'] = data['sdp']['processing_blocks'][0]['id']
     with open(file, 'w') as f:
         json.dump(data, f)
         #f.write(json.dump(data))

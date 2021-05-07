45d20eea2cd47ffa27c3bc001c0a351f382f83d1
Peter Wortmann <p.wortmann@skatelescope.org>
2021-05-07 18:51:16 +0100

    DPPT-23 Apply timeouts to all major actions while running stress tests
    We sometimes lose entire sets of logs due to the tests getting
    stuck. Let's just kill the run if it takes too long.

--- scripts/run-stresstest.sh
+++ scripts/run-stresstest.sh
@@ -13,9 +13,9 @@ while (( `date +%s` < $END_TIME )); do
     kubectl delete namespace $KUBE_NAMESPACE $KUBE_NAMESPACE_SDP
     kubectl create namespace $KUBE_NAMESPACE &&
         kubectl create namespace $KUBE_NAMESPACE_SDP && \
-        make install && \
-        make smoketest && \
-        (make k8s_test | grep --line-buffered -E "xfailed|PASSED|FAILED|XFAIL|XPASS|ERROR|\.py::")
+        timeout 1m make install && \
+        timeout 5m make smoketest && \
+        (timeout 10m make k8s_test | grep --line-buffered -E "xfailed|PASSED|FAILED|XFAIL|XPASS|ERROR|\.py::")
 
     # Move logs
     mkdir -p logs

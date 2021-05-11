3b7e5be246c40d77059488779204c3183b987c9b
shraddhabajare <shraddhab.ska@gmail.com>
2021-05-11 13:28:05 +0000

    AT1-880 Address review comments

--- post-deployment/resources/test_support/controls_low.py
+++ post-deployment/resources/test_support/controls_low.py
@@ -52,8 +52,13 @@ def telescope_is_in_standby():
     LOGGER.info('resource("ska_low/tm_subarray_node/1").get("State")'+ str(resource('ska_low/tm_subarray_node/1').get("State")))
     LOGGER.info('resource("ska_low/tm_leaf_node/mccs_master").get("State")' +
                 str(resource('ska_low/tm_leaf_node/mccs_master').get("State")))
+    LOGGER.info('resource("low-mccs/control/control").get("State")' +
+                str(resource('low-mccs/control/control').get("State")))
     return  [resource('ska_low/tm_subarray_node/1').get("State"),
-            resource('ska_low/tm_leaf_node/mccs_master').get("State")] == ['OFF','OFF']
+            resource('ska_low/tm_leaf_node/mccs_master').get("State"),
+            resource('low-mccs/control/control').get("State")] == \
+            ['OFF','OFF', 'OFF']
+
 
 def set_telescope_to_running(disable_waiting = False):
     resource('ska_low/tm_subarray_node/1').assert_attribute('State').equals('OFF')
@@ -128,5 +133,3 @@ def restart_subarray_low(id):
     if exceptions_raised != "":
         raise Exception(f'Error in initialising devices:{exceptions_raised}')
     the_waiter.wait()
-
-

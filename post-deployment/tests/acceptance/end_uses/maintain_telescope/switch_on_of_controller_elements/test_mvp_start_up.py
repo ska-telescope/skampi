67e19583fbf75803157fb1e02e82caccf47bb833
Gerhard le Roux <gerhardlr@gmail.com>
2021-05-18 20:54:21 +0200

    updates for new release

--- post-deployment/tests/acceptance/end_uses/maintain_telescope/switch_on_of_controller_elements/test_mvp_start_up.py
+++ post-deployment/tests/acceptance/end_uses/maintain_telescope/switch_on_of_controller_elements/test_mvp_start_up.py
@@ -3,27 +3,22 @@ import pytest
 from types import SimpleNamespace
 import logging
 
-logger = logging.getLogger(__name__)
-
 from skallop.event_handling import builders
+from skallop.event_handling.occurences import Occurrences
+from skallop.subscribing.base import MessageBoardBase
 from skallop.mvp_control.describing import mvp_names
-from skallop.mvp_control.telescope import start_up as tel
+from skallop.mvp_fixtures.context import TelescopeContext
 from skallop.mvp_control.entry_points.base import EntryPoint
-from skallop.mvp_fixtures import telescope as tel_fxt
-from skallop.connectors import configuration as con_conf
 from skallop.mvp_control.event_waiting import wait
+
+logger = logging.getLogger(__name__)
 
 class Context(SimpleNamespace):
     pass
 
 @pytest.fixture(name='context')
-def fxt_context() -> Iterator[Any]:
-    context = Context()
-    # uncomment this if you want to test only local
-    ##
-    # context.mvp_stubbed=True
-    ##
-    yield context
+def fxt_context():
+    return Context()
 
 @pytest.fixture(name='devices')
 def fxt_devices()->List[str]:
@@ -34,48 +29,37 @@ def fxt_devices()->List[str]:
         devices = devices + mvp_names.Sensors(index).subtract('vcc')
     return devices.list
     
-@pytest.fixture(name='checking_transits_during_start_up')
-def fxt_checking_transits_during_start_up(devices: List[str]) -> Tuple[builders.Occurrences, builders.MessageBoardBuilder]:
 
-    builder = builders.MessageBoardBuilder()
+@pytest.fixture(name='transit_checking')
+def fxt_transit_checker(devices, standby_telescope: TelescopeContext)-> Tuple[Occurrences, MessageBoardBase]:
+    builder = builders.get_message_board_builder()
     central_node = mvp_names.Mid.tm.central_node
-    transitions = ['ON']
     checker = (
         builder.check_that(central_node)
-        .transits_according_to(transitions)
+        .transits_according_to(["ON"])
         .on_attr("state")
         .when_transit_occur_on(devices,ignore_first=True)
     )
-    return checker, builder
-
-@pytest.fixture(name='prepare_switch_on')
-def fxt_prepare_switch_on(running_telescope_args,
-    checking_transits_during_start_up: Tuple[builders.Occurrences, builders.MessageBoardBuilder],
-    context: Context,
-):
-
-    context.checker, builder = checking_transits_during_start_up
-    # with log.device_logging(builder,compose_args.devices_to_log):
-    with tel_fxt.tear_down_when_finished(running_telescope_args):
-        with wait.waiting_context(builder) as board:
-            context.board = board
-            yield
-
-
+    board  = standby_telescope.push_context_onto_test(wait.waiting_context(builder))
+    return checker, board
 
 
 @pytest.mark.skamid
-def test_start_up(prepare_switch_on,running_telescope_args:tel_fxt.RunningTelescopeSettings,context:Context, entry_point: EntryPoint):
-    checker: builders.Occurrences = context.checker
-    args = running_telescope_args
-    # when I start up the telescope
-    logger.info('setting telescope to running')
+def test_start_up(
+        transit_checking: Tuple[Occurrences, MessageBoardBase],
+        standby_telescope: TelescopeContext,
+        entry_point: EntryPoint
+    ):
+
+    checker, board = transit_checking
     entry_point.set_telescope_to_running()
-    checker: builders.Occurrences = context.checker
-    wait.wait(context.board, 15)
-    #logs = context.board.play_log_book()
-    #logger.info(f"Log messages during waiting:\n{logs}")
+    standby_telescope.state = "ON"
+    try:
+        wait.wait(board, 60, live_logging=False)
+    except wait.EWhilstWaiting as exception:
+        logs = board.play_log_book()
+        logger.info(f"Log messages during waiting:\n{logs}")
+        raise exception
     checking_logs = checker.print_outcome_for(checker.subject_device)
     logger.info(f"Results of checking:\n{checking_logs}")
     checker.assert_that(checker.subject_device).is_behind_all_on_transit("ON")
-    #checker.assert_that(checker.subject_device).is_behind_all_on_transit("IDLE")

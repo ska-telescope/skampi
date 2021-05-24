b0492a498fa2ca6ddd2e8e81367f4dac2aa5da29
Gerhard le Roux <gerhardlr@gmail.com>
2021-05-24 14:16:53 +0200

    wait for more devices

--- post-deployment/tests/acceptance/end_uses/maintain_telescope/switch_on_of_controller_elements/test_central_node_sync.py
+++ post-deployment/tests/acceptance/end_uses/maintain_telescope/switch_on_of_controller_elements/test_central_node_sync.py
@@ -8,6 +8,7 @@ from skallop.connectors.configuration import get_device_proxy
 from skallop.transactions.atomic import atomic
 
 
+
 @pytest.mark.skalow
 def test_central_node_sync():
     tmc_central_node = get_device_proxy('ska_low/tm_central/central_node')
@@ -15,10 +16,18 @@ def test_central_node_sync():
         state = tmc_central_node.State()
         event_state = event.attr_value.value
         assert_that(event_state).is_equal_to(state)
+
+    devices = [
+        'ska_low/tm_central/central_node',
+        'low-mccs/control/control',
+        'low-mccs/station/001',
+        'low-mccs/tile/0001'
+    ]
     tmc_central_node.subscribe_event('State',CHANGE_EVENT,callback)
-    with atomic('ska_low/tm_central/central_node','state','ON',5):
+    with atomic(devices,'state','ON',5):
             tmc_central_node.StartUpTelescope() #type: ignore
-    sleep(2)
+    
+    sleep(10)
     with atomic('ska_low/tm_central/central_node','state','OFF',5):
             tmc_central_node.StandByTelescope() #type: ignore
 

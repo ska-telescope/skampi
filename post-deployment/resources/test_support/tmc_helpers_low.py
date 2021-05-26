ba52730507b7ac30d7caf3a28af033c866647f78
samueltwum1 <samueltwum1@gmail.com>
2021-05-26 17:08:42 +0200

    SAR-213 Include release resources in examples

--- post-deployment/resources/test_support/tmc_helpers_low.py
+++ post-deployment/resources/test_support/tmc_helpers_low.py
@@ -44,8 +44,10 @@ def end():
 def release_resources():
     resource('ska_low/tm_subarray_node/1').assert_attribute('obsState').equals('IDLE')
     CentralNodeLow = DeviceProxy('ska_low/tm_central/central_node')
-    release_resources_file = 'resources/test_data/TMC_integration/mccs_release_resources.json'
+    release_resources_file = download_test_data(
+        "low_release_resources_v1.json", "skampi-test-data/tmc-integration/release-resources")
     release_json = load_config_from_file(release_resources_file)
+    os.remove(release_resources_file)
     CentralNodeLow.ReleaseResources(release_json)
     SubarrayNodeLow = DeviceProxy('ska_low/tm_subarray_node/1')
     LOGGER.info('After Invoking Release Resource on Subarray, SubarrayNodeLow State and ObsState:' + str(SubarrayNodeLow.State()) + str(SubarrayNodeLow.ObsState))

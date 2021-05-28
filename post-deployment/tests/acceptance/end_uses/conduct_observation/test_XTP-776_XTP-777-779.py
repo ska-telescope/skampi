805f2411264e7556be23a24b30b51dddd344db25
Abhijeet Deolalikar <abhijeet_deolalikar@persistent.com>
2021-05-28 12:39:24 +0000

    AT2-704 removed skuid module import

--- post-deployment/tests/acceptance/end_uses/conduct_observation/test_XTP-776_XTP-777-779.py
+++ post-deployment/tests/acceptance/end_uses/conduct_observation/test_XTP-776_XTP-777-779.py
@@ -20,7 +20,7 @@ from resources.test_support.controls import (set_telescope_to_running,
 
 from resources.test_support.helpers import resource
 from resources.test_support.oet_helpers import ScriptExecutor, Poller, Subarray
-from skuid.client import SkuidClient
+
 
 # used as labels within the oet_result fixture
 # this should be refactored at some point to something more elegant

e7eba2469b58a6b23e54a5f14b3ed77788448620
Viivi Pursiainen <viivi.pursiainen@stfc.ac.uk>
2021-05-28 13:52:32 +0100

    AT2-806 skip central node sync test

--- post-deployment/tests/acceptance/end_uses/maintain_telescope/switch_on_of_controller_elements/test_central_node_sync.py
+++ post-deployment/tests/acceptance/end_uses/maintain_telescope/switch_on_of_controller_elements/test_central_node_sync.py
@@ -30,6 +30,7 @@ def start_up_telescope(tmc_central_node):
 
 
 @pytest.mark.quarantine
+@pytest.mark.skip
 @pytest.mark.skalow
 def test_central_node_sync(context: StackableContext):
     tmc_central_node = get_device_proxy('ska_low/tm_central/central_node')

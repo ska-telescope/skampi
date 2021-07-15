46b8cc8f0da51e0c4b227f7611f25620a870bd62
Digvijay Mali <digvijay.ska@gmail.com>
2021-07-15 09:55:46 +0530

    AT1-949 Update controls file

--- post-deployment/resources/test_support/controls.py
+++ post-deployment/resources/test_support/controls.py
@@ -157,7 +157,7 @@ def set_telescope_to_standby():
 
     #It is observed that CSP and CBF subarrays sometimes take more than 8 sec to change the State to DISABLE
     #therefore timeout is given as 12 sec
-    the_waiter.wait(200)
+    the_waiter.wait(300)
     if the_waiter.timed_out:
         pytest.fail("timed out whilst setting telescope to standby:\n {}".format(the_waiter.logs))
 
@@ -173,7 +173,7 @@ def set_telescope_to_running(disable_waiting = False):
     CentralNode.TelescopeOn()
 
     if not disable_waiting:
-        the_waiter.wait(100)
+        the_waiter.wait(300)
         if the_waiter.timed_out:
             pytest.fail("timed out whilst starting up telescope:\n {}".format(the_waiter.logs))
 

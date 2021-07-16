3f8124e3284f3d6bbfc7d457f320c1aa536fa38e
Digvijay Mali <digvijay.ska@gmail.com>
2021-07-16 12:14:07 +0530

    AT1-949 Add log statements in mvp clean test file.

--- post-deployment/tests/smoke/test_mvp_clean.py
+++ post-deployment/tests/smoke/test_mvp_clean.py
@@ -34,9 +34,67 @@ def test_smell_mvp(pre_or_post="#PRE"):
     LOGGER.info(f'Current state of the MVP:{header+aggegate_output}')
     LOGGER.info("Testing only for equality, omitting SDP states for now")
 
+    LOGGER.info(
+        'resource("ska_mid/tm_central/central_node").get("telescopeState")'
+        + str(resource("ska_mid/tm_central/central_node").get("telescopeState"))
+    )
+    LOGGER.info(
+        'resource("ska_mid/tm_subarray_node/1").get("State")'
+        + str(resource("ska_mid/tm_subarray_node/1").get("State"))
+    )
+    LOGGER.info(
+        'resource("mid_csp/elt/subarray_01").get("State")'
+        + str(resource("mid_csp/elt/subarray_01").get("State"))
+    )
+    LOGGER.info(
+        'resource("mid_sdp/elt/subarray_1").get("State")'
+        + str(resource("mid_sdp/elt/subarray_1").get("State"))
+    )
+    LOGGER.info(
+        'resource("mid_csp/elt/master").get("State")'
+        + str(resource("mid_csp/elt/master").get("State"))
+    )
+    LOGGER.info(
+        'resource("mid_sdp/elt/master").get("State")'
+        + str(resource("mid_sdp/elt/master").get("State"))
+    )
+    LOGGER.info(
+        'resource("ska_mid/tm_leaf_node/d0001").get("State")'
+        + str(resource("ska_mid/tm_leaf_node/d0001").get("State"))
+    )
+    LOGGER.info(
+        'resource("ska_mid/tm_leaf_node/d0002").get("State")'
+        + str(resource("ska_mid/tm_leaf_node/d0002").get("State"))
+    )
+    LOGGER.info(
+        'resource("ska_mid/tm_leaf_node/d0003").get("State")'
+        + str(resource("ska_mid/tm_leaf_node/d0003").get("State"))
+    )
+    LOGGER.info(
+        'resource("ska_mid/tm_leaf_node/d0004").get("State")'
+        + str(resource("ska_mid/tm_leaf_node/d0004").get("State"))
+    )
+    LOGGER.info(
+        'resource("mid_d0001/elt/master").get("State")'
+        + str(resource("mid_d0001/elt/master").get("State"))
+    )
+    LOGGER.info(
+        'resource("mid_d0002/elt/master").get("State")'
+        + str(resource("mid_d0002/elt/master").get("State"))
+    )
+    LOGGER.info(
+        'resource("mid_d0003/elt/master").get("State")'
+        + str(resource("mid_d0003/elt/master").get("State"))
+    )
+    LOGGER.info(
+        'resource("mid_d0004/elt/master").get("State")'
+        + str(resource("mid_d0004/elt/master").get("State"))
+    )
+
     assert_that(resource('mid_csp/elt/subarray_01').get('State')).is_equal_to(resource('ska_mid/tm_subarray_node/1').get('State'))
     assert_that(resource('ska_mid/tm_subarray_node/1').get('State')).is_equal_to(resource('mid_csp_cbf/sub_elt/subarray_01').get('State'))
     assert_that(resource('mid_csp/elt/subarray_01').get('State')).is_equal_to(resource('mid_csp_cbf/sub_elt/subarray_01').get('State'))
+    assert 0
 
 @pytest.mark.select   
 @pytest.mark.last

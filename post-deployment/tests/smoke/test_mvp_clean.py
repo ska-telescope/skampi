012763f614880037816e0047da06d1ad086c8d16
Digvijay Mali <digvijay.ska@gmail.com>
2021-07-16 13:02:37 +0530

    AT1-949 Update subarray_Devices in helpers function.

--- post-deployment/tests/smoke/test_mvp_clean.py
+++ post-deployment/tests/smoke/test_mvp_clean.py
@@ -1,3 +1,4 @@
+import time
 import os, sys
 import logging
 import pytest
@@ -27,7 +28,7 @@ def test_is_running(running_telescope):
 @pytest.mark.first
 @pytest.mark.last
 def test_smell_mvp(pre_or_post="#PRE"):
-
+    time.sleep(10)
     header = f"\n###{pre_or_post}-TEST STATES###\n{'Device Name:':<34} {'State':<15}{'obsState':<15}\n"
     output = [f"{device:<35}{resource(device).get('State'):<15}{resource(device).get('obsState'):<15}" for device in subarray_devices]
     aggegate_output = reduce(lambda x,y:x +'\n'+y ,output)

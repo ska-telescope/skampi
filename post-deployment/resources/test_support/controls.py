caf90af9cac751c8489a2a0853446f48aed7cfb0
Digvijay Mali <digvijay.ska@gmail.com>
2021-07-16 10:13:47 +0530

    AT1-949

--- post-deployment/resources/test_support/controls.py
+++ post-deployment/resources/test_support/controls.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from datetime import date,datetime
 import os
@@ -184,6 +185,7 @@ def telescope_is_in_standby():
     #     'resource("ska_mid/tm_central/central_node").get("State")'
     #     + str(resource("ska_mid/tm_central/central_node").get("State"))
     # )
+    time.sleep(10)
     LOGGER.info(
         'resource("ska_mid/tm_central/central_node").get("telescopeState")'
         + str(resource("ska_mid/tm_central/central_node").get("telescopeState"))

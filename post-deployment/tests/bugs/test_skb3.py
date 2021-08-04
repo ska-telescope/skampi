311aebb47ce8fa6fea5efd5332ff118b59e044ff
Apurva Patkar <apurva.ska@gmail.com>
2021-08-04 06:08:23 +0000

    AT1-949-Execute skamid BDD test cases.

--- post-deployment/tests/bugs/test_skb3.py
+++ post-deployment/tests/bugs/test_skb3.py
@@ -6,7 +6,7 @@ from tango import DeviceProxy
 import logging
 LOGGER = logging.getLogger(__name__)
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 @pytest.mark.skamid
 def test_tm_subarray_inconsistent_at_start_up():
     try:

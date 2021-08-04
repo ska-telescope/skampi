615750c90f868715ae348c02643c3b44e6b0f0d0
DigvijayMali92 <digvijay.ska@gmail.com>
2021-08-04 10:49:36 +0000

    AT1-949 Skip BDD test case test_XTP-2413_XTP-2415-2416-2417

--- post-deployment/tests/acceptance/end_uses/conduct_observation/test_XTP-2413_XTP-2415-2416-2417.py
+++ post-deployment/tests/acceptance/end_uses/conduct_observation/test_XTP-2413_XTP-2415-2416-2417.py
@@ -71,6 +71,7 @@ def end(result):
                 resource(result[SUBARRAY_USED]).get('obsState'))
 
 
+@pytest.mark.skip
 @pytest.mark.oetlow
 @pytest.mark.skalow
 @scenario("XTP-2413.feature", "Creating a new SBI with updated SB IDs and PB IDs")
@@ -82,6 +83,7 @@ def test_sbi_creation():
     """
 
 
+@pytest.mark.skip
 @pytest.mark.oetlow
 @pytest.mark.skalow
 @pytest.mark.quarantine
@@ -97,6 +99,7 @@ def test_resource_allocation():
     pass
 
 
+@pytest.mark.skip
 @pytest.mark.oetlow
 @pytest.mark.skalow
 @pytest.mark.quarantine

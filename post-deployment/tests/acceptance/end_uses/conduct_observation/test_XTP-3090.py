e3e481b2bc3e1dc704ccef9afe527b0b2b601678
samueltwum1 <samueltwum1@gmail.com>
2021-08-17 15:49:53 +0200

    SAR-245 Wait for dish to arrive at stow position

--- post-deployment/tests/acceptance/end_uses/conduct_observation/test_XTP-3090.py
+++ post-deployment/tests/acceptance/end_uses/conduct_observation/test_XTP-3090.py
@@ -1,10 +1,9 @@
 """Test for XTP-3090"""
-import os
 import logging
-import time
 import pytest
+import time
 
-import tango 
+import tango
 
 from pytest_bdd import scenario, given, when, then
 from assertpy import assert_that
@@ -12,10 +11,11 @@ from assertpy import assert_that
 from resources.test_support.helpers import resource
 
 STOW_POSITION = 85.0
-INITIAL_AZ = 0 # to be overwritten before stow request
+INITIAL_AZ = 0  # to be overwritten before stow request
+DISH_MASTER = "mid_d0001/elt/master"
+LOGGER = logging.getLogger(__name__)
 
-logger = logging.getLogger(__name__)
-dish_master = "mid_d0001/elt/master"
+
 
 @scenario("XTP-3090.feature", "Test dish master simulator stow request")
 def test_stow_command():
@@ -23,33 +23,47 @@ def test_stow_command():
 
 
 @given("a running telescope")
-def telescope_is_runnnig(running_telescope):
+def telescope_is_running(running_telescope):
     pass
 
 
 @when("I execute a stow command")
 def set_stow_mode():
-    dp = tango.DeviceProxy(dish_master)
-    INITIAL_AZ = resource(dish_master).get('achievedPointing')[1]
-    logger.info(f"{dish_master} initial azimuth: {INITIAL_AZ}")
-    logger.info(f"{dish_master} initial elevation: {resource(dish_master).get('achievedPointing')[2]}")
-    dp.setstowmode()
-    logger.info(f"{dish_master} requested dishMode: STOW")
+    dsh_proxy = tango.DeviceProxy(DISH_MASTER)
+    INITIAL_AZ = resource(DISH_MASTER).get("achievedPointing")[1]
+    LOGGER.info(f"{DISH_MASTER} initial azimuth: {INITIAL_AZ}")
+    LOGGER.info(
+        f"{DISH_MASTER} initial elevation: {resource(DISH_MASTER).get('achievedPointing')[2]}"
+    )
+    dsh_proxy.setstowmode()
+    LOGGER.info(f"{DISH_MASTER} requested dishMode: STOW")
 
 
 @then("the dish mode should report STOW")
 def check_dish_mode():
-    assert_that(resource(dish_master).get("dishMode")).is_equal_to("STOW")
-    logger.info(f"{dish_master} current dishMode: {resource(dish_master).get('dishMode')}")
+    assert_that(resource(DISH_MASTER).get("dishMode")).is_equal_to("STOW")
+    LOGGER.info(f"{DISH_MASTER} current dishMode: {resource(DISH_MASTER).get('dishMode')}")
 
 
 @then("the elevation should be almost equal to the stow position")
 def check_dish_master_elevation():
-    assert_that(resource(dish_master).get("achievedPointing")[2]).is_close_to(STOW_POSITION, 0.5)
-    logger.info(f"{dish_master} elevation: {resource(dish_master).get('achievedPointing')[2]}")
+    future = time.time() + 5  # 5 seconds from now
+    current_el = resource(DISH_MASTER).get("achievedPointing")[2]
+    dish_far_from_target = not (STOW_POSITION - current_el == pytest.approx(1, abs=1))
+
+    while dish_far_from_target:
+        now = time.time()
+        current_el = resource(DISH_MASTER).get("achievedPointing")[2]
+        dish_far_from_target = not (STOW_POSITION - current_el == pytest.approx(1, abs=1))
+        time.sleep(1)  # sleep to avoid using full CPU resources while we wait to get on target
+        if future < now:
+            raise Exception("Timeout occurred")
+
+    assert_that(resource(DISH_MASTER).get("achievedPointing")[2]).is_close_to(STOW_POSITION, 1.0)
+    LOGGER.info(f"{DISH_MASTER} elevation: {resource(DISH_MASTER).get('achievedPointing')[2]}")
 
 
 @then("the azimuth should remain in the same position")
 def check_dish_master_azimuth():
-    assert_that(resource(dish_master).get("achievedPointing")[1]).is_equal_to(INITIAL_AZ)
-    logger.info(f"{dish_master} azimuth: {resource(dish_master).get('achievedPointing')[2]}")
+    assert_that(resource(DISH_MASTER).get("achievedPointing")[1]).is_equal_to(INITIAL_AZ)
+    LOGGER.info(f"{DISH_MASTER} azimuth: {resource(DISH_MASTER).get('achievedPointing')[1]}")

81e525dd91f76f336719a26ea3b4934c8e1dba5a
Gerhard le Roux <gerhardlr@gmail.com>
2021-08-25 17:15:13 +0000

    update test

--- post-deployment/tests/acceptance/end_uses/conduct_observation/edit_subarray_resources/test_XTP-417.py
+++ post-deployment/tests/acceptance/end_uses/conduct_observation/edit_subarray_resources/test_XTP-417.py
@@ -12,7 +12,7 @@ from pytest_bdd import scenario, given, when, then
 import pytest
 from time import sleep
 
-from tango import DeviceProxy
+#from tango import DeviceProxy
 
 from ska_ser_skallop.mvp_fixtures.env_handling import ExecEnv
 from ska_ser_skallop.mvp_fixtures.base import ExecSettings
@@ -23,6 +23,7 @@ from ska_ser_skallop.mvp_control.event_waiting import wait
 from ska_ser_skallop.mvp_control.describing import mvp_names
 from ska_ser_skallop.event_handling import builders
 from ska_ser_skallop.mvp_fixtures.context_management import TelescopeContext
+from ska_ser_skallop.event_handling.logging import device_logging_context, LogSpec
 
 logger = logging.getLogger(__name__)
 
@@ -41,7 +42,8 @@ def fxt_init(exec_env: ExecEnv, exec_settings: ExecSettings):
     #exec_settings.log_enabled = True
 
 @pytest.mark.skamid
-@pytest.mark.skip("test is still WIP")
+@pytest.mark.sst587
+#@pytest.mark.skip("test is still WIP")
 @scenario("1_XR-13_XTP-494.feature", "A1-Test, Sub-array resource allocation")
 def test_allocate_resources(init):
     """Assign Resources."""
@@ -65,10 +67,10 @@ def allocate(
     nr_of_dishes = 4
     receptors = list(range(1, int(nr_of_dishes) + 1))
     # check sdp subarray has polling set up
-    sdp_subarray = mvp_names.Mid.sdp.subarray(subarray_id).__str__()
-    sdp_proxy = DeviceProxy(sdp_subarray)
-    polling = sdp_proxy.get_attribute_poll_period('obsState')
-    logger.info(f'Note {sdp_subarray} is polled with {polling}ms')
+    #sdp_subarray = mvp_names.Mid.sdp.subarray(subarray_id).__str__()
+    #sdp_proxy = DeviceProxy(sdp_subarray)
+    #polling = sdp_proxy.get_attribute_poll_period('obsState')
+    #logger.info(f'Note {sdp_subarray} is polled with {polling}ms')
 
     composition = conf_types.CompositionByFile(tmp_path, conf_types.CompositionType.STANDARD)
 
@@ -83,6 +85,11 @@ def allocate(
     )
 
     running_telescope.release_subarray_when_finished(subarray_id, receptors, exec_settings)
+    # logging sdp_subarry as it is suspect
+    devices_to_log = LogSpec().add_log(
+        device_name=mvp_names.Mid.sdp.subarray(subarray_id).__str__()
+    )
+    running_telescope.push_context_onto_test(device_logging_context(builder, devices_to_log))
     board = running_telescope.push_context_onto_test(wait.waiting_context(builder))
     context.board = board
     context.checker = checker
@@ -99,14 +106,14 @@ def check_subarray_composition(context):
     board: wait.MessageBoardBase = context.board
     
     try:
-        wait.wait(context.board, 8*60, live_logging=True)
+        wait.wait(context.board, 8*60, live_logging=False)
         sleep(4) # hack to circumvent possible synchronization fault in event generated data vs queried data
     except wait.EWhilstWaiting as exception:
         logs = board.play_log_book()
         logger.info(f"Log messages during waiting:\n{logs}")
         raise exception
-    logs = board.play_log_book()
-    logger.info(f"Log messages during waiting:\n{logs}")
+    logs = board.play_log_book(filter_log=False,log_filter_pattern="txn")
+    logger.info(f"Log messages and events captured whilst test ran:\n{logs}")
 
 
 @then("the subarray is in the condition that allows scan configurations to take place")

55de7b62f8d586bdf4c60d7fe0f4cb773d614923
Gerhard le Roux <gerhardlr@gmail.com>
2021-08-25 11:06:55 +0000

    skip 417

--- post-deployment/tests/acceptance/end_uses/conduct_observation/edit_subarray_resources/test_XTP-417.py
+++ post-deployment/tests/acceptance/end_uses/conduct_observation/edit_subarray_resources/test_XTP-417.py
@@ -41,7 +41,7 @@ def fxt_init(exec_env: ExecEnv, exec_settings: ExecSettings):
     #exec_settings.log_enabled = True
 
 @pytest.mark.skamid
-@pytest.mark.trial
+@pytest.mark.skip("test is still WIP")
 @scenario("1_XR-13_XTP-494.feature", "A1-Test, Sub-array resource allocation")
 def test_allocate_resources(init):
     """Assign Resources."""

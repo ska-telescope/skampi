1577fb591e8d2e4d918b4474b546f5920afaac9e
Adriaan de Beer <adebeer42@gmail.com>
2021-10-29 17:48:52 +0200

    AT-98 removed more unnecessary things

--- attic/resources/test.mk
+++ attic/resources/test.mk
@@ -94,30 +94,30 @@
 
 # CI_JOB_ID ?= local
 # CLUSTER_TEST_NAMESPACE ?= ci-$(CI_JOB_ID)##Test namespace for cluster readiness tests
-LOADBALANCER_IP?=$(shell kubectl cluster-info | grep Kubernetes | cut -d/ -f3 | sed -e 's,:.*,,g')
+# LOADBALANCER_IP?=$(shell kubectl cluster-info | grep Kubernetes | cut -d/ -f3 | sed -e 's,:.*,,g')
 
-# Make target based test of cluster setup. This is done so that the SKAMPI user can test the basic
-# functionality of the cluster by simply calling `make cluster-k8s-test`
+# # Make target based test of cluster setup. This is done so that the SKAMPI user can test the basic
+# # functionality of the cluster by simply calling `make cluster-k8s-test`
 
-cluster-k8s-test-pre: ## Setup of kubernetes resources for testing cluster
-	kubectl config view --flatten --raw > tests/resources/assets/kubeconfig
-	kubectl get nodes -o wide --kubeconfig=tests/resources/assets/kubeconfig
-	kubectl version
-	kubectl auth can-i create pods/exec
+# cluster-k8s-test-pre: ## Setup of kubernetes resources for testing cluster
+# 	kubectl config view --flatten --raw > tests/resources/assets/kubeconfig
+# 	kubectl get nodes -o wide --kubeconfig=tests/resources/assets/kubeconfig
+# 	kubectl version
+# 	kubectl auth can-i create pods/exec
 	
-cluster-k8s-test-post: ## teardown step for testing cluster
-	kubectl -n $(CLUSTER_TEST_NAMESPACE) delete deployments,pods,svc,daemonsets,replicasets,statefulsets,cronjobs,jobs,ingresses,configmaps,pvc --all --ignore-not-found
-	kubectl delete pv pv-test-$(CLUSTER_TEST_NAMESPACE) --ignore-not-found
-	kubectl delete ns $(CLUSTER_TEST_NAMESPACE) --ignore-not-found
-	rm tests/resources/assets/kubeconfig || true
+# cluster-k8s-test-post: ## teardown step for testing cluster
+# 	kubectl -n $(CLUSTER_TEST_NAMESPACE) delete deployments,pods,svc,daemonsets,replicasets,statefulsets,cronjobs,jobs,ingresses,configmaps,pvc --all --ignore-not-found
+# 	kubectl delete pv pv-test-$(CLUSTER_TEST_NAMESPACE) --ignore-not-found
+# 	kubectl delete ns $(CLUSTER_TEST_NAMESPACE) --ignore-not-found
+# 	rm tests/resources/assets/kubeconfig || true
 
-cluster-k8s-test-do: export CLUSTER_TEST_NAMESPACE=ci-$(CI_JOB_ID)
-cluster-k8s-test-do: ## Test the cluster using pytest
-	echo $$CLUSTER_TEST_NAMESPACE
-	echo ${LOADBALANCER_IP}
-	LOADBALANCER_IP=${LOADBALANCER_IP} pytest tests/unit/test_cluster_k8s.py
+# cluster-k8s-test-do: export CLUSTER_TEST_NAMESPACE=ci-$(CI_JOB_ID)
+# cluster-k8s-test-do: ## Test the cluster using pytest
+# 	echo $$CLUSTER_TEST_NAMESPACE
+# 	echo ${LOADBALANCER_IP}
+# 	LOADBALANCER_IP=${LOADBALANCER_IP} pytest tests/unit/test_cluster_k8s.py
 
-cluster-k8s-test: cluster-k8s-test-pre cluster-k8s-test-do cluster-k8s-test-post ## Test the cluster using make setup and teardown
+# cluster-k8s-test: cluster-k8s-test-pre cluster-k8s-test-do cluster-k8s-test-post ## Test the cluster using make setup and teardown
 
 # get_size_images: ## get a list of images together with their size (both local and compressed) in the namespace KUBE_NAMESPACE
 # 	@for p in `kubectl get pods -n $(KUBE_NAMESPACE) -o jsonpath="{range .items[*]}{range .spec.containers[*]}{.image}{'\n'}{end}{range .spec.initContainers[*]}{.image}{'\n'}{end}{end}" | sort | uniq`; do \

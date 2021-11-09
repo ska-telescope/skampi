2863fdbe740e12c1e3a03fe1409556a4a8bfb51f
Gerhard le Roux <gerhardlr@gmail.com>
2021-11-09 11:53:49 +0000

    SST-618: change to loadbalancer_IP

--- tests/integration/test_tango_jql.py
+++ tests/integration/test_tango_jql.py
@@ -1,8 +1,6 @@
 """Default feature tests."""
 import os
 from ska_ser_skallop.connectors.configuration import get_device_proxy
-import logging
-import requests
 from pytest_bdd import (
     given,
     scenario,
@@ -19,13 +17,9 @@ def test_tangojql_service_available():
 @given("a configuration to access a tango device remotely")
 def a_configuration_to_access_a_tango_device_remotely():
     """a configuration to access a tango device remotely."""
-    host = os.getenv("INGRESS_HOST")
+    host = os.getenv("LOADBALANCER_IP")
     assert host, "Expected an INGRESS_HOST variable to be set"
-    # bypass external api domain name with internal one
-    # if host == "k8s.stfc.skao.int":
-    #    host = "k8s.skao.stfc"
     os.environ["KUBE_HOST"] = host
-    os.environ["BYPASS_AUTH"] = "True"
     namespace = os.getenv("KUBE_NAMESPACE")
     assert namespace, "Expected an KUBE_NAMESPACE variable to be set"
     os.environ["TEST_ENV"] = "BUILD_OUT"
@@ -35,16 +29,8 @@ def a_configuration_to_access_a_tango_device_remotely():
 def i_send_a_ping_command_to_the_tango_database_device_server():
     """I send a ping command to the tango database device server."""
     device_name = "sys/database/2"
-    # temp code for dev work delete afterwards
-    namespace = os.getenv("KUBE_NAMESPACE")
-    host = os.getenv("INGRESS_HOST")
-    url = f"http://{host}/{namespace}/taranta"
-    logging.info(f"checking results from running a get on {url}")
-    result = requests.get(url)
-    logging.info(f"Result:\n{result}")
-    # delete preceding code
-    # device_proxy = get_device_proxy(device_name)
-    # device_proxy.ping()
+    device_proxy = get_device_proxy(device_name)
+    device_proxy.ping()
 
 
 @then("I expect a response to be returned from the device server")

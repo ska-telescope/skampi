9356d984cf5f7ff2c6922fbfb5da95d8579d59f8
Adriaan de Beer <adebeer42@gmail.com>
2021-12-13 21:30:24 +0200

    AT-111 testing infra tests executed only from python-test job

--- tests/integration/test_xray_upload.py
+++ tests/integration/test_xray_upload.py
@@ -1,5 +1,5 @@
 """Cucumber test results are uploaded to XRay feature tests."""
-
+import pytest
 from pytest_bdd import (
     given,
     scenario,
@@ -8,6 +8,7 @@ from pytest_bdd import (
 )
 
 
+@pytest.mark.infra
 @scenario("features/xray_upload.feature", "SKAMPI CI Pipeline tests execute on SKAMPI")
 def test_skampi_ci_pipeline_tests_execute_on_skampi():
     """SKAMPI CI Pipeline tests execute on SKAMPI."""
@@ -28,10 +29,4 @@ def an_attempt_is_made_to_run_tests_within_the_repository():
 @then("the tests within the SKAMPI repository are run")
 def the_tests_within_the_skampi_repository_are_run():
     """the tests within the SKAMPI repository are run."""
-    pass
-
-
-@then("there was a passing test")
-def there_was_a_passing_test():
-    """there was a passing test."""
     pass

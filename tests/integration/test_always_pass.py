f7ca1d012e7720bd740bdfb066a41255c8278c0a
Adriaan de Beer <adebeer42@gmail.com>
2021-12-14 13:20:07 +0200

    AT-115 make pipeline green again

---
+++ tests/integration/test_always_pass.py
@@ -0,0 +1,4 @@
+# This test is purely a placeholder so that k8s-test can pass while there are no tests executed using make k8s-test. It can be removed as soon as there are integration tests included in SKAMPI.
+
+def test_always_pass():
+    pass

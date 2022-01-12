762da6846d7ff10bad3208b267b5734e5ca9d3a4
Gerhard le Roux <gerhardlr@gmail.com>
2022-01-12 18:42:07 +0200

    AT-111: uses builder if provided

--- tests/resources/models/mvp_model/synched_entry_point.py
+++ tests/resources/models/mvp_model/synched_entry_point.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from contextlib import contextmanager
 from typing import List
 
 from ska_ser_skallop.mvp_control.entry_points.base import EntryPoint
@@ -10,64 +11,75 @@ class SynchedEntryPoint(EntryPoint):
     perform synchronized actions
     """
 
+    builder = None
+
+    def __init__(self) -> None:
+        super().__init__()
+
     @abstractmethod
     def set_telescope_to_running(self):
         pass
 
     def set_wating_for_start_up(self) -> waiting.MessageBoardBuilder:
-        return waiting.set_wating_for_start_up()
+        return waiting.set_wating_for_start_up(self.builder)
 
     def set_waiting_for_assign_resources(
         self,
         sub_array_id: int,
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_waiting_for_assign_resources(sub_array_id)
+        return waiting.set_waiting_for_assign_resources(sub_array_id, self.builder)
 
     def set_waiting_for_release_resources(
         self,
         sub_array_id: int,
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_waiting_for_release_resources(sub_array_id)
+        return waiting.set_waiting_for_release_resources(sub_array_id, self.builder)
 
     def set_wating_for_shut_down(
         self,
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_wating_for_shut_down()
+        return waiting.set_wating_for_shut_down(self.builder)
 
     def set_waiting_for_configure(
         self, sub_array_id: int, receptors: List[int]
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_waiting_for_configure_scan(sub_array_id, receptors)
+        return waiting.set_waiting_for_configure_scan(
+            sub_array_id, receptors, self.builder
+        )
 
     def set_waiting_until_configuring(
         self, sub_array_id: int, receptors: List[int]
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_waiting_until_configuring(sub_array_id, receptors)
+        return waiting.set_waiting_until_configuring(
+            sub_array_id, receptors, self.builder
+        )
 
     def set_waiting_until_scanning(
         self, sub_array_id: int, receptors: List[int]
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_waiting_until_scanning(sub_array_id, receptors)
+        return waiting.set_waiting_until_scanning(sub_array_id, receptors, self.builder)
 
     def set_waiting_for_clear_configure(
         self, sub_array_id: int, receptors: List[int]
     ) -> waiting.MessageBoardBuilder:
         return waiting.set_waiting_for_releasing_a_configuration(
-            sub_array_id, receptors
+            sub_array_id, receptors, self.builder
         )
 
     def set_waiting_for_obsreset(
         self, sub_array_id: int, receptors: List[int]
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_waiting_for_obsreset(sub_array_id, receptors)
+        return waiting.set_waiting_for_obsreset(sub_array_id, receptors, self.builder)
 
     def set_waiting_until_resourcing(
         self,
         sub_array_id: int,
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_waiting_until_resourcing(sub_array_id)
+        return waiting.set_waiting_until_resourcing(sub_array_id, self.builder)
 
     def set_wating_for_scan_completion(
         self, sub_array_id: int, receptors: List[int]
     ) -> waiting.MessageBoardBuilder:
-        return waiting.set_waiting_for_scanning_to_complete(sub_array_id, receptors)
+        return waiting.set_waiting_for_scanning_to_complete(
+            sub_array_id, receptors, self.builder
+        )

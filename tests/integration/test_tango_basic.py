c90a1ee3744dfbf8bc9be4437ca8544abdb741d6
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2022-01-25 12:55:04 +0100

    ST-1101 apply suggestion
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- tests/integration/test_tango_basic.py
+++ tests/integration/test_tango_basic.py
@@ -10,7 +10,7 @@ from requests.models import Response
 from assertpy import assert_that
 from ska_ser_skallop.connectors.configuration import get_device_proxy
 
-
+@pytest.mark.taranta
 @scenario("features/taranta_basic.feature", "TangoGQL service available")
 def test_tangogql_service_available():
     """TangoGQL service available."""
@@ -34,7 +34,7 @@ def i_send_a_ping_command_to_the_tango_database_device_server():
 def i_expect_a_response_to_be_returned_from_the_device_server():
     """I expect a response to be returned from the device server."""
 
-
+@pytest.mark.taranta
 @scenario("features/taranta_basic.feature", "taranta dashboard services available")
 def test_taranta_dashboard_services_available():
     """taranta dashboard services available."""
@@ -59,7 +59,7 @@ def a_deployed_taranta_web_dashboard_service(env: ENV) -> str:
     """a deployed Taranta web dashboard service."""
     return f"http://{env.host}/{env.namespace}/taranta/dashboard/"
 
-
+@pytest.mark.taranta
 @scenario("features/taranta_basic.feature", "taranta devices service available")
 def test_taranta_devices_service_available(env: ENV) -> str:
     """taranta devices service available."""

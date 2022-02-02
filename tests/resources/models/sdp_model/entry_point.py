c8c5457c5514a7e41f2c4118973cf89eeb335de0
Gerhard le Roux <gerhardlr@gmail.com>
2022-02-02 19:03:56 +0200

    AT-111: allow for debugging

--- tests/resources/models/sdp_model/entry_point.py
+++ tests/resources/models/sdp_model/entry_point.py
@@ -1,11 +1,17 @@
+import imp
 from typing import List
 import json
+import os
 from ..mvp_model.synched_entry_point import SynchedEntryPoint
 
 from ska_ser_skallop.mvp_control.describing import mvp_names as names
 from ska_ser_skallop.connectors import configuration as con_config
 from ska_ser_skallop.mvp_control.configuration import types
 from ska_ser_skallop.mvp_control.configuration import composition as comp
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class SDPEntryPoint(SynchedEntryPoint):
@@ -16,13 +22,22 @@ class SDPEntryPoint(SynchedEntryPoint):
     def __init__(self) -> None:
         super().__init__()
         self._tel = names.TEL()
-        self.sdp_master = con_config.get_device_proxy(self._tel.sdp.master)
+        self._sdp_master_name = self._tel.sdp.master
+        self._live_logging = True if os.getenv("DEBUG") else False
+
+    def _log(self, mssage: str):
+        if self._live_logging:
+            logger.info(mssage)
 
     def set_telescope_to_running(self):
         for index in range(1, self.nr_of_subarrays + 1):
+            subarray_name = self._tel.sdp.subarray(index)
             subarray = con_config.get_device_proxy(self._tel.sdp.subarray(index))
+            self._log(f"commanding {subarray_name} to On")
             subarray.command_inout("On")
-        self.sdp_master.command_inout("On")
+        self._log(f"commanding {self._sdp_master_name} to On")
+        sdp_master = con_config.get_device_proxy(self._sdp_master_name)
+        sdp_master.command_inout("On")
 
     def abort_subarray(self, sub_array_id: int):
         pass
@@ -38,11 +53,15 @@ class SDPEntryPoint(SynchedEntryPoint):
         sb_id: str,
     ):
         self._tel = names.TEL()
-        subarray = con_config.get_device_proxy(self._tel.sdp.subarray(sub_array_id))
+        subarray_name = self._tel.sdp.subarray(sub_array_id)
+        subarray = con_config.get_device_proxy(subarray_name)
         standard_composition = comp.generate_standard_comp(
             sub_array_id, dish_ids, sb_id
         )
         sdp_standard_composition = json.dumps(json.loads(standard_composition)["sdp"])
+        self._log(
+            f"commanding {subarray_name} with AssignResources: {sdp_standard_composition} "
+        )
         subarray.command_inout("AssignResources", sdp_standard_composition)
 
     def configure_subarray(
@@ -63,42 +82,17 @@ class SDPEntryPoint(SynchedEntryPoint):
 
     def set_telescope_to_standby(self):
         for index in range(1, self.nr_of_subarrays + 1):
-            subarray = con_config.get_device_proxy(self._tel.sdp.subarray(index))
+            subarray_name = self._tel.sdp.subarray(index)
+            subarray = con_config.get_device_proxy(subarray_name)
+            self._log(f"commanding {subarray_name} to Off")
             subarray.command_inout("Off")
-        self.sdp_master.command_inout("Off")
+        self._log(f"commanding {self._sdp_master_name} to Off")
+        sdp_master = con_config.get_device_proxy(self._sdp_master_name)
+        sdp_master.command_inout("Off")
 
     def tear_down_subarray(self, sub_array_id: int):
         self._tel = names.TEL()
-        subarray = con_config.get_device_proxy(self._tel.sdp.subarray(sub_array_id))
+        subarray_name = self._tel.sdp.subarray(sub_array_id)
+        subarray = con_config.get_device_proxy(subarray_name)
+        self._log(f"Commanding {subarray_name} to ReleaseResources")
         subarray.command_inout("ReleaseResources")
-
-    def set_waiting_for_assign_resources(
-        self,
-        sub_array_id: int,
-    ):
-        builder = super().set_waiting_for_assign_resources(sub_array_id)
-        if self._tel.skalow:
-            subarray = self._tel.skalow.sdp.subarray(sub_array_id)
-            builder.set_waiting_on(subarray).for_attribute(
-                "obsState"
-            ).to_become_equal_to("IDLE")
-        elif self._tel.skamid:
-            subarray = self._tel.skamid.sdp.subarray(sub_array_id)
-            builder.set_waiting_on(subarray).for_attribute(
-                "obsState"
-            ).to_become_equal_to("IDLE")
-        return builder
-
-    def set_waiting_for_release_resources(self, sub_array_id: int):
-        builder = super().set_waiting_for_release_resources(sub_array_id)
-        if self._tel.skalow:
-            subarray = self._tel.skalow.sdp.subarray(sub_array_id)
-            builder.set_waiting_on(subarray).for_attribute(
-                "obsState"
-            ).to_become_equal_to("EMPTY")
-        elif self._tel.skamid:
-            subarray = self._tel.skamid.sdp.subarray(sub_array_id)
-            builder.set_waiting_on(subarray).for_attribute(
-                "obsState"
-            ).to_become_equal_to("EMPTY")
-        return builder

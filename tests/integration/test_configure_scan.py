a3310a23c9e1e1a8d65ec131950eb3d7c6e8f72f
Gerhard le Roux <gerhardlr@gmail.com>
2022-02-07 17:49:56 +0200

    AT-111: update configure scan test

--- tests/integration/test_configure_scan.py
+++ tests/integration/test_configure_scan.py
@@ -1,15 +1,72 @@
 """Configure scan on subarray feature tests."""
+import os
+
 import pytest
 from assertpy import assert_that
 from pytest_bdd import given, scenario, then, when
-from resources.models.mvp_model.states import ObsState
 from ska_ser_skallop.connectors import configuration as con_config
 from ska_ser_skallop.mvp_control.describing import mvp_names as names
 from ska_ser_skallop.mvp_control.entry_points import types as conf_types
-from ska_ser_skallop.mvp_control.event_waiting.wait import wait_for
 from ska_ser_skallop.mvp_fixtures.fixtures import fxt_types
 
+from resources.models.mvp_model.states import ObsState
+
 SCAN_DURATION = 2
+RECEPTORS = [1, 2]
+SUB_ARRAY_ID = 1
+
+
+@pytest.fixture(name="configure_scan_test_exec_settings")
+def fxt_configure_scan_test_exec_settings(
+    exec_settings: fxt_types.exec_settings,
+) -> fxt_types.exec_settings:
+    """Set up test specific execution settings.
+
+    :param exec_settings: The global test execution settings as a fixture.
+    :return: test specific execution settings as a fixture
+    """
+    configure_scan_test_exec_settings = exec_settings.replica()
+    configure_scan_test_exec_settings.time_out = 150
+    if os.getenv("DEBUG"):
+        exec_settings.run_with_live_logging()
+        configure_scan_test_exec_settings.run_with_live_logging()
+    elif os.getenv("LIVE_LOGGING"):
+        configure_scan_test_exec_settings.run_with_live_logging()
+    elif os.getenv("REPLAY_EVENTS_AFTERWARDS"):
+        configure_scan_test_exec_settings.replay_events_afterwards()
+    return configure_scan_test_exec_settings
+
+
+# scan configurations
+
+
+@pytest.fixture(name="sdp_base_configuration")
+def fxt_sdp_base_composition(tmp_path) -> conf_types.ScanConfiguration:
+    """Setup a base scan configuration to use for sdp.
+
+    :param tmp_path: a temporary path for sending configuration as a file.
+    :return: the configuration settings.
+    """
+    configuration = conf_types.ScanConfigurationByFile(
+        tmp_path, conf_types.ScanConfigurationType.STANDARD
+    )
+    return configuration
+
+
+# log capturing
+
+
+@pytest.fixture(name="set_up_log_checking_for_sdp")
+@pytest.mark.usefixtures("set_sdp_entry_point")
+def fxt_set_up_log_capturing_for_cbf(log_checking: fxt_types.log_checking):
+    """Set up log capturing (if enabled by CATPURE_LOGS).
+
+    :param log_checking: The skallop log_checking fixture to use
+    """
+    if os.getenv("CAPTURE_LOGS"):
+        tel = names.TEL()
+        sdp_subarray = str(tel.sdp.subarray(SUB_ARRAY_ID))
+        log_checking.capture_logs_from_devices(sdp_subarray)
 
 
 @pytest.mark.skip(reason="test still in dev phase")
@@ -20,7 +77,7 @@ def test_configure_scan_on_sdp_subarray_in_low():
     """Configure scan on sdp subarray in low."""
 
 
-@pytest.mark.skip(reason="test still in dev phase")
+# @pytest.mark.skip(reason="test still in dev phase")
 @scenario(
     "features/sdp_configure_scan.feature", "Configure scan on sdp subarray in mid"
 )
@@ -28,30 +85,42 @@ def test_configure_scan_on_sdp_subarray_in_mid():
     """Configure scan on sdp subarray in mid."""
 
 
-@given("an SDP subarray in IDLE state")
-def an_sdp_subarray_in_idle_state(set_sdp_entry_point):
+@given("an SDP subarray in IDLE state", target_fixture="configuration")
+def an_sdp_subarray_in_idle_state(
+    configure_scan_test_exec_settings,  # pylint: disable=unused-argument
+    set_sdp_entry_point,  # pylint: disable=unused-argument
+    set_up_log_checking_for_sdp,  # pylint: disable=unused-argument
+    sdp_base_configuration: conf_types.ScanConfiguration,
+    subarray_allocation_spec: fxt_types.subarray_allocation_spec,
+) -> conf_types.ScanConfiguration:
     """an SDP subarray in IDLE state."""
+    subarray_allocation_spec.receptors = RECEPTORS
+    subarray_allocation_spec.subarray_id = SUB_ARRAY_ID
+    # will use default composition for the allocated subarray
+    # subarray_allocation_spec.composition
+    return sdp_base_configuration
 
 
 @when("I configure it for a scan")
 def i_configure_it_for_a_scan(
     allocated_subarray: fxt_types.allocated_subarray,
-    exec_settings: fxt_types.exec_settings,
+    context_monitoring: fxt_types.context_monitoring,
     entry_point: fxt_types.entry_point,
-    tmp_path,
+    configuration: conf_types.ScanConfiguration,
+    configure_scan_test_exec_settings: fxt_types.exec_settings,
 ):
     """I configure it for a scan."""
     sub_array_id = allocated_subarray.id
     receptors = allocated_subarray.receptors
     sb_id = allocated_subarray.sb_config.sbid
-    configuration = conf_types.ScanConfigurationByFile(
-        tmp_path, conf_types.ScanConfigurationType.STANDARD
-    )
-    allocated_subarray.clear_configuration_when_finished(exec_settings)
-    with wait_for(entry_point.set_waiting_for_configure(sub_array_id, receptors)):
-        entry_point.configure_subarray(
-            sub_array_id, receptors, configuration, sb_id, SCAN_DURATION
-        )
+
+    with context_monitoring.context_monitoring():
+        with allocated_subarray.wait_for_configuring_a_subarray(
+            configure_scan_test_exec_settings
+        ):
+            entry_point.configure_subarray(
+                sub_array_id, receptors, configuration, sb_id, SCAN_DURATION
+            )
 
 
 @then("the subarray must be in the READY state")

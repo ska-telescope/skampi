fcc22465c1fe0b10c261aea53165e0b30f609cf4
Gerhard le Roux <gerhardlr@gmail.com>
2022-02-08 12:44:38 +0200

    AT-111: add test for configure scan on cbf

--- tests/integration/test_configure_scan.py
+++ tests/integration/test_configure_scan.py
@@ -53,12 +53,25 @@ def fxt_sdp_base_composition(tmp_path) -> conf_types.ScanConfiguration:
     return configuration
 
 
+@pytest.fixture(name="csp_base_configuration")
+def fxt_csp_base_configuration(tmp_path) -> conf_types.ScanConfiguration:
+    """Setup a base scan configuration to use for csp/cbf.
+
+    :param tmp_path: a temporary path for sending configuration as a file.
+    :return: the configuration settings.
+    """
+    configuration = conf_types.ScanConfigurationByFile(
+        tmp_path, conf_types.ScanConfigurationType.STANDARD
+    )
+    return configuration
+
+
 # log capturing
 
 
 @pytest.fixture(name="set_up_log_checking_for_sdp")
 @pytest.mark.usefixtures("set_sdp_entry_point")
-def fxt_set_up_log_capturing_for_cbf(log_checking: fxt_types.log_checking):
+def fxt_set_up_log_capturing_for_sdp(log_checking: fxt_types.log_checking):
     """Set up log capturing (if enabled by CATPURE_LOGS).
 
     :param log_checking: The skallop log_checking fixture to use
@@ -67,6 +80,19 @@ def fxt_set_up_log_capturing_for_cbf(log_checking: fxt_types.log_checking):
         tel = names.TEL()
         sdp_subarray = str(tel.sdp.subarray(SUB_ARRAY_ID))
         log_checking.capture_logs_from_devices(sdp_subarray)
+
+
+@pytest.fixture(name="set_up_log_checking_for_cbf")
+@pytest.mark.usefixtures("set_cbf_entry_point")
+def fxt_set_up_log_checking_for_cbf(log_checking: fxt_types.log_checking):
+    """Set up log capturing (if enabled by CATPURE_LOGS).
+
+    :param log_checking: The skallop log_checking fixture to use
+    """
+    if os.getenv("CAPTURE_LOGS"):
+        tel = names.TEL()
+        cbf_subarray = str(tel.csp.cbf.subarray(SUB_ARRAY_ID))
+        log_checking.capture_logs_from_devices(cbf_subarray)
 
 
 @pytest.mark.skip(reason="test still in dev phase")
@@ -85,6 +111,11 @@ def test_configure_scan_on_sdp_subarray_in_mid():
     """Configure scan on sdp subarray in mid."""
 
 
+@scenario("features/cbf_configure_scan.feature", "Configure scan on CBF mid subarray")
+def test_configure_scan_on_cbf_mid_subarray():
+    """Configure scan on CBF mid subarray."""
+
+
 @given("an SDP subarray in IDLE state", target_fixture="configuration")
 def an_sdp_subarray_in_idle_state(
     configure_scan_test_exec_settings,  # pylint: disable=unused-argument
@@ -99,6 +130,22 @@ def an_sdp_subarray_in_idle_state(
     # will use default composition for the allocated subarray
     # subarray_allocation_spec.composition
     return sdp_base_configuration
+
+
+@given("an CBF subarray in IDLE state", target_fixture="configuration")
+def an_cbf_subarray_in_idle_state(
+    configure_scan_test_exec_settings,  # pylint: disable=unused-argument
+    set_cbf_entry_point,  # pylint: disable=unused-argument
+    set_up_log_checking_for_cbf,  # pylint: disable=unused-argument
+    csp_base_configuration: conf_types.ScanConfiguration,
+    subarray_allocation_spec: fxt_types.subarray_allocation_spec,
+) -> conf_types.ScanConfiguration:
+    """Given an CBF subarray in IDLE state."""
+    subarray_allocation_spec.receptors = RECEPTORS
+    subarray_allocation_spec.subarray_id = SUB_ARRAY_ID
+    # will use default composition for the allocated subarray
+    # subarray_allocation_spec.composition
+    return csp_base_configuration
 
 
 @when("I configure it for a scan")
@@ -131,6 +178,18 @@ def the_subarray_must_be_in_the_ready_state(
     sub_array_id = allocated_subarray.id
     tel = names.TEL()
     sdp_subarray = con_config.get_device_proxy(tel.sdp.subarray(sub_array_id))
+    result = sdp_subarray.read_attribute("obsstate").value
+    assert_that(result).is_equal_to(ObsState.READY)
+
+
+@then("the CBF subarray must be in the READY state")
+def the_cbf_subarray_must_be_in_the_ready_state(
+    allocated_subarray: fxt_types.allocated_subarray,
+):
+    """the subarray must be in the READY state."""
+    sub_array_id = allocated_subarray.id
+    tel = names.TEL()
+    sdp_subarray = con_config.get_device_proxy(tel.csp.cbf.subarray(sub_array_id))
     result = sdp_subarray.read_attribute("obsstate").value
     assert_that(result).is_equal_to(ObsState.READY)
 

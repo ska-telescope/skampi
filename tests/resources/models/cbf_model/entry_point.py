b6182a7bcf419aeb9aea4c0b06a6d96856a62b20
Gerhard le Roux <gerhardlr@gmail.com>
2022-02-09 12:49:11 +0200

    AT-111: update cbf entry point

--- tests/resources/models/cbf_model/entry_point.py
+++ tests/resources/models/cbf_model/entry_point.py
@@ -25,7 +25,6 @@ class CBFEntryPoint(SynchedEntryPoint):
         self._tel = names.TEL()
         self.cbf_controller = con_config.get_device_proxy(self._tel.csp.cbf.controller)
         self._live_logging = bool(os.getenv("DEBUG"))
-        self._receptors: List[int] = []
 
     def _log(self, mssage: str):
         if self._live_logging:
@@ -55,11 +54,10 @@ class CBFEntryPoint(SynchedEntryPoint):
     ) -> waiting.MessageBoardBuilder:
         builder = waiting.get_message_board_builder()
         self._tel = names.TEL()
-        if self._tel.skamid:
-            subarray_name = self._tel.skamid.csp.cbf.subarray(sub_array_id)
-            builder.set_waiting_on(subarray_name).for_attribute(
-                "obsState"
-            ).to_become_equal_to("IDLE")
+        subarray_name = self._tel.csp.cbf.subarray(sub_array_id)
+        builder.set_waiting_on(subarray_name).for_attribute(
+            "obsState"
+        ).to_become_equal_to("IDLE")
 
         return builder
 
@@ -76,7 +74,14 @@ class CBFEntryPoint(SynchedEntryPoint):
             subarray = con_config.get_device_proxy(subarray_name)
             self._log(f"commanding {subarray_name} with AddReceptors: {dish_ids} ")
             subarray.command_inout("AddReceptors", dish_ids)
-            self._receptors = dish_ids
+        elif self._tel.skalow:
+            subarray_name = self._tel.skalow.csp.cbf.subarray(sub_array_id)
+            subarray = con_config.get_device_proxy(subarray_name)
+            cbf_low_configuration = json.dumps(cbf_low_assign_resources)
+            self._log(
+                f"commanding {subarray_name} with AssignResources: {cbf_low_configuration} "
+            )
+            subarray.command_inout("AssignResources", cbf_low_configuration)
 
     def set_waiting_for_configure(
         self, sub_array_id: int, receptors: List[int]
@@ -113,6 +118,14 @@ class CBFEntryPoint(SynchedEntryPoint):
                 f"commanding {subarray_name} with ConfigureScan: {cbf_standard_configuration} "
             )
             subarray.command_inout("ConfigureScan", cbf_standard_configuration)
+        if self._tel.skalow:
+            subarray_name = self._tel.skalow.csp.cbf.subarray(sub_array_id)
+            subarray = con_config.get_device_proxy(subarray_name)
+            cbf_low_configuration = json.dumps(cbf_low_configure_scan)
+            self._log(
+                f"commanding {subarray_name} with ConfigureScan: {cbf_low_configuration} "
+            )
+            subarray.command_inout("ConfigureScan", cbf_low_configuration)
 
     def set_waiting_for_clear_configure(
         self, sub_array_id: int, receptors: List[int]
@@ -167,3 +180,110 @@ class CBFEntryPoint(SynchedEntryPoint):
             subarray = con_config.get_device_proxy(subarray_name)
             self._log(f"commanding {subarray_name} with RemoveAllReceptors")
             subarray.command_inout("RemoveAllReceptors")
+        if self._tel.skalow:
+            subarray_name = self._tel.skalow.csp.cbf.subarray(sub_array_id)
+            subarray = con_config.get_device_proxy(subarray_name)
+            self._log(f"commanding {subarray_name} with ReleaseAllResources")
+            subarray.command_inout("ReleaseAllResources")
+
+
+cbf_low_assign_resources = {
+    "common": {"subarrayID": 1},
+    "lowcbf": {
+        "stations": [
+            {"station_id": 1, "sub_station_id": 1},
+            {"station_id": 3, "sub_station_id": 1},
+            {"station_id": 3, "sub_station_id": 2},
+        ],
+        "station_beams": [
+            {
+                "station_beam_id": 1,
+                "channels": [1, 2, 3, 4, 5, 6, 7, 8],
+                "pst_beams": [{"pst_beam_id": 1}, {"pst_beam_id": 2}],
+            },
+            {
+                "station_beam_id": 2,
+                "channels": [9, 10, 11, 12, 13, 14, 15],
+                "pst_beams": [{"pst_beam_id": 3}],
+            },
+        ],
+    },
+}
+
+cbf_low_configure_scan = {
+    "id": 1,
+    "scanId": 1,
+    "stationType": 0,
+    "common": {"id": 1},
+    "lowcbf": {
+        "jones_source": "tango://host:port/domain/family/member",
+        "station_beams": [
+            {
+                "station_beam_id": 1,
+                "station_delay_src": "tango://host:port/domain/family/member",
+                "visibility_dest": [
+                    {"dest_ip": "10.0.2.1", "dest_mac": "02:00:00:00:02:01"}
+                ],
+                "pst_beams": [
+                    {
+                        "pst_beam_id": 1,
+                        "pst_beam_delay_src": "tango://host:port/domain/family/member",
+                        "pst_beam_dest": [
+                            {
+                                "dest_ip": "10.0.3.1",
+                                "dest_mac": "02:00:00:00:03:01",
+                                "channels": 200,
+                            },
+                            {
+                                "dest_ip": "10.0.3.2",
+                                "dest_mac": "02:00:00:00:03:02",
+                                "channels": 500,
+                            },
+                        ],
+                    },
+                    {
+                        "pst_beam_id": 2,
+                        "pst_beam_delay_src": "tango://host:port/domain/family/member",
+                        "pst_beam_dest": [
+                            {
+                                "dest_ip": "10.0.3.3",
+                                "dest_mac": "02:00:00:00:03:03",
+                                "channels": 200,
+                            },
+                            {
+                                "dest_ip": "10.0.3.4",
+                                "dest_mac": "02:00:00:00:03:04",
+                                "channels": 500,
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                "station_beam_id": 2,
+                "station_delay_src": "tango://host:port/domain/family/member",
+                "visibility_dest": [
+                    {"dest_ip": "10.0.3.3", "dest_mac": "02:00:00:00:03:03"}
+                ],
+                "pst_beams": [
+                    {
+                        "pst_beam_id": 3,
+                        "pst_beam_delay_src": "tango://host:port/domain/family/member",
+                        "pst_beam_dest": [
+                            {
+                                "dest_ip": "10.0.2.5",
+                                "dest_mac": "02:00:00:00:02:05",
+                                "channels": 200,
+                            },
+                            {
+                                "dest_ip": "10.0.2.6",
+                                "dest_mac": "02:00:00:00:02:06",
+                                "channels": 500,
+                            },
+                        ],
+                    }
+                ],
+            },
+        ],
+    },
+}

fa5577349b032062a26634f8e745b2f42a83754e
SKAJohanVenter <aventer@ska.ac.za>
2022-02-09 18:33:59 +0200

    SAR-355 Added dishmaster sim charts and tests

--- resources/skampi.mk
+++ resources/skampi.mk
@@ -206,3 +206,14 @@ skampi-test-02skuidservice:  ## launcher for skuid tests
 	@version=$$(helm dependency list charts/$(DEPLOYMENT_CONFIGURATION) | awk '$$1 == "ska-ser-skuid" {print $$2}'); \
 	telescope=$$(echo $(DEPLOYMENT_CONFIGURATION) | sed s/-/_/ | sed s/ska/SKA/); \
 	make skampi-k8s-test K8S_TEST_IMAGE_TO_TEST=artefact.skao.int/ska-ser-skuid:$$version MARK="$$telescope and acceptance"
+
+## TARGET: skampi-test-03dishmaster-sim
+## SYNOPSIS: make skampi-test-03dishmaster-sim
+## HOOKS: none
+## VARS: none
+##  make target for running dishmaster simulator component's acceptance tests in the SKAMPI CI pipeline.
+
+skampi-test-03dishmaster-sim:  ## launcher for dishmaster tests
+	@version=$$(helm dependency list charts/$(DEPLOYMENT_CONFIGURATION) | awk '$$1 == "ska-sim-dishmaster" {print $$2}'); \
+	telescope=$$(echo $(DEPLOYMENT_CONFIGURATION) | sed s/-/_/ | sed s/ska/SKA/); \
+	make skampi-k8s-test K8S_TEST_IMAGE_TO_TEST=artefact.skao.int/ska-sim-dishmaster:$$version MARK="$$telescope and acceptance"

852980eee74931282e8d4bca8829fbab26aea9af
Gerhard le Roux <gerhardlr@gmail.com>
2022-02-09 06:18:33 +0200

    AT-111: make implementation errors explicit

--- tests/resources/models/mvp_model/session_entry_point.py
+++ tests/resources/models/mvp_model/session_entry_point.py
@@ -1,35 +1,39 @@
+"""Entrypoint to use for session wide commands."""
 import logging
-from typing import Callable, List, Union, cast
-
+from typing import Callable, List, Union
 from ska_ser_skallop.mvp_control.entry_points.base import EntryPoint
-from ska_ser_skallop.mvp_control.describing.mvp_names import TEL
-from ska_ser_skallop.utils.piping import ConcurrentTaskExecutionError
-from typing import cast
-
-from ska_ser_skallop.mvp_control.infra_mon.configuration import (
-    get_mvp_release,
-    DevicesChart,
-)
+from .waiting import MessageBoardBuilder
 
 logger = logging.getLogger(__name__)
 
-from .waiting import MessageBoardBuilder, get_message_board_builder
 
 # TODO implement telescope on of commands for used in a session wide context
 
 
 def get_session_entry_point() -> Callable[[], EntryPoint]:
-    tel = TEL()
-    if tel.skalow:
-        return SessionEntryPointLow
-    return EntryPoint
+    """Return an instance of session point
+    :return: [description]
+    :rtype: Callable[[], EntryPoint]
+    """
+    return SessionEntryPointLow
 
 
 class SessionEntryPointLow(EntryPoint):
+    """[summary]
+
+    :param EntryPoint: [description]
+    :type EntryPoint: [type]
+    """
+
     def set_waiting_for_offline_components_to_become_online(
         self,
     ) -> Union[MessageBoardBuilder, None]:
-        pass
+        """[summary]
+
+        :return: [description]
+        :rtype: Union[MessageBoardBuilder, None]
+        """
+        return None
 
         # disable as mccs are flaky
         # if not (brd := super().set_waiting_for_offline_components_to_become_online()):
@@ -41,8 +45,8 @@ class SessionEntryPointLow(EntryPoint):
         # return brd
 
     def set_offline_components_to_online(self):
+        """[summary]"""
         pass
-
         # logging.info("command mccs components to go into online")
         # chart = cast(DevicesChart, get_mvp_release().sub_charts["ska-low-mccs"])
         # try:
@@ -51,3 +55,141 @@ class SessionEntryPointLow(EntryPoint):
         #    logger.exception(error)
         #    logger.warning("Will attempt to write attributes again after failure...")
         #    chart.devices.write_attributes("adminmode", 0, in_series=True)
+
+    def set_telescope_to_running(self):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_telescope_to_standby(self):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def tear_down_subarray(self, sub_array_id: int):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def compose_subarray(
+        self,
+        sub_array_id: int,
+        dish_ids: List[int],
+        composition,
+        sb_id: str,
+    ):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def clear_configuration(self, sub_array_id: int):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def configure_subarray(
+        self,
+        sub_array_id: int,
+        dish_ids: List[int],
+        configuration,
+        sb_id: str,
+        duration: float,
+    ):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def scan(self, sub_array_id: int):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_wating_for_scan_completion(self, sub_array_id: int, receptors: List[int]):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_wating_for_start_up(self):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_waiting_for_assign_resources(self, sub_array_id: int):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_waiting_for_configure(self, sub_array_id: int, receptors: List[int]):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_waiting_until_configuring(self, sub_array_id: int, receptors: List[int]):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_waiting_until_scanning(self, sub_array_id: int, receptors: List[int]):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_waiting_until_resourcing(self, sub_array_id: int):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_waiting_for_clear_configure(self, sub_array_id: int, receptors: List[int]):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_waiting_for_obsreset(self, sub_array_id: int, receptors: List[int]):
+        """[summary]
+
+        :param sub_array_id: [description]
+        :param receptors: [description]
+
+        :return: [description]
+        """
+
+    def set_waiting_for_release_resources(self, sub_array_id: int):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def set_wating_for_shut_down(
+        self,
+    ):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def abort_subarray(self, sub_array_id: int):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )
+
+    def reset_subarray(self, sub_array_id: int):
+        raise NotImplementedError(
+            "Session entry point does not implement session wide behavior,"
+            " please set DISABLE_MAINTAIN_ON=True"
+        )

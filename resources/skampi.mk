b47e5127ed89ed1eb38241c5c05a5b418a7d1913
SKAJohanVenter <aventer@ska.ac.za>
2022-02-10 07:36:09 +0200

    SAR-355 Updated image to test

--- resources/skampi.mk
+++ resources/skampi.mk
@@ -216,4 +216,4 @@ skampi-component-tests:  ## iterate over Skampi component tests defined as make 
 skampi-test-03dishmaster-sim:  ## launcher for dishmaster tests
 	@version=$$(helm dependency list charts/$(DEPLOYMENT_CONFIGURATION) | awk '$$1 == "ska-sim-dishmaster" {print $$2}'); \
 	telescope=$$(echo $(DEPLOYMENT_CONFIGURATION) | sed s/-/_/ | sed s/ska/SKA/); \
-	make skampi-k8s-test K8S_TEST_IMAGE_TO_TEST=registry.gitlab.com/ska-telescope/ska-sim-dishmaster/ska-sim-dishmaster:2.0.1-dev.cf140ed39 MARK="$$telescope and acceptance"
+	make skampi-k8s-test K8S_TEST_IMAGE_TO_TEST=registry.gitlab.com/ska-telescope/ska-sim-dishmaster/ska-sim-dishmaster:2.0.1-dev.ceec297ed MARK="$$telescope and acceptance"

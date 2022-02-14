e1938f3a1ced865a83b50d4abdeed6f2083c29b2
Mark Ashdown <maja1@mrao.cam.ac.uk>
2022-02-14 17:35:15 +0000

    ORC-1167: Update SDP to 0.8.3 and use released image in tests

--- resources/skampi.mk
+++ resources/skampi.mk
@@ -240,4 +240,4 @@ skampi-test-02skuidservice:  ## launcher for skuid tests
 skampi-test-03sdp:  ## launcher for SDP tests
 	@version=$$(helm dependency list charts/$(DEPLOYMENT_CONFIGURATION) | awk '$$1 == "ska-sdp" {print $$2}'); \
 	telescope=$$(echo $(DEPLOYMENT_CONFIGURATION) | sed s/-/_/ | sed s/ska/SKA/); \
-	make skampi-k8s-test-component K8S_TEST_IMAGE_TO_TEST=registry.gitlab.com/ska-telescope/sdp/ska-sdp-integration/ska-sdp-integration-tests:$$version MARK="$$telescope and acceptance"
+	make skampi-k8s-test-component K8S_TEST_IMAGE_TO_TEST=artefact.skao.int/ska-sdp-integration-tests:$$version MARK="$$telescope and acceptance"

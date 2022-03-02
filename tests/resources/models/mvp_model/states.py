452b71e3b5a3f99e9c6d9941947521ca9ecd4d2b
Gerhard le Roux <gerhardlr@gmail.com>
2022-03-02 12:43:20 +0200

    AT-135: add models package for managing domain logic of mcp subsystems

---
+++ tests/resources/models/mvp_model/states.py
@@ -0,0 +1,19 @@
+"""module containing values for interpreting enumerated values (e.g. ObsState)"""
+
+import enum
+
+
+class ObsState(enum.IntEnum):
+    """Representation of int ObsState as an Enum."""
+
+    EMPTY = 0
+    RESOURCING = 1
+    IDLE = 2
+    CONFIGURING = 3
+    READY = 4
+    SCANNING = 5
+    ABORTING = 6
+    ABORTED = 7
+    RESETTING = 8
+    FAULT = 9
+    RESTARTING = 10

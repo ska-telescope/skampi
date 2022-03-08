44d7a9a33d91bfaf94a8c874b3a93ee38c4edf63
Gerhard le Roux <gerhardlr@gmail.com>
2022-03-08 13:27:36 +0200

    AT-135: remove the use of global var

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,30 +1,42 @@
+"""pytest global settings and utility like fixtures."""
 import logging
+from types import SimpleNamespace
 
 from typing import Callable
 from mock import patch, Mock
 
 import pytest
 
-
+from ska_ser_skallop.mvp_fixtures.fixtures import fxt_types
 from resources.models.sdp_model.entry_point import SDPEntryPoint
 from resources.models.sdp_model.mocking import setup_sdp_mock
-from ska_ser_skallop.mvp_fixtures.fixtures import fxt_types
 
 
 logger = logging.getLogger(__name__)
 
-MOCK_SUT = False
-NR_OF_SUBARRAYS = 2
+
+class ConfTestSettings(SimpleNamespace):
+    """Object representing env like settings for fixtures in conftest."""
+
+    mock_sut: bool = False
+    nr_of_subarrays = 2
+
+
+@pytest.fixture(name="conftest_settings")
+def fxt_conftest_settings() -> ConfTestSettings:
+    """Fixture to use for setting env like settings for fixtures in conftest"""
+    return ConfTestSettings()
 
 
 @pytest.fixture(name="run_mock")
-def fxt_run_mock_wrapper(request, _pytest_bdd_example):
+def fxt_run_mock_wrapper(
+    request, _pytest_bdd_example, conftest_settings: ConfTestSettings
+):
     """Fixture that returns a function to use for running a test as a mock."""
 
     def run_mock(mock_test: Callable):
         """Test the test using a mock SUT"""
-        global MOCK_SUT  # pylint: disable=global-statement
-        MOCK_SUT = True
+        conftest_settings.mock_sut = True
         # pylint: disable-next=too-many-function-args
         with patch(
             "ska_ser_skallop.mvp_fixtures.fixtures.TransitChecking"
@@ -36,11 +48,14 @@ def fxt_run_mock_wrapper(request, _pytest_bdd_example):
 
 
 @pytest.fixture(name="set_sdp_entry_point")
-def fxt_set_entry_point(set_session_exec_env: fxt_types.set_session_exec_env):
+def fxt_set_entry_point(
+    set_session_exec_env: fxt_types.set_session_exec_env,
+    conftest_settings: ConfTestSettings,
+):
     """Fixture to use for setting up the entry point as from only the interface to sdp."""
     exec_env = set_session_exec_env
-    if not MOCK_SUT:
-        SDPEntryPoint.nr_of_subarrays = NR_OF_SUBARRAYS
+    if not conftest_settings.mock_sut:
+        SDPEntryPoint.nr_of_subarrays = conftest_settings.nr_of_subarrays
         exec_env.entrypoint = SDPEntryPoint
     else:
         exec_env.entrypoint = "mock"

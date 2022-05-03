4638ee162b6945cfd3bfba2faa804117b2916f41
Alexander Soderqvist <alexander.soderqvist@cosylab.com>
2022-05-03 15:23:41 +0200

    CT-666 CSP Configure low

--- tests/integration/csp/test_configure.py
+++ tests/integration/csp/test_configure.py
@@ -18,3 +18,13 @@ logger = logging.getLogger(__name__)
 )
 def test_configure_csp_mid_subarray():
     """Configure CSP low subarray."""
+
+@pytest.mark.skalow
+@pytest.mark.csp
+@pytest.mark.configure
+@scenario(
+    "features/csp_configure_scan.feature",
+    "Configure scan on csp subarray in low",
+)
+def test_configure_csp_low_subarray():
+    """Configure CSP low subarray."""

066f0ef8f58b355ea33fef3d0dcb42b2e4b71e42
shraddhabajare <shraddhab.ska@gmail.com>
2022-05-06 12:31:46 +0530

    AT1-1129 - Modify startup test module to include tmc Off command

--- tests/integration/tmc/test_start_up.py
+++ tests/integration/tmc/test_start_up.py
@@ -23,6 +23,15 @@ def test_tmc_start_up_telescope_mid_sdp_only():
     """Start up the telescope in mid  (sdp only)."""
 
 
+@pytest.mark.skamid
+@pytest.mark.standby
+@scenario(
+    "features/tmc_start_up_telescope.feature", "Switch of the telescope (SDP only)"
+)
+def test_tmc_off_telescope_mid_sdp_only():
+    """Off the telescope in mid  (sdp only)."""
+
+
 @given("an TMC")
 def a_tmc():
     """an TMC"""
@@ -33,10 +42,18 @@ def a_partial_telescope():
     """an partial Telescope consisting of SDP only"""
 
 
+@given("an partial Telescope consisting of SDP only that is ON")
+def a_partial_telescope_with_sdp_on():
+    """an partial Telescope consisting of SDP only that is ON"""
+
 # when
 # use @when("I start up the telescope") from ..conftest
 
+# when
+# use @when("I switch off the telescope") from ..conftest
+
 # thens
+
 
 @then("the sdp must be on")
 def the_sdp_must_be_on(sut_settings: conftest.SutTestSettings):
@@ -49,3 +66,16 @@ def the_sdp_must_be_on(sut_settings: conftest.SutTestSettings):
         subarray = con_config.get_device_proxy(tel.sdp.subarray(index))
         result = subarray.read_attribute("state").value
         assert_that(str(result)).is_equal_to("ON")
+
+
+@then("the sdp must be off")
+def the_sdp_must_be_off(sut_settings: conftest.SutTestSettings):
+    """the sdp must be off."""
+    tel = names.TEL()
+    sdp_master = con_config.get_device_proxy(tel.sdp.master)
+    result = sdp_master.read_attribute("state").value
+    assert_that(str(result)).is_equal_to("OFF")
+    for index in range(1, sut_settings.nr_of_subarrays + 1):
+        subarray = con_config.get_device_proxy(tel.sdp.subarray(index))
+        result = subarray.read_attribute("state").value
+        assert_that(str(result)).is_equal_to("OFF")

20e395638d1caa703a41a2c73ac4358f1b9f6fb6
Gerhard le Roux <gerhardlr@gmail.com>
2022-06-20 19:31:42 +0000

    AT1-1135: remove automatic failure

--- tests/integration/test_tango_basic.py
+++ tests/integration/test_tango_basic.py
@@ -25,9 +25,6 @@ def test_tangogql_service_available():
 def fxt_inject_build_out():
     """Inject a value into os env for using build out"""
     original_value = os.environ["TEST_ENV"]
-    os_environ = json.dumps(os.environ, indent=2, default=str)
-    logging.info(f"Env Vars:{os_environ}")
-    assert False
     os.environ["TEST_ENV"] = "BUILD_OUT"
     yield
     os.environ["TEST_ENV"] = original_value

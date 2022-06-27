fbbe51239617b2c9dfa659e45b1091f764bee22d
Adriaan de Beer <adebeer42@gmail.com>
2022-06-27 23:35:57 +0200

    SKB-155 fixed error and implemented test to check if fixversion is correctly set

--- tests/features/xray_upload.feature
+++ tests/features/xray_upload.feature
@@ -1,10 +1,19 @@
 @XTP-3361
-Feature: Cucumber test results are uploaded to XRay
-	#Cucumber test results are uploaded to XRay after CI job execution.
+Feature: Cucumber test results are uploaded to XRay from k8s cluster
+	#Cucumber test results are uploaded to XRay after CI job execution in a kubernetes cluster.
 
 	
-	@XTP-3346 @XTP-3347 @XTP-3348
+	@XTP-3346 @XTP-3347 @XTP-6104 @XTP-3348 @XTP-4804
 	Scenario: SKAMPI CI Pipeline tests execute on SKAMPI
 		Given a Continuous Integration Pipeline
 		When an attempt is made to run tests within the repository
 		Then the tests within the SKAMPI repository are run	
+
+	#Tests if test results are going to be linked to the currently in-progress PI FixVersion after execution
+	#This should be set in the test-exec.json file.
+	@XTP-9480
+	Scenario: Test if test-exec.json is configured to the latest Unreleased Version
+		Given the SKA Jira project for Solution and System Acceptance Tests (skampi tests) with project key XTP
+		And a test-exec.json file that contains a version parameter
+		When integration tests are uploaded after tests executed in Skampi master branch pipelines
+		Then the results will be associated with the Earliest Unreleased FixVersion in the XTP project
--- tests/unit/test_xray_upload.py
+++ tests/unit/test_xray_upload.py
@@ -1,6 +1,10 @@
 """Cucumber test results are uploaded to XRay feature tests."""
 
+from asyncio.log import logger
+from atlassian import Jira
 import pytest
+import os
+import json
 from pytest_bdd import (
     given,
     scenario,
@@ -33,3 +37,76 @@ def stuff_happens():
 def all_is_good():
     """the tests within the SKAMPI repository are run."""
     pass
+
+
+@scenario(
+    "features/xray_upload.feature",
+    "Test if test-exec.json is configured to the latest Unreleased Version",
+)
+def test_fix_version_is_correct():
+    """SKAMPI CI Pipeline tests execute on SKAMPI."""
+
+
+@given("the SKA Jira project for Solution and System Acceptance Tests (skampi tests) with project key XTP", target_fixture="unreleased_fixversion")
+def get_earliest_unreleased_fix_version():
+    """Get the earliest unreleased FixVersion in Jira project
+
+    :return: FixVersion is defined according to PIs
+    :rtype: str
+    """
+    url = os.environ["JIRA_URL"]
+    username = os.environ["JIRA_USERNAME"]
+    password = os.environ["JIRA_PASSWORD"]
+    # logger.info(f"URL: {url}, USERNAME: {username}, PASSWORD: {password}")
+    jira = Jira(url=url, username=username, password=password)
+    project_key = "XTP"
+    versions = jira.get_project_versions(key=project_key, expand="All")
+    unrel_vs = [v for v in versions if v['released'] == False]
+    filtered_vs = [v for v in unrel_vs if v['archived'] == False][0]
+    # logger.info(f"{versions}")
+    logger.info(f"{filtered_vs}")
+    return filtered_vs['name']
+    # pytest.unreleased_fixversion = filtered_vs['name']
+    # logger.info(f"Earliest Unreleased version: {pytest.unreleased_fixversion}")
+
+
+@given("a test-exec.json file that contains a version parameter", target_fixture="local_version")
+def fxt_load_file():
+    """Extract Fix Version in local config
+
+    :return: Current PI that all uploads will be associated with
+    :rtype: str
+    """
+    fn = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), os.path.pardir, "test-exec.json")
+    )
+    # assert isfile(fn), f"{fn} not found"
+
+    with open(fn, "r") as file:  # noqa
+        data = json.load(file)
+        # logger.info(f"Fixture 'data': {data}")
+        curr_pi = data["versions"][0]["maps_to"]["master"]
+    logger.info(f"Local FixVersion set to {curr_pi}")
+    return curr_pi
+
+
+@when(
+    "integration tests are uploaded after tests executed in Skampi master branch pipelines"
+)
+def get_fix_version_from_test_exec_json_file():
+    """This step will happen after all the tests were executed
+    """
+
+
+@then(
+    "the results will be associated with the Earliest Unreleased FixVersion in the XTP project"
+)
+def check_if_fix_version_matches(local_version: str, unreleased_fixversion: str):
+    """Test to see if they matched
+
+    :param current_pi_in_jira: fixture
+    :type current_pi_in_jira: str
+    :param local_version: fixture
+    :type local_version: str
+    """
+    assert local_version == unreleased_fixversion, f"{local_version} in test-exec.json; earliest unreleased version in Jira: {unreleased_fixversion}"

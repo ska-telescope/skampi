b5efe723b713d222e98bc3d8f081ab00d2b2fdc7
akshaysonawane <akshayss.ska@gmail.com>
2022-06-30 18:32:28 +0530

    HM-3 : Resolve review comments

--- tests/resources/models/csp_model/entry_point.py
+++ tests/resources/models/csp_model/entry_point.py
@@ -62,7 +62,7 @@ class StartUpStep(base.ObservationStep, LogEnabled):
             # we wait for cbf vccs to be in proper initialised state
             brd.set_waiting_on(self._tel.csp.cbf.controller).for_attribute(
                 "reportVccState"
-            ).to_become_equal_to("[0, 0, 0, 0]", ignore_first=False)
+            ).to_become_equal_to(["[0, 0, 0, 0]", "[0 0 0 0]"], ignore_first=False)
         for index in range(1, self.nr_of_subarrays + 1):
             brd.set_waiting_on(self._tel.csp.subarray(index)).for_attribute(
                 "state"

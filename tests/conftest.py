b655789646494143578005c5b216e53d8bf8cd9c
akshaysonawane <akshayss.ska@gmail.com>
2022-07-05 11:01:12 +0530

    HM-3 : update timeout

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,6 +37,10 @@ def pytest_addoption(parser):
         ),
     )
 
+@pytest.fixture(autouse=True)
+def override_timeouts(exec_settings):
+    exec_settings.time_out = 3
+
 # @pytest.fixture
 # def tango_context(devices_to_load, request):
 #     true_context = request.config.getoption("--true-context")

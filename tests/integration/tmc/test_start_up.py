5f323030d2d7d32a42bd8c19dd9f046e0c8f7e49
devesh pandey <devesh.ska@gmail.com>
2022-07-06 18:06:20 +0530

    HM-3:Adding checks in given

--- tests/integration/tmc/test_start_up.py
+++ tests/integration/tmc/test_start_up.py
@@ -42,34 +42,35 @@ def a_tmc():
     central_node = con_config.get_device_proxy(central_node_name)
     result=central_node.ping()
     assert result>0
-
-    for index in range(1, sut_settings.nr_of_subarrays + 1):
-        sdp_subarray = con_config.get_device_proxy(tel.tm.subarray(index))
-        result=sdp_subarray.ping()
-        assert result>0
-
-    sdp_master = con_config.get_device_proxy(tel.sdp.master)
-    result=sdp_master.ping()
-    assert result>0
-    for index in range(1, sut_settings.nr_of_subarrays + 1):
-        sdp_subarray = con_config.get_device_proxy(tel.sdp.subarray(index))
-        result=sdp_subarray.ping()
-        assert result>0
-
-    csp_master = con_config.get_device_proxy(tel.csp.controller)
-    result=csp_master.ping()
-    assert result>0
-
-    for index in range(1, sut_settings.nr_of_subarrays + 1):
-        csp_subarray = con_config.get_device_proxy(tel.csp.subarray(index))
-        result=csp_subarray.ping()
-        assert result>0
-
-    for dish in tel.skamid.dishes(sut_settings.receptors):
-        dish = con_config.get_device_proxy(dish)
-        result= dish.ping()
-        assert result>0
     
+    for index in range(1, sut_settings.nr_of_subarrays + 1):
+        subarray_node= con_config.get_device_proxy(tel.tm.subarray(index))
+        result=subarray_node.ping()
+        assert result>0
+
+    csp_master_leaf_node = con_config.get_device_proxy(tel.tm.csp_leaf_node)
+    result=csp_master_leaf_node.ping()
+    assert result>0
+
+    sdp_master_leaf_node = con_config.get_device_proxy(tel.tm.sdp_leaf_node)
+    result=sdp_master_leaf_node.ping()
+    assert result>0
+
+    for index in range(1, sut_settings.nr_of_subarrays + 1):
+        csp_subarray_leaf_node=tel.tm.subarray(index).csp_leaf_node
+        result=csp_subarray_leaf_node.ping()
+        assert result>0
+
+    for index in range(1, sut_settings.nr_of_subarrays + 1):
+        sdp_subarray_leaf_node=tel.tm.subarray(index).sdp_leaf_node
+        result=sdp_subarray_leaf_node.ping()
+        assert result>0
+
+    for index in range(1, sut_settings.nr_of_subarrays + 1):
+        dish_leaf_nodes=tel.tm.dish_leafnode(index)
+        result=dish_leaf_nodes.ping()
+        assert result>0
+
 
 
 

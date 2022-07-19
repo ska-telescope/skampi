f9c8b0d2589bd3f78044f943badd7162f59ac3ad
Gerhard le Roux <gerhardlr@gmail.com>
2022-07-19 14:16:25 +0000

    SAH-1143: exclude tm from scope until bug fixed in skallop

--- tests/integration/tmc/conftest.py
+++ tests/integration/tmc/conftest.py
@@ -30,8 +30,14 @@ def fxt_set_entry_point(
     TMCEntryPoint.receptors = sut_settings.receptors
     exec_env.entrypoint = TMCEntryPoint
     #  TODO  determine correct scope for readiness checks to work
-    exec_env.scope = ["tm", "mid", "sdp", "csp","tmc scope", "csp scope", "csp control", "sdp control"]
-
+    exec_env.scope = [
+        "mid",
+        "sdp",
+        "csp",
+        "csp scope",
+        "csp control",
+        "sdp control",
+    ]
 
 
 @pytest.fixture(name="nr_of_subarrays", autouse=True, scope="session")

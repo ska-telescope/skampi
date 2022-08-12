a10e422212922d59646f1d0cf95c936e9413b1ea
Ugur Yilmaz <ugur.yilmaz@skao.int>
2022-08-12 13:23:33 +0100

    ST-1260: disable tangogql test

--- tests/integration/test_tango_basic.py
+++ tests/integration/test_tango_basic.py
@@ -15,6 +15,7 @@ from ska_ser_skallop.connectors.configuration import get_device_proxy
 @pytest.mark.skamid
 @pytest.mark.skalow
 @pytest.mark.taranta
+@pytest.mark.skip
 @scenario("features/taranta_basic.feature", "TangoGQL service available")
 def test_tangogql_service_available():
     """TangoGQL service available."""

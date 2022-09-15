084a378d53889026857d6cdedbec47d3634438b9
Adriaan de Beer <adebeer42@gmail.com>
2022-09-15 15:00:40 +0200

    SKB-175 added back regression test to CI jobs

--- tests/features/xray_upload.feature
+++ tests/features/xray_upload.feature
@@ -2,16 +2,16 @@
 Feature: Cucumber test results are uploaded to XRay from k8s cluster
 	#Cucumber test results are uploaded to XRay after CI job execution in a kubernetes cluster.
 
-	
-	@XTP-3346 @XTP-3347 @XTP-3348
-	Scenario: SKAMPI CI Pipeline tests execute on SKAMPI
-		Given a Continuous Integration Pipeline
-		When an attempt is made to run tests within the repository
-		Then the tests within the SKAMPI repository are run	
+
+	# @XTP-3346 @XTP-3347 @XTP-3348
+	# Scenario: SKAMPI CI Pipeline tests execute on SKAMPI
+	# 	Given a Continuous Integration Pipeline
+	# 	When an attempt is made to run tests within the repository
+	# 	Then the tests within the SKAMPI repository are run
 
 	#Tests if test results are going to be linked to the currently in-progress PI FixVersion after execution
 	#This should be set in the test-exec.json file.
-	@XTP-9480 @XTP-3347 @XTP-3348
+	@XTP-9480 @XTP-3347 @XTP-3348 @XTP-3324 @XTP-3325
 	Scenario: Test if test-exec.json is configured to the latest Unreleased Version
 		Given the SKA Jira project for Solution and System Acceptance Tests (skampi tests) with project key XTP
 		And a test-exec.json file that contains a version parameter
--- tests/unit/test_xray_upload.py
+++ tests/unit/test_xray_upload.py
@@ -16,27 +16,27 @@ from pytest_bdd import (
 pytestmark = pytest.mark.infra
 
 
-@scenario("features/xray_upload.feature", "SKAMPI CI Pipeline tests execute on SKAMPI")
-def test_skampi_ci_pipeline_tests_execute_on_skampi():
-    """SKAMPI CI Pipeline tests execute on SKAMPI."""
+# @scenario("features/xray_upload.feature", "SKAMPI CI Pipeline tests execute on SKAMPI")
+# def test_skampi_ci_pipeline_tests_execute_on_skampi():
+#     """SKAMPI CI Pipeline tests execute on SKAMPI."""
 
 
-@given("a Continuous Integration Pipeline")
-def stuff():
-    """a Continuous Integration Pipeline."""
-    pass
+# @given("a Continuous Integration Pipeline")
+# def stuff():
+#     """a Continuous Integration Pipeline."""
+#     pass
 
 
-@when("an attempt is made to run tests within the repository")
-def stuff_happens():
-    """an attempt is made to run tests within the repository."""
-    pass
+# @when("an attempt is made to run tests within the repository")
+# def stuff_happens():
+#     """an attempt is made to run tests within the repository."""
+#     pass
 
 
-@then("the tests within the SKAMPI repository are run")
-def all_is_good():
-    """the tests within the SKAMPI repository are run."""
-    pass
+# @then("the tests within the SKAMPI repository are run")
+# def all_is_good():
+#     """the tests within the SKAMPI repository are run."""
+#     pass
 
 
 @scenario(

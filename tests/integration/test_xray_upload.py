90a01e97a9aa28e0f362901e93f47640ea164166
Adriaan de Beer <adebeer42@gmail.com>
2022-09-23 16:59:39 +0200

    SKB-175 added k8s mark

--- tests/integration/test_xray_upload.py
+++ tests/integration/test_xray_upload.py
@@ -12,33 +12,34 @@ from pytest_bdd import (
     when,
 )
 
-# declare pytestmark globally so that the whole scenario can be marked as infra
-# pytestmark = pytest.mark.infra
+
+# @pytest.mark.skamid
+@pytest.mark.k8s
+@scenario("features/xray_upload.feature", "SKAMPI CI Pipeline tests execute on SKAMPI")
+def test_skampi_ci_pipeline_tests_execute_on_skampi():
+    """SKAMPI CI Pipeline tests execute on SKAMPI."""
 
 
-# @scenario("features/xray_upload.feature", "SKAMPI CI Pipeline tests execute on SKAMPI")
-# def test_skampi_ci_pipeline_tests_execute_on_skampi():
-#     """SKAMPI CI Pipeline tests execute on SKAMPI."""
+@given("a Continuous Integration Pipeline")
+def stuff():
+    """a Continuous Integration Pipeline."""
+    pass
 
 
-# @given("a Continuous Integration Pipeline")
-# def stuff():
-#     """a Continuous Integration Pipeline."""
-#     pass
+@when("an attempt is made to run tests within the repository")
+def stuff_happens():
+    """an attempt is made to run tests within the repository."""
+    pass
 
 
-# @when("an attempt is made to run tests within the repository")
-# def stuff_happens():
-#     """an attempt is made to run tests within the repository."""
-#     pass
+@then("the tests within the SKAMPI repository are run")
+def all_is_good():
+    """the tests within the SKAMPI repository are run."""
+    pass
 
 
-# @then("the tests within the SKAMPI repository are run")
-# def all_is_good():
-#     """the tests within the SKAMPI repository are run."""
-#     pass
-
-
+# @pytest.mark.skamid
+@pytest.mark.k8s
 @scenario(
     "features/xray_upload.feature",
     "Test if test-exec.json is configured to the latest Unreleased Version",

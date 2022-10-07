f7897b36127fb7f08f22ade6a06e1774de237786
Bhushan Parakh <bhushan.ska@gmail.com>
2022-10-07 10:50:21 +0530

    HM-51: Reverting temporary fix

--- tests/resources/models/tmc_model/entry_point.py
+++ tests/resources/models/tmc_model/entry_point.py
@@ -457,7 +457,7 @@ class TMCEntryPoint(CompositeEntryPoint):
     def __init__(self) -> None:
         """Init Object"""
         super().__init__()
-        self.set_online_step = CSPSetOnlineStep(self.nr_of_subarrays)  # Temporary fix
+        # self.set_online_step = CSPSetOnlineStep(self.nr_of_subarrays)  # Temporary fix
         self.start_up_step = StartUpStep(self.nr_of_subarrays, self.receptors)
         self.assign_resources_step = AssignResourcesStep()
         self.configure_scan_step = ConfigureStep()

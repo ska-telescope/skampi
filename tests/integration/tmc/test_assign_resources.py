f72961da35a122e68d8b6485c368d7fad5e53d99
shraddhabajare <shraddhab.ska@gmail.com>
2022-10-13 11:49:57 +0530

    SAH-1204 - Execute on integration test only

--- tests/integration/tmc/test_assign_resources.py
+++ tests/integration/tmc/test_assign_resources.py
@@ -16,6 +16,7 @@ from ..conftest import SutTestSettings
 logger = logging.getLogger(__name__)
 
 # log capturing
+@pytest.mark.skip
 @pytest.mark.k8s
 @pytest.mark.k8sonly
 @pytest.mark.skamid
@@ -24,6 +25,7 @@ logger = logging.getLogger(__name__)
 def test_assign_resources_to_tmc_subarray_in_mid():
     """Assign resources to tmc subarray in mid."""
 
+@pytest.mark.skip
 @pytest.mark.k8s
 @pytest.mark.k8sonly
 @pytest.mark.skamid

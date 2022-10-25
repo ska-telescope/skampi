b1f21e13580a4c0ff4fe3ca5fed89427c1dec5da
user <jayant.ska@gmail.com>
2022-10-25 20:53:26 +0530

    SAH-1208: Test test_configure test case only

--- tests/integration/sdp/test_assign_resources.py
+++ tests/integration/sdp/test_assign_resources.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 def test_assign_resources_to_sdp_subarray_in_low():
     """Assign resources to sdp subarray in low."""
 
-
+@pytest.mark.skip
 @pytest.mark.skamid
 @pytest.mark.assign
 @scenario(

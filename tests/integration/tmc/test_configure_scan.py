772425c155ce4eb5f10d250fa188b3ad4cabc785
shraddhabajare <shraddhab.ska@gmail.com>
2022-10-27 12:14:33 +0530

    SAH-1208 - Update scenario and test implementation for configure scan

--- tests/integration/tmc/test_configure_scan.py
+++ tests/integration/tmc/test_configure_scan.py
@@ -37,12 +37,13 @@ def a_tmc():
     """an TMC"""
 
 
-@given("an telescope subarray", target_fixture="composition")
+@given("an telescope subarray", target_fixture="configuration")
 def an_telescope_subarray(
-    set_up_subarray_log_checking_for_tmc, base_composition: conf_types.Composition
-) -> conf_types.Composition:
+    set_up_subarray_log_checking_for_tmc, base_configuration: conf_types.ScanConfiguration,  subarray_allocation_spec: fxt_types.subarray_allocation_spec,
+    sut_settings: SutTestSettings,
+) -> conf_types.ScanConfiguration:
     """an telescope subarray."""
-    return base_composition
+    return base_configuration
 
 
 @given("a subarray in the IDLE state")

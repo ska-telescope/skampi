c3c992483867d585f8ad05e8e8026d7cab41ae4b
user <jayant.ska@gmail.com>
2022-10-28 17:50:03 +0530

    SAH-1211: Update test entry point for cspsln

--- tests/resources/models/tmc_model/leafnodes/cspln_entry_point.py
+++ tests/resources/models/tmc_model/leafnodes/cspln_entry_point.py
@@ -80,7 +80,7 @@ class CspLnAssignResourcesStep(CspAsignResourcesStep):
         :param sub_array_id: The index id of the subarray to control
         """
         csp_subarray_ln_name = self._tel.tm.subarray(sub_array_id).csp_leaf_node  # type: ignore
-        csp_subarray_ln = con_config.get_device_proxy(subarray_name)  # type: ignore
+        csp_subarray_ln = con_config.get_device_proxy(csp_subarray_ln_name)  # type: ignore
         self._log(f"Commanding {csp_subarray_ln_name} to ReleaseAllResources")
         csp_subarray_ln.command_inout("ReleaseAllResources")
 

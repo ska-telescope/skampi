712456ca61a18d445223422ea04c8873812eb480
Bhushan Parakh <bhushan.ska@gmail.com>
2022-11-02 11:30:31 +0530

    HM-74 Enabling assign release test cases

--- tests/integration/tmc/test_assign_resources.py
+++ tests/integration/tmc/test_assign_resources.py
@@ -19,7 +19,6 @@ logger = logging.getLogger(__name__)
 # log capturing
 
 
-@pytest.mark.skip(reason="currently failing")
 @pytest.mark.skamid
 @pytest.mark.assign
 @scenario("features/tmc_assign_resources.feature", "Assign resources to mid subarray")
@@ -27,7 +26,6 @@ def test_assign_resources_to_tmc_subarray_in_mid():
     """Assign resources to tmc subarray in mid."""
 
 
-@pytest.mark.skip("currently failing")
 @pytest.mark.skamid
 @scenario(
     "features/tmc_assign_resources.feature", "Release resources from mid subarray"

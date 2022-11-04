5b783eebac40ae2d05142b6f54938d036e66e0cb
Omprakash Verma <omprakash_verma@persistent.com>
2022-11-04 14:40:42 +0530

    NAK-223 Updated BDD test

--- tests/integration/oet/test_oet_configure_scan.py
+++ tests/integration/oet/test_oet_configure_scan.py
@@ -1,4 +1,5 @@
 """Configure scan on telescope subarray feature tests."""
+import logging
 import pytest
 from assertpy import assert_that
 from pytest_bdd import given,parsers, scenario, then, when
@@ -10,6 +11,8 @@ from ska_ser_skallop.mvp_fixtures.fixtures import fxt_types
 from resources.models.mvp_model.states import ObsState
 from ..conftest import SutTestSettings
 from .oet_helpers import ScriptExecutor
+
+logger = logging.getLogger(__name__)
 EXECUTOR = ScriptExecutor()
 
 @pytest.mark.k8s
@@ -54,16 +57,21 @@ def when_configure_resources_from_sbi(
             script_completion_state == "FAILED"
         ), f"Expected configure script to be COMPLETED, instead was {script_completion_state}"
 
-
-
-@then("the sub-array goes to ObsState {obsstate}")
-def the_subarray_must_be_in_the_ready_state(
-    sut_settings: SutTestSettings, integration_test_exec_settings: fxt_types.exec_settings
+@then(parsers.parse("the sub-array goes to ObsState {obsstate}"))
+def check_final_subarray_state(
+    obsstate: str,
+    sut_settings: SutTestSettings,
 ):
-    """the subarray must be in the READY state."""
+    """
+    Check that the final state of the sub-array is as expected.
+
+    Args:
+        obsstate (str): Sub-array Tango device ObsState
+    """
     tel = names.TEL()
-    integration_test_exec_settings.recorder.assert_no_devices_transitioned_after(
-        str(tel.tm.subarray(sut_settings.subarray_id)))
-    tmc_subarray = con_config.get_device_proxy(tel.tm.subarray(sut_settings.subarray_id))
-    result = tmc_subarray.read_attribute("obsState").value
-    assert_that(result).is_equal_to(ObsState.READY)
+    subarray = con_config.get_device_proxy(tel.tm.subarray(sut_settings.subarray_id))
+    subarray_state = ObsState(subarray.read_attribute("obsState").value).name
+    assert (
+        subarray_state == obsstate
+    ), f"Expected sub-array to be in {obsstate} but instead was in {subarray_state}"
+    logger.info("Sub-array is in ObsState %s", obsstate)

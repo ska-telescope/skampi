547cbdc7972174169db7c50a26a4ff24a1b342bf
shraddhabajare <shraddhab.ska@gmail.com>
2022-11-15 16:31:20 +0530

    SAH-1230 - Enable tmc bdd tests

--- tests/integration/tmc/test_scan.py
+++ tests/integration/tmc/test_scan.py
@@ -3,7 +3,6 @@
 import pytest
 from assertpy import assert_that
 from pytest_bdd import given, scenario, then
-
 from ska_ser_skallop.connectors import configuration as con_config
 from ska_ser_skallop.mvp_control.describing import mvp_names as names
 from ska_ser_skallop.mvp_control.entry_points import types as conf_types

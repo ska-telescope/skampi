33e076c145375cbae673ec71df6837dad012510d
Gerhard le Roux <gerhardlr@gmail.com>
2022-12-01 15:47:26 +0000

    SP-2761: implement abort and obsreset in csp entrypoint

--- tests/resources/models/csp_model/entry_point.py
+++ tests/resources/models/csp_model/entry_point.py
@@ -13,10 +13,13 @@ from ska_ser_skallop.mvp_control.entry_points import base
 from ska_ser_skallop.mvp_control.entry_points.composite import (
     CompositeEntryPoint,
     MessageBoardBuilder,
+    AbortStep,
+    ObsResetStep,
 )
 from ska_ser_skallop.utils.singleton import Memo
 
 from ..mvp_model.env import get_observation_config, Observation
+from ..mvp_model.states import ObsState
 
 
 logger = logging.getLogger(__name__)
@@ -258,8 +261,16 @@ class CspConfigureStep(base.ConfigureStep, LogEnabled):
     def set_wait_for_doing(
         self, sub_array_id: int, receptors: List[int]
     ) -> MessageBoardBuilder:
-        """Not implemented."""
-        raise NotImplementedError()
+        """Domain logic specifying what needs to be waited for a subarray to be in a state of configuring.
+
+        :param sub_array_id: The index id of the subarray to control
+        """
+        builder = get_message_board_builder()
+        subarray_name = self._tel.csp.subarray(sub_array_id)
+        builder.set_waiting_on(subarray_name).for_attribute(
+            "obsState"
+        ).to_become_equal_to("CONFIGURING")
+        return builder
 
     def set_wait_for_undo(
         self, sub_array_id: int, receptors: List[int]
@@ -306,7 +317,9 @@ class CspScanStep(base.ScanStep, LogEnabled):
         try:
             subarray.command_inout("Scan", scan_config_arg)
             sleep(scan_duration)
-            subarray.command_inout("EndScan")
+            current_state = subarray.read_attribute("obsState")
+            if current_state.value == ObsState.SCANNING:
+                subarray.command_inout("EndScan")
         except Exception as exception:
             logger.exception(exception)
             raise exception
@@ -419,6 +432,40 @@ class CSPSetOnlineStep(base.ObservationStep, LogEnabled):
         raise NotImplementedError()
 
 
+class CSPAbortStep(AbortStep, LogEnabled):
+    def do(self, sub_array_id: int):
+        subarray_name = self._tel.csp.subarray(sub_array_id)
+        subarray = con_config.get_device_proxy(subarray_name)
+        self._log(f"commanding {subarray_name} with Abort command")
+        subarray.command_inout("Abort")
+
+    def set_wait_for_do(self, sub_array_id: int) -> Union[MessageBoardBuilder, None]:
+        builder = get_message_board_builder()
+        subarray_name = self._tel.sdp.subarray(sub_array_id)
+        builder.set_waiting_on(subarray_name).for_attribute(
+            "obsState"
+        ).to_become_equal_to("ABORTED", ignore_first=True)
+        return builder
+
+
+class CSPObsResetStep(ObsResetStep, LogEnabled):
+    def set_wait_for_do(
+        self, sub_array_id: int, receptors: List[int]
+    ) -> Union[MessageBoardBuilder, None]:
+        builder = get_message_board_builder()
+        subarray_name = self._tel.csp.subarray(sub_array_id)
+        builder.set_waiting_on(subarray_name).for_attribute(
+            "obsState"
+        ).to_become_equal_to("IDLE", ignore_first=True)
+        return builder
+
+    def do(self, sub_array_id: int):
+        subarray_name = self._tel.csp.subarray(sub_array_id)
+        subarray = con_config.get_device_proxy(subarray_name)
+        self._log(f"commanding {subarray_name} with ObsReset command")
+        subarray.command_inout("Obsreset")
+
+
 class CSPEntryPoint(CompositeEntryPoint):
     """Derived Entrypoint scoped to SDP element."""
 
@@ -435,151 +482,5 @@ class CSPEntryPoint(CompositeEntryPoint):
         self.assign_resources_step = CspAsignResourcesStep(observation)
         self.configure_scan_step = CspConfigureStep(observation)
         self.scan_step = CspScanStep(observation)
-
-
-csp_mid_assign_resources_template = {
-    "interface": "https://schema.skao.int/ska-csp-configure/2.0",
-    "subarray_id": 1,
-    "dish": {"receptor_ids": ["001", "002"]},
-}
-
-csp_mid_configure_scan_template = {
-    "interface": "https://schema.skao.int/ska-csp-configure/2.0",
-    "subarray": {"subarray_name": "science period 23"},
-    "common": {
-        "config_id": "sbi-mvp01-20200325-00001-science_A",
-        "frequency_band": "1",
-        "subarray_id": "1",
-    },
-    "cbf": {
-        "delay_model_subscription_point": "sys/tg_test/1/string_scalar",
-        "fsp": [
-            {
-                "fsp_id": 1,
-                "function_mode": "CORR",
-                "frequency_slice_id": 1,
-                "integration_factor": 1,
-                "zoom_factor": 0,
-                "channel_averaging_map": [[0, 2], [744, 0]],
-                "channel_offset": 0,
-                "output_link_map": [[0, 0], [200, 1]],
-            },
-            {
-                "fsp_id": 2,
-                "function_mode": "CORR",
-                "frequency_slice_id": 2,
-                "integration_factor": 1,
-                "zoom_factor": 1,
-                "zoom_window_tuning": 650000,
-                "channel_averaging_map": [[0, 2], [744, 0]],
-                "channel_offset": 744,
-                "output_link_map": [[0, 4], [200, 5]],
-                "output_host": [[0, "192.168.1.1"]],
-                "output_port": [[0, 9744, 1]],
-            },
-        ],
-        "vlbi": {},
-    },
-    "pss": {},
-    "pst": {},
-    "pointing": {
-        "target": {
-            "system": "ICRS",
-            "target_name": "Polaris Australis",
-            "ra": "21:08:47.92",
-            "dec": "-88:57:22.9",
-        }
-    },
-}
-
-csp_low_assign_resources = {
-    "interface": "https://schema.skao.int/ska-low-csp-assignresources/2.0",
-    "common": {"subarray_id": 1},
-    "lowcbf": {
-        "stations": [
-            {"station_id": 1, "sub_station_id": 1},
-            {"station_id": 3, "sub_station_id": 1},
-            {"station_id": 3, "sub_station_id": 2},
-        ],
-        "station_beams": [
-            {
-                "station_beam_id": 1,
-                "channels": [1, 2, 3, 4, 5, 6, 7, 8],
-                "pst_beams": [{"pst_beam_id": 1}],
-            }
-        ],
-    },
-}
-
-csp_low_configure_scan = {
-    "interface": "https://schema.skao.int/ska-csp-configure/2.0",
-    "subarray": {"subarray_name": "science period 23"},
-    "common": {
-        "config_id": "sbi-mvp01-20200325-00001-science_A",
-        "subarray_id": 1,
-    },
-    "lowcbf": {
-        "jones_source": "tango://host:port/domain/family/member",
-        "station_beams": [
-            {
-                "station_beam_id": 1,
-                "station_delay_src": "tango://host:port/domain/family/member",
-                "visibility_dest": [
-                    {"dest_ip": "10.0.2.1", "dest_mac": "02:00:00:00:02:01"}
-                ],
-                "zooms": [
-                    {
-                        "zoom_id": 1,
-                        "zoom_centre_hz": 90000000,
-                        "zoom_resolution_hz": 14,
-                        "zoom_channels": 2000,
-                        "zoom_dest": {
-                            "dest_ip": "10.0.5.1",
-                            "dest_mac": "02:00:00:00:05:01",
-                        },
-                    },
-                    {
-                        "zoom_id": 2,
-                        "zoom_centre_hz": 120000000,
-                        "zoom_resolution_hz": 30,
-                        "zoom_channels": 2000,
-                        "zoom_dest": {
-                            "dest_ip": "10.0.5.2",
-                            "dest_mac": "02:00:00:00:05:02",
-                        },
-                    },
-                ],
-            },
-            {
-                "station_beam_id": 2,
-                "station_delay_src": "tango://host:port/domain/family/member",
-                "visibility_dest": [
-                    {"dest_ip": "10.0.2.2", "dest_mac": "02:00:00:00:02:02"}
-                ],
-                "zooms": [
-                    {
-                        "zoom_id": 3,
-                        "zoom_centre_hz": 190000000,
-                        "zoom_resolution_hz": 60,
-                        "zoom_channels": 2000,
-                        "zoom_dest": {
-                            "dest_ip": "10.0.5.3",
-                            "dest_mac": "02:00:00:00:05:03",
-                        },
-                    }
-                ],
-            },
-        ],
-    },
-}
-
-csp_low_scan = {
-    "common": {"subarray_id": 1},
-    "lowcbf": {
-        "scan_id": 987654321,
-        "unix_epoch_seconds": 1616971738,
-        "timestamp_ns": 987654321,
-        "packet_offset": 123456789,
-        "scan_seconds": 30,
-    },
-}
+        self.abort_step = CSPAbortStep()
+        self.obsreset_step = CSPObsResetStep()

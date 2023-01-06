35618b53705bb2bc80209312f95713b8a87a0cd5
shraddhabajare <shraddhab.ska@gmail.com>
2023-01-06 14:49:20 +0530

    SAH-1260 - Modify make script

--- resources/alarmhandler.mk
+++ resources/alarmhandler.mk
@@ -1,10 +1,6 @@
 .PHONY: configure-alarmhandler
 
-# ATTR_CONFIG_FILE ?= attribute_config.yaml
-
-
 # This script configures the alarmhandler with the rules defined in text file.
-# ATTR_CONFIG_FILE= attribute configuration yaml file
 configure-alarmhandler:
 	curl https://gitlab.com/ska-telescope/ska-tango-alarmhandler/-/raw/0.1.1/charts/configuration_job.sh?inline=false > charts/configuration_job.sh
 	curl https://gitlab.com/ska-telescope/ska-tango-alarmhandler/-/raw/0.1.1/charts/ska-tango-alarmhandler/data/alarm_configure.py?inline=false > resources/alarm_configure.py 
@@ -13,4 +9,5 @@ configure-alarmhandler:
 	kubectl  --kubeconfig=$(KUBECONFIG) create -f charts/configuration_job.yaml -n $(KUBE_NAMESPACE)
 	kubectl  --kubeconfig=$(KUBECONFIG) wait --for=condition=Complete job/alarm-configuration -n $(KUBE_NAMESPACE)
 	kubectl  --kubeconfig=$(KUBECONFIG) logs job.batch/alarm-configuration -n $(KUBE_NAMESPACE)
-	rm charts/configuration_job.yaml
+	rm charts/configuration_job.sh
+	rm resources/alarm_configure.py 
---
+++ tests/integration/alarmhandler/test_alarm_handler.py
@@ -0,0 +1,5 @@
+"""Test to verify alarm-handler configuration"""
+
+# WIP: Implementation is in progress
+def test_alarm_configure():
+    pass

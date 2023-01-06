f862744f28369e9cbc66c312cceb7e7c721d8162
Roshan Tayde <roshant.ska@gmail.com>
2023-01-06 09:41:54 +0530

    HM-131: Minor Changes.

--- tests/integration/tmc/leafnode/test_configure_sdp_with_ln.py
+++ tests/integration/tmc/leafnode/test_configure_sdp_with_ln.py
@@ -19,6 +19,15 @@ from resources.models.mvp_model.states import ObsState
 def test_configure_scan_on_sdp_subarray_in_mid():
     """Configure scan on sdp subarray in mid using the leaf node."""
 
+@pytest.mark.skalow
+@pytest.mark.configure
+@scenario(
+    "features/tmc_sdpln_configure.feature",
+    "Configure the SDP low using SDP leaf node",
+)
+def test_configure_scan_on_sdp_subarray_in_low():
+    """Configure scan on SDP subarray in low using the leaf node."""
+
 
 # @given("an SDP subarray in the IDLE state") from .conftest
 
@@ -43,34 +52,3 @@ def the_subarray_must_be_in_the_ready_state(
     assert_that(result).is_equal_to(ObsState.READY)
 
 
-
-
-@pytest.mark.skalow
-@pytest.mark.configure
-@scenario(
-    "features/tmc_sdpln_configure.feature",
-    "Configure the SDP low using SDP leaf node",
-)
-def test_configure_scan_on_sdp_subarray_in_low():
-    """Configure scan on sdp subarray in low using the leaf node."""
-
-# @given("an SDP subarray in the IDLE state") from .conftest
-
-@given("a TMC SDP subarray Leaf Node")
-def a_tmc_sdp_subarray_leaf_node(set_sdp_ln_entry_point):  # type: ignore
-    """a tmc SDP subarray leaf node."""
-
-
-# @when("I configure it for a scan") from ...conftest
-
-
-@then("the SDP subarray shall go from IDLE to READY state")
-def the_sdp_subarray_shall_go_from_idle_to_ready_state(
-    allocated_subarray: fxt_types.allocated_subarray,
-):
-    """the SDP subarray shall go from IDLE to READY state."""
-    sub_array_id = allocated_subarray.id
-    tel = names.TEL()
-    sdp_subarray = con_config.get_device_proxy(tel.sdp.subarray(sub_array_id))
-    result = sdp_subarray.read_attribute("obsState").value
-    assert_that(result).is_equal_to(ObsState.READY)

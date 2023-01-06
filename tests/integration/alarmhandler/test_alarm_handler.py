4b8880282f20f3a59c41c52b28c32051db61151f
shraddhabajare <shraddhab.ska@gmail.com>
2023-01-06 15:16:31 +0530

    SAH-1260 - Add test case for alarm-handler

--- tests/integration/alarmhandler/test_alarm_handler.py
+++ tests/integration/alarmhandler/test_alarm_handler.py
@@ -1,5 +1,19 @@
 """Test to verify alarm-handler configuration"""
 
-# WIP: Implementation is in progress
-def test_alarm_configure():
-    pass
+"""
+Test Elettra Alarm Handler
+"""
+import pytest
+import tango
+
+@pytest.mark.k8s
+@pytest.mark.k8sonly
+@pytest.mark.skamid
+def test_load_alarm():
+  """ Test to load and verify the configured alarm. """
+  # test_device = tango.DeviceProxy("sys/tg_test/1")
+  alarm_device = tango.DeviceProxy("alarm/handler/01")
+  alarm_device.command_inout("Load","tag=test;formula=(sys/tg_test/1/attribute == 0);priority=log;group=gr_none;message=(\"alarm for current attribute\")")
+  tag="test"
+  searched_alarm = alarm_device.command_inout("SearchAlarm",tag)
+  assert "test" in searched_alarm[0]

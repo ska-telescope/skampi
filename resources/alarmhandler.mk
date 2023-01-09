f8025cb8fae0b060cd50319581dd5c3e4cde360d
shraddhabajare <shraddhab.ska@gmail.com>
2023-01-09 12:29:35 +0530

    SAH-1260 - Modify alarmhandler tag for curl

--- resources/alarmhandler.mk
+++ resources/alarmhandler.mk
@@ -1,11 +1,11 @@
-.PHONY: configure-alarmhandler
+.PHONY: configure-alarm
 
 # This script configures the alarmhandler with the rules defined in text file.
 # Requries paramater KUBECONFIG, FILE_NAME, ALARM_HANDLER_FQDN, KUBE_NAMESPACE, TANGO_HOST
 # FILE_NAME is a text file containing alarm rules to configure the Elettra Alarm Handler
-configure-alarmhandler:
-	curl https://gitlab.com/ska-telescope/ska-tango-alarmhandler/-/raw/0.1.1/charts/configuration_job.sh?inline=false > charts/configuration_job.sh
-	curl https://gitlab.com/ska-telescope/ska-tango-alarmhandler/-/raw/0.1.1/charts/ska-tango-alarmhandler/data/alarm_configure.py?inline=false > resources/alarm_configure.py 
+configure-alarm:
+	curl https://gitlab.com/ska-telescope/ska-tango-alarmhandler/-/raw/0.1.2/charts/configuration_job.sh?inline=false > charts/configuration_job.sh
+	curl https://gitlab.com/ska-telescope/ska-tango-alarmhandler/-/raw/0.1.2/charts/ska-tango-alarmhandler/data/alarm_configure.py?inline=false > resources/alarm_configure.py 
 	bash ./charts/configuration_job.sh $(FILE_NAME) $(ALARM_HANDLER_FQDN)
 	kubectl  --kubeconfig=$(KUBECONFIG) create configmap alarm-configure  --from-file $(FILE_NAME) --from-file resources/alarm_configure.py -o yaml -n $(KUBE_NAMESPACE) --dry-run=client | kubectl apply -f -
 	kubectl  --kubeconfig=$(KUBECONFIG) create -f charts/configuration_job.yaml -n $(KUBE_NAMESPACE)

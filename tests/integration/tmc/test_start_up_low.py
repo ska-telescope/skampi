255e7d1c3b4156acd3eb1f38c73187d8d839a9bd
akshaysonawane <akshayss.ska@gmail.com>
2023-01-16 11:15:32 +0530

    HM-136: Add logs for debugging

--- tests/integration/tmc/test_start_up_low.py
+++ tests/integration/tmc/test_start_up_low.py
@@ -115,26 +115,35 @@ def a_telescope_with_sdp_and_csp_on():
     sut_settings = conftest.SutTestSettings()
 
     csp_master_leaf_node = con_config.get_device_proxy(tel.tm.csp_leaf_node)
+    logger.info("I am in get cspmln")
     result = csp_master_leaf_node.read_attribute("state").value
+    logger.info("I am in read attr cspmln")
     assert_that(str(result)).is_equal_to("ON")
 
     sdp_master_leaf_node = con_config.get_device_proxy(tel.tm.sdp_leaf_node)
+    logger.info("I am in get sdpmln")
     result = sdp_master_leaf_node.read_attribute("state").value
+    logger.info("I am in read attr sdpmln")
     assert_that(str(result)).is_equal_to("ON")
+    logger.info("I am in assert sdpmln")
 
     for index in range(1, sut_settings.nr_of_subarrays + 1):
         csp_subarray_leaf_node = con_config.get_device_proxy(
             tel.tm.subarray(index).csp_leaf_node
         )
+        logger.info("I am in get dv proxy cspsln")
         result = csp_subarray_leaf_node.read_attribute("state").value
+        logger.info("I am in assert dv proxy cspsln")
         assert_that(str(result)).is_equal_to("ON")
 
     for index in range(1, sut_settings.nr_of_subarrays + 1):
         sdp_subarray_leaf_node = con_config.get_device_proxy(
             tel.tm.subarray(index).sdp_leaf_node
         )
+        logger.info("I am in get dv proxy sdpsln")
         result = sdp_subarray_leaf_node.read_attribute("state").value
         assert_that(str(result)).is_equal_to("ON")
+        logger.info("I am in assert dv proxy sdpsln")
 
 # use @when("I start up the telescope") from ..conftest
 
@@ -143,23 +152,37 @@ def a_telescope_with_sdp_and_csp_on():
 def the_sdp_and_csp_must_be_on(sut_settings: conftest.SutTestSettings):
     """the sdp and csp must be on"""
     tel = names.TEL()
+    logger.info("I am in 4th TEL")
     # Check state attribute of SDP Master
     sdp_master = con_config.get_device_proxy(tel.sdp.master)
+    logger.info("I am in get2 dv proxy sdpm")
     result = sdp_master.read_attribute("state").value
+    logger.info("I am in assert dv proxy sdpm")
     assert_that(str(result)).is_equal_to("ON")
+    logger.info("I am in get3 assert dv proxy sdpm")
     for index in range(1, sut_settings.nr_of_subarrays + 1):
         subarray = con_config.get_device_proxy(tel.sdp.subarray(index))
+        logger.info("I am in get3  dv proxy subarray")
         result = subarray.read_attribute("state").value
+        logger.info("I am in assert  dv proxy subarray")
         assert_that(str(result)).is_equal_to("ON")
     # Check state attribute of CSP Master
     csp_master = con_config.get_device_proxy(tel.csp.controller)
+    logger.info("I am in get3 dv proxy cspmaster")
     result = csp_master.read_attribute("state").value
+    logger.info("I am in result dv proxy cspmaster")
     assert_that(str(result)).is_equal_to("ON")
+    logger.info("I am in assert dv proxy cspmaster")
     for index in range(1, sut_settings.nr_of_subarrays + 1):
         subarray = con_config.get_device_proxy(tel.csp.subarray(index))
+        logger.info("I am in assert dv proxy cspsubarray")
         result = subarray.read_attribute("state").value
+        logger.info("I am in result4 dv proxy cspsubarray")
         assert_that(str(result)).is_equal_to("ON")
+        logger.info("I am in assert5 dv proxy cspsubarray")
     # Check telescopeState attribute of Central Node
     central_node = con_config.get_device_proxy(tel.tm.central_node)
+    logger.info("I am in assert5 dv proxy cn")
     result = central_node.read_attribute("telescopeState").value
+    logger.info("I am in assert6 dv proxy cspsubarray")
     assert_that(str(result)).is_equal_to("ON")

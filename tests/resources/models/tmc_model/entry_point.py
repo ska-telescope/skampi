4daddf3c096c77b8b3fd98197cc3c209ec4260ac
user <jayant.ska@gmail.com>
2023-02-09 21:22:13 +0530

    SAH-1275: Update ska-tmc-low chart version

--- tests/resources/models/tmc_model/entry_point.py
+++ tests/resources/models/tmc_model/entry_point.py
@@ -828,6 +828,7 @@ CONFIGURE_JSON_LOW = {
     },
     "common": {
       "config_id": "sbi-mvp01-20200325-00001-science_A",
+      
     },
     "lowcbf": {
       "stations": {
@@ -868,6 +869,41 @@ CONFIGURE_JSON_LOW = {
       },
       "timing_beams": {
         "beams": [
+          {
+            "pst_beam_id": 13,
+            "stn_beam_id": 1,
+            "offset_dly_poly": "url",
+            "stn_weights": [
+              0.9,
+              1.0,
+              1.0,
+              0.9
+            ],
+            "jones": "url",
+            "dest_ip": [
+              "10.22.0.1:2345",
+              "10.22.0.3:3456"
+            ],
+            "dest_chans": [
+              128,
+              256
+            ],
+            "rfi_enable": [
+              true,
+              true,
+              true
+            ],
+            "rfi_static_chans": [
+              1,
+              206,
+              997
+            ],
+            "rfi_dynamic_chans": [
+              242,
+              1342
+            ],
+            "rfi_weighted": 0.87
+          }
         ]
       }
     }

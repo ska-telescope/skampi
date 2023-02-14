b030e07ca5022323c61cba366f30c8c83a9d0ff8
truptiska <truptin.ska@gmail.com>
2023-02-14 20:00:14 +0530

    SAH-1292 : Test case updated

--- tests/integration/tmc/test_start_up.py
+++ tests/integration/tmc/test_start_up.py
@@ -250,7 +250,7 @@ def the_sdp_csp_and_dish_must_be_off(
         assert_that(str(result)).is_equal_to("OFF")
 
 
-@then("And TMC devices are healthy")
+@then("TMC devices are healthy")
 def the_tmc_devices_must_be_healthy(sut_settings: conftest.SutTestSettings):
     """the sdp, csp and dish must be on."""
     tel = names.TEL()

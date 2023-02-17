64f7851415a72eb781b0e487edb8ca9267506f22
Tejas Phadtare <tejasphadtare.ska@gmail.com>
2023-02-17 04:35:04 +0000

    NAK-353 Update test_scan to check for scanning

--- tests/integration/oet/test_scan.py
+++ tests/integration/oet/test_scan.py
@@ -68,6 +68,6 @@ def the_subarray_must_be_in_the_scanning_state(
     tmc_subarray_name = str(tel.tm.subarray(configured_subarray.id))
     tmc_subarray = con_config.get_device_proxy(tmc_subarray_name)
     tmc_state_changes = recorder.get_transitions_for(tmc_subarray_name, "obsstate")
-    assert_that(tmc_state_changes).is_equal_to("SCANNING")
+    assert_that(tmc_state_changes).is_equal_to(['SCANNING'])
     result = tmc_subarray.read_attribute("obsstate").value
     assert_that(result).is_equal_to(ObsState.READY)

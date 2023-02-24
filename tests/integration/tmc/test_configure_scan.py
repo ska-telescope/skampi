0994894670bccabb78cd9348a8b9f255de94d235
Gerhard le Roux <gerhardlr@gmail.com>
2023-02-24 10:09:31 +0000

    SAH-12931: mark tests as xfail

--- tests/integration/tmc/test_configure_scan.py
+++ tests/integration/tmc/test_configure_scan.py
@@ -15,7 +15,10 @@ from ..conftest import SutTestSettings
 @pytest.mark.k8sonly
 @pytest.mark.skamid_skip
 @pytest.mark.configure
-@scenario("features/tmc_configure_scan.feature", "Configure the mid telescope subarray using TMC")
+@scenario(
+    "features/tmc_configure_scan.feature",
+    "Configure the mid telescope subarray using TMC",
+)
 def test_tmc_configure_scan_on_mid_subarray():
     """Configure scan on TMC mid telescope subarray."""
 
@@ -24,18 +27,22 @@ def test_tmc_configure_scan_on_mid_subarray():
 @pytest.mark.k8sonly
 @pytest.mark.skalow_skip
 @pytest.mark.configure
-@scenario("features/tmc_configure_scan.feature", "Configure the low telescope subarray using TMC")
+@scenario(
+    "features/tmc_configure_scan.feature",
+    "Configure the low telescope subarray using TMC",
+)
 def test_tmc_configure_scan_on_low_subarray():
     """Configure scan on TMC low telescope subarray."""
 
 
-
+@pytest.mark.xfail
 @pytest.mark.skamid_skip
 @pytest.mark.configure
 @pytest.mark.tmc
 @scenario("features/tmc_configure_scan.feature", "Abort configuring")
 def test_abort_configuring_on_mid_tmc_subarray():
     """Abort configuring."""
+
 
 @given("an TMC")
 def a_tmc():
@@ -44,7 +51,9 @@ def a_tmc():
 
 @given("an telescope subarray", target_fixture="configuration")
 def an_telescope_subarray(
-    set_up_subarray_log_checking_for_tmc, base_configuration: conf_types.ScanConfiguration,  subarray_allocation_spec: fxt_types.subarray_allocation_spec,
+    set_up_subarray_log_checking_for_tmc,
+    base_configuration: conf_types.ScanConfiguration,
+    subarray_allocation_spec: fxt_types.subarray_allocation_spec,
     sut_settings: SutTestSettings,
 ) -> conf_types.ScanConfiguration:
     """an telescope subarray."""
@@ -61,12 +70,16 @@ def a_subarray_in_the_idle_state():
 
 @then("the subarray must be in the READY state")
 def the_subarray_must_be_in_the_ready_state(
-    sut_settings: SutTestSettings, integration_test_exec_settings: fxt_types.exec_settings
+    sut_settings: SutTestSettings,
+    integration_test_exec_settings: fxt_types.exec_settings,
 ):
     """the subarray must be in the READY state."""
     tel = names.TEL()
     integration_test_exec_settings.recorder.assert_no_devices_transitioned_after(
-        str(tel.tm.subarray(sut_settings.subarray_id)))
-    tmc_subarray = con_config.get_device_proxy(tel.tm.subarray(sut_settings.subarray_id))
+        str(tel.tm.subarray(sut_settings.subarray_id))
+    )
+    tmc_subarray = con_config.get_device_proxy(
+        tel.tm.subarray(sut_settings.subarray_id)
+    )
     result = tmc_subarray.read_attribute("obsState").value
     assert_that(result).is_equal_to(ObsState.READY)

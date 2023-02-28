8d3ce58b90150404e238b5382b1d0a0da433e4bd
Gerhard le Roux <gerhardlr@gmail.com>
2023-02-28 13:47:30 +0000

    SAH-12931: add logs to subarray abort

--- tests/integration/tmc/test_configure_scan.py
+++ tests/integration/tmc/test_configure_scan.py
@@ -44,7 +44,9 @@ def fxt_disable_abort(allocated_subarray: fxt_types.allocated_subarray):
 @pytest.mark.configure
 @pytest.mark.tmc
 @scenario("features/tmc_configure_scan.feature", "Abort configuring")
-def test_abort_configuring_on_mid_tmc_subarray(disable_clear_and_tear_down: None):
+def test_abort_configuring_on_mid_tmc_subarray(
+    disable_clear_and_tear_down: None, set_up_subarray_log_checking_for_tmc: None
+):
     """Abort configuring."""
 
 

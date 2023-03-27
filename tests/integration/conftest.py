94a15d3eb0bb7d6c969441c5f5d0dbbe2f9b2017
Omprakash Verma <omprakash_verma@persistent.com>
2023-03-27 08:46:30 +0000

    NAK-292 Update conftest.py

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,7 +3,6 @@ integration tests."""
 import logging
 from types import SimpleNamespace
 import os
-from ska_tmc_cdm.schemas import CODEC
 from typing import Any, Callable, Concatenate, ParamSpec, TypeVar
 from mock import patch, Mock
 from assertpy import assert_that
@@ -308,8 +307,11 @@ def a_subarray_configured_for_scan_type(
     configuration = SKAScanConfiguration(observation_config)
     configuration.set_next_target_to_be_configured(scan_targets[scan_type])
     configuration_specs = SubarrayConfigurationSpec(scan_duration, configuration)
-    logger.info(f"$$$$$$$ configuration dumps: {CODEC.dumps(configuration)}")
-    logger.info(f"$$$$$$$ configuration_specs dumps: {CODEC.dumps(configuration_specs)}")
+    logger.info(f"$$$$$$$ configuration_specs: {dir(configuration_specs)}")
+    logger.info(f"$$$$$$$ configuration_specs: {configuration_specs.__dict__}")
+    logger.info(f"$$$$$$$ configuration dumps: {configuration.as_json}")
+    logger.info(f"$$$$$$$ configuration_specs dumps: {configuration_specs.as_json}")
+    
     
     return factory_configured_subarray(
         injected_subarray_configuration_spec=configuration_specs

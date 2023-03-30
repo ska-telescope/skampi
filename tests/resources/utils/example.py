5dc1357569241f917025c4307446946ea630e73c
Gerhard le Roux <gerhardlr@gmail.com>
2023-03-30 14:40:47 +0000

    AT-431: implement first try example

--- tests/resources/utils/example.py
+++ tests/resources/utils/example.py
@@ -1,6 +1,26 @@
+import logging
 from diagrams import Diagram
+from diagrams.k8s.ecosystem import Helm
+from .input import DiagramItems
+
+
+def test_it():
+    for item in DiagramItems().get_next_item_depth_first():
+        logging.info(item.name)
+
+    for item in DiagramItems().get_next_item_breath_first():
+        logging.info(item.name)
+
+    for edge in DiagramItems().get_next_edge_depth_first():
+        logging.info(f"{edge.start.name}-------->{edge.end.name}")
+
+    for edge in DiagramItems().get_next_edge_breath_first():
+        logging.info(f"{edge.start.name}-------->{edge.end.name}")
 
 
 def main():
-    with Diagram("Hallo world Diagram", show=False):
-        pass
+    diagram = DiagramItems()
+    test_it()
+    with Diagram("First try", show=False):
+        for edge in diagram.get_next_edge_depth_first():
+            Helm(edge.start.name) >> Helm(edge.end.name)

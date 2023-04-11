307f64189164e225264f96bdefe6e30e3e872cf2
mansi.shirore <mansis.ska@gmail.com>
2023-04-11 17:23:06 +0530

    SAH-1309: Update abort scanning test case.

--- tests/integration/csp/test_csp_abort_scanning.py
+++ tests/integration/csp/test_csp_abort_scanning.py
@@ -1,10 +1,10 @@
-import pytest
-from pytest_bdd import scenario
+# import pytest
+# from pytest_bdd import scenario
 
 
-@pytest.mark.skalow
-@pytest.mark.scan
-@pytest.mark.csp
-@scenario("features/csp_abort_scanning.feature", "Abort scanning on CSP Low")
-def test_abort_scanning_on_low_csp(disable_clear):
-    """Abort in scanning obstate."""
+# @pytest.mark.skalow
+# @pytest.mark.scan
+# @pytest.mark.csp
+# @scenario("features/csp_abort_scanning.feature", "Abort scanning on CSP Low")
+# def test_abort_scanning_on_low_csp(disable_clear):
+#     """Abort in scanning obstate."""
--- tests/integration/csp/test_scan.py
+++ tests/integration/csp/test_scan.py
@@ -36,6 +36,14 @@ def test_abort_scanning(disable_clear):
     """Abort scanning."""
 
 
+@pytest.mark.skalow
+@pytest.mark.scan
+@pytest.mark.csp
+@scenario("features/csp_abort_scanning.feature", "Abort scanning on CSP Low")
+def test_abort_scanning_on_low_csp(disable_clear):
+    """Abort in scanning obstate."""
+
+
 @given("an CSP subarray in READY state")
 def an_csp_subarray_in_ready_state(
     set_up_subarray_log_checking_for_csp,

dca48ff2bce4005d327a6d4480db1a0aa1248cd5
mansi.shirore <mansis.ska@gmail.com>
2023-04-11 16:50:23 +0530

    SAH-1309: Updates csp abort scanning test case.

--- tests/integration/csp/test_csp_abort_scanning.py
+++ tests/integration/csp/test_csp_abort_scanning.py
@@ -6,5 +6,5 @@ from pytest_bdd import scenario
 @pytest.mark.scan
 @pytest.mark.csp
 @scenario("features/csp_abort_scanning.feature", "Abort scanning on CSP Low")
-def test_abort_scanning_on_low_csp():
+def test_abort_scanning_on_low_csp(disable_clear):
     """Abort in scanning obstate."""

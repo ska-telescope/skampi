852dd7d21deaf51c7d3d4b6b16c4afa30c3b8d3a
Pieter Jordaan <pieter.jordaan@tsolo.io>
2023-05-12 11:00:06 +0200

    AT-489: disable test_abort_scanning.

--- tests/integration/csp/test_scan.py
+++ tests/integration/csp/test_scan.py
@@ -27,6 +27,7 @@ def test_run_a_scan_on_csp_subarray_in_mid():
     """Run a scan on sdp subarray in mid."""
 
 
+@pytest.mark.skip(reason="temp skip for at-489")
 @pytest.mark.skamid
 @pytest.mark.scan
 @pytest.mark.csp

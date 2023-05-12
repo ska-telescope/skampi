5a71332136e7c64d40f27e64c0e24b94f231c653
Gerhard le Roux <gerhardlr@gmail.com>
2023-05-12 11:06:57 +0000

    AT-489: revert to use local time only

--- tests/integration/tmc/test_scan.py
+++ tests/integration/tmc/test_scan.py
@@ -82,7 +82,8 @@ def the_sdp_subarray_must_be_in_the_scanning_state(
         "READY", ignore_first=False, settings=integration_test_exec_settings
     )
     integration_test_exec_settings.recorder.assert_no_devices_transitioned_after(  # noqa: E501
-        tmc_subarray_name
+        str(tmc_subarray_name),
+        time_source='local'
     )
     result = tmc_subarray.read_attribute("obsstate").value
     assert_that(result).is_equal_to(ObsState.READY)

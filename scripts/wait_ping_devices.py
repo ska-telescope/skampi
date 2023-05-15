3fc06aea065e8791e678f80dd33b2be2eef54b7b
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2023-05-15 13:10:50 +0200

    ST-1535 >0
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- scripts/wait_ping_devices.py
+++ scripts/wait_ping_devices.py
@@ -1,18 +1,18 @@
 import tango
 import time
 
-TIMEOUT=10
+TIMEOUT=60
 database = tango.Database()
-instance_list = database.get_device_exported("*")
 failed = 1000
 start_time = time.time()
 print("TANGO waiting start")
-while failed == 0:
+while failed > 0:
     elapsed_time = time.time() - start_time
     if elapsed_time > TIMEOUT:
         print("Devices not working: exiting tango wait with failure")
         exit(1)
     failed = 0
+    instance_list = database.get_device_exported("*")
     for instance in instance_list.value_string:
         try:
             dev = tango.DeviceProxy(instance)

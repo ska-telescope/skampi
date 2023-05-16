a84ce48f8c0993f9dc4c768899299c53507ac632
Gerhard le Roux <gerhardlr@gmail.com>
2023-05-16 09:07:29 +0000

    AT-489: push utils model

---
+++ tests/resources/models/tmc_model/leafnodes/utils.py
@@ -0,0 +1,19 @@
+def retry(nr_of_reties: int = 3, wait_time: int = 1):
+    @functools.wraps
+    def wrapper(command: Callable[P, T], *args: P.args, **kwargs: P.kwargs) -> T:
+        try:
+            return command(*args, **kwargs)
+        except Exception:
+            nr_of_retries = 0
+            exception_to_raise = None
+            while nr_of_retries < nr_of_reties:
+                time.sleep(wait_time)
+                try:
+                    return command(*args, **kwargs)
+                except Exception as exception:
+                    nr_of_retries += 1
+                    exception_to_raise = exception
+            assert exception_to_raise
+            raise exception_to_raise
+
+    return wrapper

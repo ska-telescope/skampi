9b7a36262edef60b34bcabb23f759839dc51fbd7
devesh pandey <devesh.ska@gmail.com>
2023-05-17 20:11:00 +0530

    AT-489-tmc-leafnode: Update csp entry point to check csp event

--- tests/resources/models/csp_model/entry_point.py
+++ tests/resources/models/csp_model/entry_point.py
@@ -311,7 +311,7 @@ class CspConfigureStep(base.ConfigureStep, LogEnabled):
         :return: builder
         """
         builder = get_message_board_builder()
-        subarray_name = self._tel.csp.cbf.subarray(sub_array_id)
+        subarray_name = self._tel.csp.subarray(sub_array_id)
         builder.set_waiting_on(subarray_name).for_attribute("obsState").to_become_equal_to("IDLE")
         return builder
 

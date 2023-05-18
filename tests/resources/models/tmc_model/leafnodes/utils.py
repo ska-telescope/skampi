d3b8a8a73a20e5f67d3413743d1ec36955b06958
Gerhard le Roux <gerhardlr@gmail.com>
2023-05-18 07:27:40 +0000

    AT-489: add listening

--- tests/resources/models/tmc_model/leafnodes/utils.py
+++ tests/resources/models/tmc_model/leafnodes/utils.py
@@ -1,12 +1,28 @@
-import functools
 import time
 from typing import Callable, ParamSpec, TypeVar
 import logging
+from .
 
 T = TypeVar("T")
 P = ParamSpec("P")
 
 logger = logging.getLogger(__name__)
+
+_listener: None | 'Listener' = None
+
+def get_listener():
+    global _listener
+    _listener = Listener() if _listener is None else _listener
+    return _listener
+
+class Listener():
+
+    def __init__(self) -> None:
+        self.messages: list[str] = []
+
+    def record(self, message: str):
+        self.messages.append(message)
+    
 
 def retry(nr_of_reties: int = 3, wait_time: float = 1):
     def wrapper(command: Callable[P, T], ) -> Callable[P, T]:
@@ -15,6 +31,8 @@ def retry(nr_of_reties: int = 3, wait_time: float = 1):
                 return command(*args, **kwargs)
             except Exception as exception:
                 logger.exception(exception.args)
+                listener = get_listener()
+                listener.record(exception.args[0])
                 nr_of_retries = 0
                 exception_to_raise = None
                 while nr_of_retries < nr_of_reties:

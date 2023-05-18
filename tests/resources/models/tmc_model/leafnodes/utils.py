12b43b024ef664f858c747e8577dd2c08655f743
devesh pandey <devesh.ska@gmail.com>
2023-05-18 12:42:05 +0530

    AT-489-tmc-leafnode: Update retry util

--- tests/resources/models/tmc_model/leafnodes/utils.py
+++ tests/resources/models/tmc_model/leafnodes/utils.py
@@ -8,24 +8,29 @@ P = ParamSpec("P")
 
 logger = logging.getLogger(__name__)
 
-def retry(nr_of_reties: int = 3, wait_time: int = 1):
-    @functools.wraps
-    def wrapper(command: Callable[P, T], *args: P.args, **kwargs: P.kwargs) -> T:
-        try:
-            logger.info(f"Invoking {command}")
-            return command(*args, **kwargs)
-        except Exception:
-            nr_of_retries = 0
-            exception_to_raise = None
-            while nr_of_retries < nr_of_reties:
-                time.sleep(wait_time)
-                try:
-                    logger.info(f"Retrying {command} for {nr_of_retries}th time")
-                    return command(*args, **kwargs)
-                except Exception as exception:
-                    nr_of_retries += 1
-                    exception_to_raise = exception
-            assert exception_to_raise
-            raise exception_to_raise
-
+def retry(nr_of_reties: int = 3, wait_time: float = 1):
+    def wrapper(command: Callable[P, T], ) -> Callable[P, T]:
+        def command_with_retry(*args: P.args, **kwargs: P.kwargs):
+            try:
+                return command(*args, **kwargs)
+            except Exception as exception:
+                logger.exception(exception.args)
+                nr_of_retries = 0
+                exception_to_raise = None
+                while nr_of_retries < nr_of_reties:
+                    time.sleep(wait_time)
+                    try: 
+                        logger.warning(f"Retrying {command.__repr__()} for {nr_of_retries}th time")
+                        return command(*args, **kwargs)
+                    except Exception as exception:
+                        nr_of_retries += 1
+                        exception_to_raise = exception
+                assert exception_to_raise
+                raise exception_to_raise
+        return command_with_retry
     return wrapper
+
+
+
+
+

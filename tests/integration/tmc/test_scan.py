69d17d5a1431f0170dd250a712e98da3ddecd180
Ruaan@06 <tejasphadtare.ska@gmail.com>
2023-05-18 10:24:23 +0530

    AT-489: fixing linting error

--- tests/integration/tmc/test_scan.py
+++ tests/integration/tmc/test_scan.py
@@ -82,8 +82,7 @@ def the_sdp_subarray_must_be_in_the_scanning_state(
         "READY", ignore_first=False, settings=integration_test_exec_settings
     )
     integration_test_exec_settings.recorder.assert_no_devices_transitioned_after(  # noqa: E501
-        str(tmc_subarray_name),
-        time_source='local'
+        str(tmc_subarray_name), time_source="local"
     )
     result = tmc_subarray.read_attribute("obsstate").value
     assert_that(result).is_equal_to(ObsState.READY)

f56ef95c72431816cfd1a4c6559bfb4c3d797d0a
roshant.ska <roshant.ska@gmail.com>
2023-05-18 19:13:56 +0530

    HM-221: Check with gitlab ci variables.

--- tests/resources/utils/chart_imager/confluence_chart_uploader.py
+++ tests/resources/utils/chart_imager/confluence_chart_uploader.py
@@ -24,22 +24,22 @@ def update_skampi_charts():
     PAGE_TITLE = f"SKAMPI Chart Dependencies"
     TYPE = "page"
     REPRESENTATION = "storage"
-    BODY = f"""
+    BODY = """
    <h2>Overview</h2>
     <p>
-        <br />The below graphs shows a view of helm charts decomposition in SKAMPI mid and low.
+        <br />The below graphs shows a view of helm charts decomposition in SKAMPI mid and low. 
         <br />The graphs created to make it easier to understand the helm charts used in SKAMPI mid and low are dependent on, their current versions, the flow/control.
     </p>
     <h2>SKAMPI Mid Chart</h2>
-    <h2>
-        <ac:image ac:style="max-height: 250.0px;" ac:height="400">
-            <ri:attachment ri:filename="images/ska_mid_charts.png" />
-        </ac:image>
-     <br />SKAMPI Low Chart</h2>
-    <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<ac:image ac:style="max-height: 250.0px;" ac:height="150">
-        <ri:attachment ri:filename="images/ska_low_charts.png" />
-    </ac:image>&nbsp; &nbsp; &nbsp;</p>
+    <h2><ac:image ac:style="max-height: 250.0px;" ac:height="400">
+        <ri:attachment ri:filename="ska_mid_charts.png" />
+    </ac:image>
+    <br />SKAMPI Low Chart</h2>
+    <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<ac:image ac:style="max-height: 250.0px;" ac:height="400">
+        <ri:attachment ri:filename="ska_low_charts.jpg" />
+    </ac:image></p>
     """
+
     try:
         response = confluence.update_page(
             page_id=PAGE_ID, title=PAGE_TITLE, body=BODY, type=TYPE, representation=REPRESENTATION, 

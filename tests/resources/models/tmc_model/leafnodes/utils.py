0decaf2dac04261adf754a49e7488b49a28dee15
devesh pandey <devesh.ska@gmail.com>
2023-05-18 11:05:22 +0530

    AT-489-tmc-leafnode: Add logging in retry method

--- tests/resources/models/tmc_model/leafnodes/utils.py
+++ tests/resources/models/tmc_model/leafnodes/utils.py
@@ -1,14 +1,18 @@
 import functools
 import time
 from typing import Callable, ParamSpec, TypeVar
+import logging
 
 T = TypeVar("T")
 P = ParamSpec("P")
+
+logger = logging.getLogger(__name__)
 
 def retry(nr_of_reties: int = 3, wait_time: int = 1):
     @functools.wraps
     def wrapper(command: Callable[P, T], *args: P.args, **kwargs: P.kwargs) -> T:
         try:
+            logger.info(f"Invoking {command}")
             return command(*args, **kwargs)
         except Exception:
             nr_of_retries = 0
@@ -16,6 +20,7 @@ def retry(nr_of_reties: int = 3, wait_time: int = 1):
             while nr_of_retries < nr_of_reties:
                 time.sleep(wait_time)
                 try:
+                    logger.info(f"Retrying {command} for {nr_of_retries}th time")
                     return command(*args, **kwargs)
                 except Exception as exception:
                     nr_of_retries += 1

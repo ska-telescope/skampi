52c0b513cd177a65dbe407e915cc466a88290771
roshant.ska <roshant.ska@gmail.com>
2023-05-19 18:39:07 +0530

    HM-221: Resolved the bug.

--- tests/resources/utils/chart_imager/confluence_chart_uploader.py
+++ tests/resources/utils/chart_imager/confluence_chart_uploader.py
@@ -3,10 +3,11 @@ The python script will upload the images to the provided confluence page id and 
 """
 import logging
 import sys,os
+import magic
 
 from atlassian import Confluence
 
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 API_TOKEN = os.environ['RT_API_TOKEN']
 DIR_NAME = os.path.dirname(sys.argv[0])
@@ -18,29 +19,47 @@ confluence = Confluence(
 )
 
 
-
 def update_skampi_charts():
+    BODY = """
+    <h2>Overview</h2>
+    <p>
+        <br />The below graphs shows a view of helm charts decomposition in SKAMPI mid and low. 
+        <br />The graphs created to make it easier to understand the helm charts used in SKAMPI mid and low are dependent on, their current versions, the flow/control.
+    </p>
+    <h2>SKAMPI Mid Chart</h2>
+    <h2><ac:image ac:height="400">
+        <ri:attachment ri:filename="ska_mid_charts.png" />
+        </ac:image>
+    </h2>
+    <h2><br />SKAMPI Low Chart</h2>
+    <p><br /></p>
+    <p><ac:image ac:height="250">
+        <ri:attachment ri:filename="ska_low_charts.png" />
+        </ac:image>
+    </p>
+    <p><br /></p>
+    """
+
     PAGE_ID = "221065346"
     PAGE_TITLE = "SKAMPI Chart Dependencies"
     TYPE = "page"
     REPRESENTATION = "storage"
-    BODY = f"""
-   <h2>Overview</h2>
-    <p>
-        <br />The below graphs shows a view of helm charts decomposition in SKAMPI mid and low.
-        <br />The graphs created to make it easier to understand the helm charts used in SKAMPI mid and low are dependent on, their current versions, the flow/control.
-    </p>
-    <h2>SKAMPI Mid Chart</h2>
-    <h2><ac:image ac:style="max-height: 250.0px;" ac:height="400">
-        <ri:attachment ri:filename="{DIR_PATH}/images/ska_mid_charts.png" />
-    </ac:image>
-    <br />SKAMPI Low Chart</h2>
-    <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<ac:image ac:style="max-height: 250.0px;" ac:height="250">
-        <ri:attachment ri:filename="{DIR_PATH}/images/ska_low_charts.png" />
-    </ac:image>
-    </p>
-    """
+    SPACE = "TS"
+    image_names = ["ska_low_charts.png","ska_mid_charts.png"]
 
+    for image_name in image_names:
+        file_absolute_path = f"{DIR_PATH}/images/{image_name}"
+        mime_type = magic.from_file(file_absolute_path, mime=True)
+        #upload the images to confluence page first.
+        confluence.attach_file(
+            filename=file_absolute_path,
+            name=image_name,
+            content_type=mime_type,
+            page_id=PAGE_ID,
+            space=SPACE
+        )
+    
+    #Update the confluence page
     response = confluence.update_page(
         page_id=PAGE_ID, title=PAGE_TITLE, body=BODY, type=TYPE, representation=REPRESENTATION, 
     )

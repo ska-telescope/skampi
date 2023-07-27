c499f09c035d120a48cf9573bbc41b002a49bd62
akshaysonawane <akshayss.ska@gmail.com>
2023-07-27 17:21:36 +0530

    HM-277: add sdpsln test case

---
+++ tests/integration/tmc/leafnode/test_assign_error_propagation_sdp.py
@@ -0,0 +1,55 @@
+"""Assign Resource on a SDP Subarray"""
+import pytest
+from assertpy import assert_that
+from pytest_bdd import given, scenario, then
+from resources.models.mvp_model.states import ObsState
+from ska_ser_skallop.connectors import configuration as con_config
+from ska_ser_skallop.mvp_control.describing import mvp_names as names
+from ska_ser_skallop.mvp_control.entry_points import types as conf_types
+
+from ...conftest import SutTestSettings
+
+@pytest.mark.sdpln
+@pytest.mark.skamid
+@pytest.mark.assign
+@scenario(
+    "features/sdpln_assign_release.feature",
+    "Assign resources to sdp mid subarray using TMC leaf node",
+)
+def test_assign_resources_on_sdp_in_mid():
+    """AssignResources on sdp subarray in mid using the leaf node."""
+
+
+@given("a SDP subarray in the EMPTY state", target_fixture="composition")
+def an_sdp_subarray_in_empty_state(
+    set_sdp_ln_entry_point, base_composition: conf_types.Composition
+) -> conf_types.Composition:
+    """
+    an SDP subarray in Empty state.
+
+    :param set_sdp_ln_entry_point: An object to set sdp leafnode entry point
+    :param base_composition : An object for base composition
+    :return: base composition
+    """
+    return base_composition
+
+
+@given("a TMC SDP subarray Leaf Node")
+def a_sdp_sln():
+    """a TMC SDP subarray Leaf Node."""
+
+
+# @when("I assign resources to it") from ...conftest
+
+
+@then("the SDP subarray must be in IDLE state")
+def the_sdp_subarray_must_be_in_idle_state(sut_settings: SutTestSettings):
+    """
+    the SDP Subarray must be in IDLE state.
+
+    :param sut_settings: A class representing the settings for the system under test.
+    """
+    tel = names.TEL()
+    subarray = con_config.get_device_proxy(tel.sdp.subarray(sut_settings.subarray_id))
+    result = subarray.read_attribute("obsState").value
+    assert_that(result).is_equal_to(ObsState.IDLE)
--- tests/resources/models/tmc_model/leafnodes/sdpln_entry_point.py
+++ tests/resources/models/tmc_model/leafnodes/sdpln_entry_point.py
@@ -1,4 +1,5 @@
 """Domain logic for the sdp."""
+import json
 import logging
 from time import sleep
 from typing import List
@@ -80,7 +81,8 @@ class SdpLnAssignResourcesStep(SdpAssignResourcesStep):
         # currently ignore composition as all types will be standard
         subarray_name = self._tel.tm.subarray(sub_array_id).sdp_leaf_node
         subarray = con_config.get_device_proxy(subarray_name)
-        config = self.observation.generate_sdp_assign_resources_config().as_json
+        # config = self.observation.generate_sdp_assign_resources_config().as_json
+        config = json.dumps(ASSIGN_MID_JSON)
         # we retry this command three times in case there is a transitory race
         # condition
 
@@ -259,3 +261,148 @@ class SDPLnEntryPoint(CompositeEntryPoint):
         self.assign_resources_step = SdpLnAssignResourcesStep(observation)
         self.configure_scan_step = SdpLnConfigureStep(observation)
         self.scan_step = SDPLnScanStep(observation)
+
+
+ASSIGN_MID_JSON = {
+    "interface": "https://schema.skao.int/ska-tmc-assignresources/2.1",
+    "transaction_id": "txn-....-00001",
+    "subarray_id": 1,
+    "dish": {"receptor_ids": ["SKA001"]},
+    "sdp": {
+        "interface": "https://schema.skao.int/ska-sdp-assignres/0.4",
+        "execution_block": {
+            "eb_id": "eb-mvp01-20200325-00001",
+            "max_length": 100,
+            "context": {},
+            "beams": [
+                {"beam_id": "vis0", "function": "visibilities"},
+                {"beam_id": "pss1", "search_beam_id": 1, "function": "pulsar search"},
+                {"beam_id": "pss2", "search_beam_id": 2, "function": "pulsar search"},
+                {"beam_id": "pst1", "timing_beam_id": 1, "function": "pulsar timing"},
+                {"beam_id": "pst2", "timing_beam_id": 2, "function": "pulsar timing"},
+                {"beam_id": "vlbi1", "vlbi_beam_id": 1, "function": "vlbi"},
+            ],
+            "scan_types": [
+                {
+                    "scan_type_id": ".default",
+                    "beams": {
+                        "vis0": {"channels_id": "vis_channels", "polarisations_id": "all"},
+                        "pss1": {
+                            "field_id": "pss_field_0",
+                            "channels_id": "pulsar_channels",
+                            "polarisations_id": "all",
+                        },
+                        "pss2": {
+                            "field_id": "pss_field_1",
+                            "channels_id": "pulsar_channels",
+                            "polarisations_id": "all",
+                        },
+                        "pst1": {
+                            "field_id": "pst_field_0",
+                            "channels_id": "pulsar_channels",
+                            "polarisations_id": "all",
+                        },
+                        "pst2": {
+                            "field_id": "pst_field_1",
+                            "channels_id": "pulsar_channels",
+                            "polarisations_id": "all",
+                        },
+                        "vlbi": {
+                            "field_id": "vlbi_field",
+                            "channels_id": "vlbi_channels",
+                            "polarisations_id": "all",
+                        },
+                    },
+                },
+                {
+                    "scan_type_id": "target:a",
+                    "derive_from": ".default",
+                    "beams": {"vis0": {"field_id": "field_a"}},
+                },
+            ],
+            "channels": [
+                {
+                    "channels_id": "vis_channels",
+                    "spectral_windows": [
+                        {
+                            "spectral_window_id": "fsp_1_channels",
+                            "count": 744,
+                            "start": 0,
+                            "stride": 2,
+                            "freq_min": 350000000,
+                            "freq_max": 368000000,
+                            "link_map": [[0, 0], [200, 1], [744, 2], [944, 3]],
+                        },
+                        {
+                            "spectral_window_id": "fsp_2_channels",
+                            "count": 744,
+                            "start": 2000,
+                            "stride": 1,
+                            "freq_min": 360000000,
+                            "freq_max": 368000000,
+                            "link_map": [[2000, 4], [2200, 5]],
+                        },
+                        {
+                            "spectral_window_id": "zoom_window_1",
+                            "count": 744,
+                            "start": 4000,
+                            "stride": 1,
+                            "freq_min": 360000000,
+                            "freq_max": 361000000,
+                            "link_map": [[4000, 6], [4200, 7]],
+                        },
+                    ],
+                },
+                {
+                    "channels_id": "pulsar_channels",
+                    "spectral_windows": [
+                        {
+                            "spectral_window_id": "pulsar_fsp_channels",
+                            "count": 744,
+                            "start": 0,
+                            "freq_min": 350000000,
+                            "freq_max": 368000000,
+                        }
+                    ],
+                },
+            ],
+            "polarisations": [{"polarisations_id": "all", "corr_type": ["XX", "XY", "YY", "YX"]}],
+            "fields": [
+                {
+                    "field_id": "field_a",
+                    "phase_dir": {
+                        "ra": [123, 0.1],
+                        "dec": [123, 0.1],
+                        "reference_time": "...",
+                        "reference_frame": "ICRF3",
+                    },
+                    "pointing_fqdn": "low-tmc/telstate/0/pointing",
+                }
+            ],
+        },
+        "processing_blocks": [
+            {
+                "pb_id": "pb-mvp01-20200325-00001",
+                "sbi_ids": ["sbi-mvp01-20200325-00001"],
+                "script": {},
+                "parameters": {},
+                "dependencies": {},
+            },
+            {
+                "pb_id": "pb-mvp01-20200325-00002",
+                "sbi_ids": ["sbi-mvp01-20200325-00002"],
+                "script": {},
+                "parameters": {},
+                "dependencies": {},
+            },
+            {
+                "pb_id": "pb-mvp01-20200325-00003",
+                "sbi_ids": ["sbi-mvp01-20200325-00001", "sbi-mvp01-20200325-00002"],
+                "script": {},
+                "parameters": {},
+                "dependencies": {},
+            },
+        ],
+        "resources": {"csp_links": [1, 2, 3, 4], "receptors": ["FS4", "FS8"], "receive_nodes": 10},
+    },
+}

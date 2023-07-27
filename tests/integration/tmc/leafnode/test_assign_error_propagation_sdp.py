ec3cec1618d477cbcdd0af69bc6366b41354d5f4
akshaysonawane <akshayss.ska@gmail.com>
2023-07-27 18:34:37 +0530

    HM-277: run test cases in low

--- tests/integration/tmc/leafnode/test_assign_error_propagation_sdp.py
+++ tests/integration/tmc/leafnode/test_assign_error_propagation_sdp.py
@@ -10,7 +10,6 @@ from ska_ser_skallop.mvp_control.entry_points import types as conf_types
 from ...conftest import SutTestSettings
 
 
-@pytest.mark.sdpln
 @pytest.mark.skamid
 @pytest.mark.assign
 @scenario(

c7e59a3f259f261bf4f27f067813686325582837
akshaysonawane <akshayss.ska@gmail.com>
2023-08-04 23:43:09 +0530

    HM-277: update test cases

--- tests/integration/tmc/leafnode/test_assign_error_propagation_sdpln.py
+++ tests/integration/tmc/leafnode/test_assign_error_propagation_sdpln.py
@@ -153,10 +153,10 @@ def lrcr_event(
     assert message == "3"
     running_telescope.disable_automatic_setdown()
     tel = names.TEL()
-    subarray = con_config.get_device_proxy(tel.tm.subarray(sut_settings.subarray_id).sdp_leaf_node)
+    subarray = con_config.get_device_proxy(tel.sdp.subarray(sut_settings.subarray_id))
     subarray_name = tel.sdp.subarray(sut_settings.subarray_id)
     context_monitoring.wait_for(subarray_name).for_attribute("obsState").to_become_equal_to(
         "EMPTY", ignore_first=False, settings=integration_test_exec_settings
      )
-    result = subarray.read_attribute("sdpSubarrayObsState").value
+    result = subarray.read_attribute("obsState").value
     assert result == "0"

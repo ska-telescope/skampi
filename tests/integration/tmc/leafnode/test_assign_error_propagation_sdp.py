886c6afa870594f6a781227a9f442a83f4158a93
akshaysonawane <akshayss.ska@gmail.com>
2023-08-07 19:47:53 +0530

    HM-277: update test case

--- tests/integration/tmc/leafnode/test_assign_error_propagation_sdp.py
+++ tests/integration/tmc/leafnode/test_assign_error_propagation_sdp.py
@@ -74,7 +74,7 @@ def lrcr_event(
     elapsed_time = 0
     time_out = 30
     while (
-        resultcode_or_message != "Execution block eb-mvp01-20210623-00000 already exists"
+        resultcode_or_message != "3"
         and elapsed_time > time_out
     ):
         time.sleep(0.1)

0bc910565d33288e7f18ff1e83d6f9e1010f7ea0
devesh pandey <devesh.ska@gmail.com>
2023-08-07 19:51:42 +0530

    HM-277:Update test case

--- tests/integration/tmc/leafnode/test_assign_error_propagation_sdpln.py
+++ tests/integration/tmc/leafnode/test_assign_error_propagation_sdpln.py
@@ -75,9 +75,9 @@ def lrcr_event(
     "EMPTY", ignore_first=False, settings=integration_test_exec_settings
     )
     context_monitoring.wait_for(subarray_name).for_attribute("longRunningCommandsInQueue").to_become_equal_to(
-        ("AssignResources",), ignore_first=False, settings=integration_test_exec_settings
+        [("AssignResources",),('ReleaseAllResources', 'AssignResources') ], ignore_first=False, settings=integration_test_exec_settings
     )
     context_monitoring.wait_for(subarray_name).for_attribute("longRunningCommandResult").to_become_equal_to(
-        (f"{unique_id[0]}","Execution block eb-mvp01-20210623-00000 already exists"), ignore_first=False, settings=integration_test_exec_settings
+        [(f"{unique_id[0]}","Execution block eb-mvp01-20210623-00000 already exists")], ignore_first=False, settings=integration_test_exec_settings
     )
     

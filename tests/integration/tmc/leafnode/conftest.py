5b8d9809afee88d0d2f111a0a182d6e316be6517
devesh pandey <devesh.ska@gmail.com>
2023-08-08 16:11:28 +0530

    HM-277:Update test case

--- tests/integration/tmc/leafnode/conftest.py
+++ tests/integration/tmc/leafnode/conftest.py
@@ -155,12 +155,3 @@ def an_sdp_subarray_in_idle_state(
     # subarray_allocation_spec.composition
     return sdp_base_configuration
 
-@pytest.fixture(name="sdpln_test_exec_settings", autouse=True)
-def fxt_sdp_start_up_test_exec_settings(
-    integration_test_exec_settings: fxt_types.exec_settings,
-):
-    """General startup test execution settings specific to telescope from tmc.
-
-    :param integration_test_exec_settings: Fixture as used by skallop
-    """
-    integration_test_exec_settings.time_out = 200

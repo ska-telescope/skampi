541daf64d8ce0fcb710a1a716226ed41708649d2
devesh pandey <devesh.ska@gmail.com>
2023-08-11 17:25:57 +0530

    HM-297:Enable tmc test for mid

--- tests/integration/tmc/test_assign_resources.py
+++ tests/integration/tmc/test_assign_resources.py
@@ -38,8 +38,7 @@ def test_assign_resources_to_tmc_subarray_in_low():
 def test_release_resources_from_tmc_subarray_in_low():
     """Release resources from tmc subarrays in low."""
 
-
-@pytest.mark.skip(reason="temp skip for at-489")
+@pytest.mark.tmc
 @pytest.mark.k8s
 @pytest.mark.k8sonly
 @pytest.mark.skamid

bdbee5ac0e50fa475a93cdfba0eaf9d255ff4719
devesh pandey <devesh.ska@gmail.com>
2023-08-17 09:39:40 +0530

    HM-297:Update assertion in the test case

--- tests/integration/tmc/test_recover_subarray_in_resourcing.py
+++ tests/integration/tmc/test_recover_subarray_in_resourcing.py
@@ -94,7 +94,6 @@ def check_long_running_command_result_error(
     csp_error_msg = 'Exception occurred on the following devices:\\nska_low/tm_leaf_node/csp_subarray01: [2, \"Task queued\"]\\n'
     sdp_error_msg = 'ska_low/tm_leaf_node/sdp_subarray01: Execution block eb-test-20220916-00000 already exists\\n'
     error_msg = subarraynode_error_msg+csp_error_msg+sdp_error_msg
-    integration_test_exec_settings.attr_synching = False
     context_monitoring.wait_for(central_node_name).for_attribute(
         "longRunningCommandResult"
     ).to_change_in_order([f"('{unique_id[0]}', '{error_msg}')",f"('{unique_id[0]}', '3')"],
@@ -108,7 +107,6 @@ def check_csp_subarray__in_idle(
     context_monitoring: fxt_types.context_monitoring,
     integration_test_exec_settings: fxt_types.exec_settings,
 ):
-    integration_test_exec_settings.attr_synching = True
     tel = names.TEL()
     subarray_name = tel.csp.subarray(sut_settings.subarray_id)
     context_monitoring.wait_for(subarray_name).for_attribute("obsState").to_become_equal_to(

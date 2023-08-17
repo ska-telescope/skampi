3e0f2a1ddcc4b0ddf11d21dd3f9286d9c9de9afc
devesh pandey <devesh.ska@gmail.com>
2023-08-17 17:35:12 +0530

    HM-297:Update assertion

--- tests/integration/tmc/test_recover_subarray_in_resourcing.py
+++ tests/integration/tmc/test_recover_subarray_in_resourcing.py
@@ -97,7 +97,7 @@ def check_long_running_command_result_error(
     integration_test_exec_settings.attr_synching = False
     context_monitoring.wait_for(central_node_name).for_attribute(
         "longRunningCommandResult"
-    ).to_become_equal_to(f"('{unique_id[0]}', '{error_msg}')",
+    ).to_become_equal_to([f"('{unique_id[0]}', '{error_msg}')",f"('{unique_id[0]}', '3')"],
        ignore_first=False, settings=integration_test_exec_settings
     )
     

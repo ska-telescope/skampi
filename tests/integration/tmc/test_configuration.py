df91408cb9466f4e91e6e5225daf94a901f932ce
akshaysonawane <akshayss.ska@gmail.com>
2023-08-22 15:10:17 +0530

    HM-293: add wait

--- tests/integration/tmc/test_configuration.py
+++ tests/integration/tmc/test_configuration.py
@@ -121,7 +121,7 @@ def check_obsstate_attribute():
     while True:
         attribute_list = eda_es.read_attribute("AttributeList")
         logger.info(f"-------------->list{attribute_list.value}")
-        if len(attribute_list.value) == 1:
+        if len(attribute_list.value) >= 1:
             return True
         time.sleep(1) 
 

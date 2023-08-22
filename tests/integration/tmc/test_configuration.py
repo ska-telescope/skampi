16d12e4a46496b4937ff69f97d4cac02f11106d9
devesh pandey <devesh.ska@gmail.com>
2023-08-22 17:55:18 +0530

    HM-293: update check

--- tests/integration/tmc/test_configuration.py
+++ tests/integration/tmc/test_configuration.py
@@ -133,11 +133,9 @@ def configure_archiver(
             timeout=None,
         )
     assert response.status_code == 200
-    context_monitoring.wait_for(EVENT_SUBSCRIBER).for_attribute("AttributeStartedList").to_become_equal_to(
-        f'["ska_{CONFIG}/tm_subarray_node/1/obsstate"]', ignore_first=False, settings=integration_test_exec_settings
-    )
+
     context_monitoring.wait_for(EVENT_SUBSCRIBER).for_attribute("AttributeStartedNumber").to_become_equal_to(
-        "1", ignore_first=False, settings=integration_test_exec_settings
+        "3", settings=integration_test_exec_settings
     )
 
     status = eda_es.command_inout("AttributeStatus", f"ska_{CONFIG}/tm_subarray_node/1/obsstate")
